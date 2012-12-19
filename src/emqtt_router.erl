bac534c8c86fad3065883334e62e277a9ac26b2c
Ery Lee <ery.lee@gmail.com>
2012-12-19 13:45:04 +0800

    fix router

--- src/emqtt_router.erl
+++ src/emqtt_router.erl
@@ -7,13 +7,13 @@
 -behaviour(gen_server).
 
 -export([init/1,
-	handle_call/3,
-	handle_cast/2,
-	handle_info/2,
-	terminate/2,
-	code_change/3]).
+		handle_call/3,
+		handle_cast/2,
+		handle_info/2,
+		terminate/2,
+		code_change/3]).
 
--record(state,{}).
+-record(state, {}).
 
 start_link() ->
 	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
