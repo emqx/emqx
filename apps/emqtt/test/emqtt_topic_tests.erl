5ab4db7caa4d3c567f77ebd24068ca67fad4cb72
Feng Lee <ery.lee@gmail.com>
2015-01-06 21:54:08 +0800

    tests

--- apps/emqtt/test/emqtt_topic_tests.erl
+++ apps/emqtt/test/emqtt_topic_tests.erl
@@ -1,12 +1,13 @@
+
 -module(emqtt_topic_tests).
 
--include("emqtt_internal.hrl").
+-include("emqtt_topic.hrl").
 
 -import(emqtt_topic, [validate/1, type/1, match/2, triples/1, words/1]).
 
 -ifdef(TEST).
 
--include_lib("enunit/include/enunit.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 validate_test() ->
 	?assert( validate({subscribe, <<"a/b/c">>}) ),
@@ -17,8 +18,8 @@ validate_test() ->
 
 type_test() ->
 	?assertEqual(direct, type(#topic{name = <<"/a/b/cdkd">>})),
-	?assertEqual(wildcard, type(#type{name = <<"/a/+/d">>})),
-	?assertEqual(wildcard, type(#type{name = <<"/a/b/#">>})).
+	?assertEqual(wildcard, type(#topic{name = <<"/a/+/d">>})),
+	?assertEqual(wildcard, type(#topic{name = <<"/a/b/#">>})).
 
 -endif.
 
