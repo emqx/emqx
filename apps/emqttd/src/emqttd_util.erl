27f0b43e9000e823f4ec999c5ba8ff96cb5a16c3
Feng Lee <ery.lee@gmail.com>
2015-04-30 12:37:56 +0800

    fix edoc

--- apps/emqttd/src/emqttd_util.erl
+++ apps/emqttd/src/emqttd_util.erl
@@ -1,5 +1,5 @@
 %%%-----------------------------------------------------------------------------
-%%% @Copyright (C) 2012-2015, Feng Lee <feng@emqtt.io>
+%%% Copyright (c) 2012-2015 eMQTT.IO, All Rights Reserved.
 %%%
 %%% Permission is hereby granted, free of charge, to any person obtaining a copy
 %%% of this software and associated documentation files (the "Software"), to deal
