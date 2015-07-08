630dd4f0894e0b0207be8c2d6400c9815e54abe4
Feng <ery.lee@gmail.com>
2015-07-08 22:02:00 +0800

    https listener

--- src/emqttd.erl
+++ src/emqttd.erl
@@ -85,6 +85,11 @@ open_listener({mqtts, Port, Options}) ->
 %% open http port
 open_listener({http, Port, Options}) ->
     MFArgs = {emqttd_http, handle_request, []},
+	mochiweb:start_http(Port, Options, MFArgs);
+
+%% open https port
+open_listener({https, Port, Options}) ->
+    MFArgs = {emqttd_http, handle_request, []},
 	mochiweb:start_http(Port, Options, MFArgs).
 
 open_listener(Protocol, Port, Options) ->
