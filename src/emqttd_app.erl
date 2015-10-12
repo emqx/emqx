deaf2203434f2ab1502a11768cd66c7d2d139acc
Feng <ery.lee@gmail.com>
2015-10-12 21:05:37 +0800

    sysmon

--- src/emqttd_app.erl
+++ src/emqttd_app.erl
@@ -88,7 +88,7 @@ start_servers(Sup) ->
                {"emqttd mode supervisor", emqttd_mod_sup},
                {"emqttd bridge supervisor", {supervisor, emqttd_bridge_sup}},
                {"emqttd access control", emqttd_access_control},
-               {"emqttd system monitor", emqttd_sysmon}],
+               {"emqttd system monitor", emqttd_sysmon, emqttd:env(sysmon)}],
     [start_server(Sup, Server) || Server <- Servers].
 
 start_server(_Sup, {Name, F}) when is_function(F) ->
