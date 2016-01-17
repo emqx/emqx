1cbdc5e9e48700dfc225f8a8f7ce46aea3cf6551
Feng <ery.lee@gmail.com>
2016-01-17 20:00:46 +0800

    use emqttd:reg_name/2 to replace name/1

--- src/emqttd_pooler.erl
+++ src/emqttd_pooler.erl
@@ -53,10 +53,7 @@ start_link() ->
 %%%=============================================================================
 -spec start_link(atom(), pos_integer()) -> {ok, pid()} | ignore | {error, any()}.
 start_link(Pool, Id) ->
-    gen_server:start_link({local, name(Id)}, ?MODULE, [Pool, Id], []).
-
-name(Id) ->
-    list_to_atom(lists:concat([?MODULE, "_", integer_to_list(Id)])).
+    gen_server:start_link({local, emqttd:reg_name(?MODULE, Id)}, ?MODULE, [Pool, Id], []).
 
 %%------------------------------------------------------------------------------
 %% @doc Submit work to pooler
