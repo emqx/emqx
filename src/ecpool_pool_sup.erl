f1cbfee1fc08a55e1c71bbc60ddc5523abff32b4
Feng <ery.lee@gmail.com>
2016-04-12 23:53:42 +0800

    Update Copyright

--- src/ecpool_pool_sup.erl
+++ src/ecpool_pool_sup.erl
@@ -1,5 +1,5 @@
 %%%-----------------------------------------------------------------------------
-%%% Copyright (c) 2015 eMQTT.IO, All Rights Reserved.
+%%% Copyright (c) 2015-2016 Feng Lee <feng@emqtt.io>.
 %%%
 %%% Permission is hereby granted, free of charge, to any person obtaining a copy
 %%% of this software and associated documentation files (the "Software"), to deal
