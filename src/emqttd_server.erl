aab2bddedaa459258514901713e7da8cc7aef29d
Feng <ery.lee@gmail.com>
2016-09-02 09:11:55 +0800

    fix issue #671 - fix clientid trace

--- src/emqttd_server.erl
+++ src/emqttd_server.erl
@@ -106,9 +106,13 @@ trace(publish, From, _Msg) when is_atom(From) ->
     %% Dont' trace '$SYS' publish
     ignore;
 
-trace(publish, From, #mqtt_message{topic = Topic, payload = Payload}) ->
+trace(publish, {ClientId, Username}, #mqtt_message{topic = Topic, payload = Payload}) ->
+    lager:info([{client, ClientId}, {topic, Topic}],
+               "~s/~s PUBLISH to ~s: ~p", [ClientId, Username, Topic, Payload]);
+
+trace(publish, From, #mqtt_message{topic = Topic, payload = Payload}) when is_binary(From); is_list(From) ->
     lager:info([{client, From}, {topic, Topic}],
-               "~p PUBLISH to ~s: ~p", [From, Topic, Payload]).
+               "~s PUBLISH to ~s: ~p", [From, Topic, Payload]).
 
 %% @doc Unsubscribe
 -spec(unsubscribe(binary()) -> ok | emqttd:pubsub_error()).
