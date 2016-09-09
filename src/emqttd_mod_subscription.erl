66c84aef00a80cd09703cf0eacd20e08bc29e9d9
Feng Lee <ery.lee@gmail.com>
2016-09-09 17:00:12 +0800

    '' -> '%u', '' -> '%c'

--- src/emqttd_mod_subscription.erl
+++ src/emqttd_mod_subscription.erl
@@ -32,7 +32,7 @@ on_client_connected(?CONNACK_ACCEPT, Client = #mqtt_client{client_id  = ClientId
                                                            client_pid = ClientPid,
                                                            username   = Username}, Topics) ->
 
-    Replace = fun(Topic) -> rep(<<"$u">>, Username, rep(<<"$c">>, ClientId, Topic)) end,
+    Replace = fun(Topic) -> rep(<<"%u">>, Username, rep(<<"%c">>, ClientId, Topic)) end,
     TopicTable = [{Replace(Topic), Qos} || {Topic, Qos} <- Topics],
     emqttd_client:subscribe(ClientPid, TopicTable),
     {ok, Client};
@@ -43,10 +43,10 @@ on_client_connected(_ConnAck, _Client, _State) ->
 unload(_Opts) ->
     emqttd:unhook('client.connected', fun ?MODULE:on_client_connected/3).
 
-rep(<<"$c">>, ClientId, Topic) ->
-    emqttd_topic:feed_var(<<"$c">>, ClientId, Topic);
-rep(<<"$u">>, undefined, Topic) ->
+rep(<<"%c">>, ClientId, Topic) ->
+    emqttd_topic:feed_var(<<"%c">>, ClientId, Topic);
+rep(<<"%u">>, undefined, Topic) ->
     Topic;
-rep(<<"$u">>, Username, Topic) ->
-    emqttd_topic:feed_var(<<"$u">>, Username, Topic).
+rep(<<"%u">>, Username, Topic) ->
+    emqttd_topic:feed_var(<<"%u">>, Username, Topic).
 
