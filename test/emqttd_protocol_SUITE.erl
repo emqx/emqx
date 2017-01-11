8092543efba48926e82f95a7370aae4dc128add7
huangdan <wang_yi20081014@163.com>
2017-01-11 13:24:40 +0800

    Copyright (c) 2012-2017

--- test/emqttd_protocol_SUITE.erl
+++ test/emqttd_protocol_SUITE.erl
@@ -1,5 +1,5 @@
 %%--------------------------------------------------------------------
-%% Copyright (c) 2012-2016 Feng Lee <feng@emqtt.io>.
+%% Copyright (c) 2012-2017 Feng Lee <feng@emqtt.io>.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
