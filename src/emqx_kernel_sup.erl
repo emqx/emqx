83dee0e34009caf1b64df20d6ddfb098a39b4f87
Feng Lee <feng@emqx.io>
2018-07-19 10:53:41 +0800

    Rename emqx_alarm to emqx_alarm_mgr

--- src/emqx_kernel_sup.erl
+++ src/emqx_kernel_sup.erl
@@ -26,7 +26,7 @@ start_link() ->
 init([]) ->
     {ok, {{one_for_one, 10, 100},
           [child_spec(emqx_pool, supervisor),
-           child_spec(emqx_alarm, worker),
+           child_spec(emqx_alarm_mgr, worker),
            child_spec(emqx_hooks, worker),
            child_spec(emqx_stats, worker),
            child_spec(emqx_metrics, worker),
