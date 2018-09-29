fe8671d1e814dbb9e96bf7b9f83177d231640128
Gilbert Wong <gilbertwong96@icloud.com>
2018-09-29 17:37:11 +0800

    Refactor client test case to delete redundant code

--- test/emqx_client_SUITE.erl
+++ test/emqx_client_SUITE.erl
@@ -58,61 +58,29 @@ init_per_suite(Config) ->
 end_per_suite(_Config) ->
     emqx_ct_broker_helpers:run_teardown_steps().
 
+request_response(QoS) ->
+    {ok, Requester, _} = emqx_client:start_link([{proto_ver, v5},
+                                {properties, #{ 'Request-Response-Information' => 1}}]),
+    {ok, Responser, _} = emqx_client:start_link([{proto_ver, v5},
+                                {properties, #{ 'Request-Response-Information' => 1}}]),
+    {ok, ResponseTopic} = emqx_client:sub_response_topic(Responser, false, QoS, <<"request_response_test">>),
+    ct:log("ResponseTopic: ~p",[ResponseTopic]),
+    ok = emqx_client:def_response(Responser, <<"ResponseTest">>),
+    {ok, <<"ResponseTest">>} = emqx_client:request(Requester, <<"request_response_test">>, <<"request_payload">>, QoS),
+    ok = emqx_client:def_response(Responser, fun(<<"request_payload">>) ->
+                                                      <<"ResponseFunctionTest">>;
+                                                 (_) ->
+                                                      <<"404">>
+                                              end),
+    {ok, <<"ResponseFunctionTest">>} = emqx_client:request(Requester, <<"request_response_test">>, <<"request_payload">>, QoS),
+    {ok, <<"404">>} = emqx_client:request(Requester, <<"request_response_test">>, <<"invalid_request">>, QoS),
+    ok = emqx_client:disconnect(Responser),
+    ok = emqx_client:disconnect(Requester).
+
 request_response_test(_Config) ->
-    {ok, Requester1, _} = emqx_client:start_link([{proto_ver, v5},
-                                {properties, #{ 'Request-Response-Information' => 1}}]),
-    {ok, Responser1, _} = emqx_client:start_link([{proto_ver, v5},
-                                {properties, #{ 'Request-Response-Information' => 1}}]),
-    {ok, ResponseTopic1} = emqx_client:sub_response_topic(Responser1, false, ?QOS_2, <<"request_response_test">>),
-    ct:log("ResponseTopic: ~p",[ResponseTopic1]),
-    ok = emqx_client:def_response(Responser1, <<"ResponseTest">>),
-    {ok, <<"ResponseTest">>} = emqx_client:request(Requester1, <<"request_response_test">>, <<"request_payload">>, ?QOS_2),
-    ok = emqx_client:def_response(Responser1, fun(<<"request_payload">>) ->
-                                                      <<"ResponseFunctionTest">>;
-                                                 (_) ->
-                                                      <<"404">>
-                                              end),
-    {ok, <<"ResponseFunctionTest">>} = emqx_client:request(Requester1, <<"request_response_test">>, <<"request_payload">>, ?QOS_2),
-    {ok, <<"404">>} = emqx_client:request(Requester1, <<"request_response_test">>, <<"invalid_request">>, ?QOS_2),
-    ok = emqx_client:disconnect(Responser1),
-    ok = emqx_client:disconnect(Requester1),
-
-    {ok, Requester2, _} = emqx_client:start_link([{proto_ver, v5},
-                                {properties, #{ 'Request-Response-Information' => 1}}]),
-    {ok, Responser2, _} = emqx_client:start_link([{proto_ver, v5},
-                                {properties, #{ 'Request-Response-Information' => 1}}]),
-    {ok, ResponseTopic2} = emqx_client:sub_response_topic(Responser2, false, ?QOS_1, <<"request_response_test">>),
-    ct:log("ResponseTopic: ~p",[ResponseTopic2]),
-    ok = emqx_client:def_response(Responser2, <<"ResponseTest">>),
-    {ok, <<"ResponseTest">>} = emqx_client:request(Requester2, <<"request_response_test">>, <<"request_payload">>, ?QOS_1),
-    ok = emqx_client:def_response(Responser2, fun(<<"request_payload">>) ->
-                                                      <<"ResponseFunctionTest">>;
-                                                 (_) ->
-                                                      <<"404">>
-                                              end),
-    {ok, <<"ResponseFunctionTest">>} = emqx_client:request(Requester2, <<"request_response_test">>, <<"request_payload">>, ?QOS_1),
-    {ok, <<"404">>} = emqx_client:request(Requester2, <<"request_response_test">>, <<"invalid_request">>, ?QOS_1),
-    ok = emqx_client:disconnect(Responser2),
-    ok = emqx_client:disconnect(Requester2),
-
-    {ok, Requester3, _} = emqx_client:start_link([{proto_ver, v5},
-                                {properties, #{ 'Request-Response-Information' => 1}}]),
-    {ok, Responser3, _} = emqx_client:start_link([{proto_ver, v5},
-                                {properties, #{ 'Request-Response-Information' => 1}}]),
-    {ok, ResponseTopic3} = emqx_client:sub_response_topic(Responser3, false, ?QOS_0, <<"request_response_test">>),
-    ct:log("ResponseTopic: ~p",[ResponseTopic3]),
-    ok = emqx_client:def_response(Responser3, <<"ResponseTest">>),
-    {ok, <<"ResponseTest">>} = emqx_client:request(Requester3, <<"request_response_test">>, <<"request_payload">>, ?QOS_0),
-    ok = emqx_client:def_response(Responser3, fun(<<"request_payload">>) ->
-                                                      <<"ResponseFunctionTest">>;
-                                                 (_) ->
-                                                      <<"404">>
-                                              end),
-    {ok, <<"ResponseFunctionTest">>} = emqx_client:request(Requester3, <<"request_response_test">>, <<"request_payload">>, ?QOS_0),
-    {ok, <<"404">>} = emqx_client:request(Requester3, <<"request_response_test">>, <<"invalid_request">>, ?QOS_0),
-    ok = emqx_client:disconnect(Responser3),
-    ok = emqx_client:disconnect(Requester3).
-
+    request_response(?QOS_2),
+    request_response(?QOS_1),
+    request_response(?QOS_0).
 
 receive_messages(Count) ->
     receive_messages(Count, []).
