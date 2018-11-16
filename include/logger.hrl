e41cb5a67755dfa98f0731bf7894d8ef8328eafc
terry-xiaoyu <506895667@qq.com>
2018-11-16 13:15:59 +0800

    Change logger micros

--- include/logger.hrl
+++ include/logger.hrl
@@ -1,17 +1,18 @@
 %%--------------------------------------------------------------------
-%% Logs with header
+%% Logs with a header prefixed to the log message.
+%% And the log args are puted into report_cb for lazy evaluation.
 %%--------------------------------------------------------------------
 -ifdef(LOG_HEADER).
 %% with header
+
 -define(LOG(Level, Format, Args),
         begin
-          (emqx_logger:Level(#{},#{report_cb =>
+          (logger:log(Level, #{},#{report_cb =>
                                     fun(_) ->
                                         {?LOG_HEADER ++ " "++ (Format), (Args)}
                                     end}))
         end).
 -else.
 %% without header
--define(LOG(Level, Format, Args),
-        emqx_logger:Level(Format, Args)).
+-define(LOG(Level, Format, Args), logger:log(Level, Format, Args)).
 -endif.
