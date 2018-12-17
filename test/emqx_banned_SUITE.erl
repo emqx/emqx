d8929c1d9fc07a7bf8f10ff30e5cc7874293f7c1
Feng Lee <feng@emqx.io>
2018-12-17 15:47:12 +0800

    Rename emqx_banned:del/1 to emqx_banned:delete/1

--- test/emqx_banned_SUITE.erl
+++ test/emqx_banned_SUITE.erl
@@ -18,9 +18,7 @@
 -compile(nowarn_export_all).
 
 -include("emqx.hrl").
-
 -include("emqx_mqtt.hrl").
-
 -include_lib("eunit/include/eunit.hrl").
 
 all() -> [t_banned_all].
@@ -29,18 +27,27 @@ t_banned_all(_) ->
     emqx_ct_broker_helpers:run_setup_steps(),
     emqx_banned:start_link(),
     TimeNow = erlang:system_time(second),
-    Banned = #banned{who    = {client_id, <<"TestClient">>},
+    Banned = #banned{who = {client_id, <<"TestClient">>},
                      reason = <<"test">>,
-                     by     = <<"banned suite">>,
-                     desc   = <<"test">>,
-                     until  = TimeNow + 1},
+                     by = <<"banned suite">>,
+                     desc = <<"test">>,
+                     until = TimeNow + 1},
     ok = emqx_banned:add(Banned),
     % here is not expire banned test because its check interval is greater than 5 mins, but its effect has been confirmed
-    ?assert(emqx_banned:check(#{client_id => <<"TestClient">>, username => undefined, peername => {undefined, undefined}})),
+    ?assert(emqx_banned:check(#{client_id => <<"TestClient">>,
+                                username => undefined,
+                                peername => {undefined, undefined}})),
     timer:sleep(2500),
-    ?assertNot(emqx_banned:check(#{client_id => <<"TestClient">>, username => undefined, peername => {undefined, undefined}})),
+    ?assertNot(emqx_banned:check(#{client_id => <<"TestClient">>,
+                                   username => undefined,
+                                   peername => {undefined, undefined}})),
     ok = emqx_banned:add(Banned),
-    ?assert(emqx_banned:check(#{client_id => <<"TestClient">>, username => undefined, peername => {undefined, undefined}})),
-    emqx_banned:del({client_id, <<"TestClient">>}),
-    ?assertNot(emqx_banned:check(#{client_id => <<"TestClient">>, username => undefined, peername => {undefined, undefined}})),
+    ?assert(emqx_banned:check(#{client_id => <<"TestClient">>,
+                                username => undefined,
+                                peername => {undefined, undefined}})),
+    emqx_banned:delete({client_id, <<"TestClient">>}),
+    ?assertNot(emqx_banned:check(#{client_id => <<"TestClient">>,
+                                   username => undefined,
+                                   peername => {undefined, undefined}})),
     emqx_ct_broker_helpers:run_teardown_steps().
+
