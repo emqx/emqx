655c8be505e870336ea50be1e264688ef338fb87
周子博 <349832309@qq.com>
2018-12-18 12:04:05 +0800

    Fix a bug that will not send a will message in some cases

--- src/emqx_session.erl
+++ src/emqx_session.erl
@@ -963,7 +963,8 @@ ensure_expire_timer(State) ->
 
 ensure_will_delay_timer(State = #state{will_msg = #message{headers = #{'Will-Delay-Interval' := WillDelayInterval}}}) ->
     State#state{will_delay_timer = emqx_misc:start_timer(WillDelayInterval * 1000, will_delay)};
-ensure_will_delay_timer(State) ->
+ensure_will_delay_timer(State = #state{will_msg = WillMsg}) ->
+    send_willmsg(WillMsg),
     State.
 
 ensure_stats_timer(State = #state{enable_stats = true, stats_timer = undefined,
