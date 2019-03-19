bab7581c525aedc14b27eebed27fc4bbd40fc293
周子博 <349832309@qq.com>
2019-03-19 16:05:14 +0800

    Fix alarm report bug

--- src/emqx_vm_mon.erl
+++ src/emqx_vm_mon.erl
@@ -67,7 +67,8 @@ init([Opts]) ->
     {ok, ensure_check_timer(#{check_interval => proplists:get_value(check_interval, Opts, 30),
                               process_high_watermark => proplists:get_value(process_high_watermark, Opts, 0.70),
                               process_low_watermark => proplists:get_value(process_low_watermark, Opts, 0.50),
-                              timer => undefined})}.
+                              timer => undefined,
+                              process_alarm => false})}.
 
 handle_call(get_check_interval, _From, State) ->
     {reply, maps:get(check_interval, State, undefined), State};
@@ -92,13 +93,17 @@ handle_cast(_Request, State) ->
 
 handle_info({timeout, Timer, check}, State = #{timer := Timer,
                                                process_high_watermark := ProcHighWatermark,
-                                               process_low_watermark := ProcLowWatermark}) ->
+                                               process_low_watermark := ProcLowWatermark,
+                                               process_alarm := ProcessAlarm}) ->
     ProcessCount = erlang:system_info(process_count),
     case ProcessCount / erlang:system_info(process_limit) of
         Percent when Percent >= ProcHighWatermark ->
             alarm_handler:set_alarm({too_many_processes, ProcessCount});
         Percent when Percent < ProcLowWatermark ->
-            alarm_handler:clear_alarm(too_many_processes)
+            case ProcessAlarm of
+                true -> alarm_handler:clear_alarm(too_many_processes);
+                false -> ok
+            end
     end,
     {noreply, ensure_check_timer(State)}.
 
