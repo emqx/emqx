73278d3a9b5ed5ecee25623db81cbc0bd0be9c1f
周子博 <349832309@qq.com>
2019-05-05 14:40:29 +0800

    Add suboptions for stats

--- src/emqx_stats.erl
+++ src/emqx_stats.erl
@@ -73,6 +73,8 @@
 -define(PUBSUB_STATS, [
     'topics/count',
     'topics/max',
+    'suboptions/count',
+    'suboptions/max',
     'subscribers/count',
     'subscribers/max',
     'subscriptions/count',
@@ -242,9 +244,12 @@ code_change(_OldVsn, State, _Extra) ->
 %%------------------------------------------------------------------------------
 
 safe_update_element(Key, Val) ->
-    try ets:update_element(?TAB, Key, {2, Val})
+    try ets:update_element(?TAB, Key, {2, Val}) of
+        false ->
+            ets:insert_new(?TAB, {Key, Val});
+        true ->
+            true
     catch
         error:badarg ->
-            ets:insert_new(?TAB, {Key, Val})
+            ?LOG(warning, "[Stats] Update ~p to ~p failed", [Key, Val])
     end.
-
