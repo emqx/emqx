c8317a230d4d9bdc03c6b0303ffc4a9015f36911
Feng Lee <feng@emqx.io>
2019-08-02 23:15:52 +0800

    Update copyright

--- test/emqx_reason_codes_tests.erl
+++ test/emqx_reason_codes_tests.erl
@@ -1,6 +1,7 @@
-%% Copyright (c) 2013-2019 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%--------------------------------------------------------------------
+%% Copyright (c) 2019 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%
-%% Licensed under the Apache License, Version 2.0 (the "License")
+%% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
 %% You may obtain a copy of the License at
 %%
@@ -11,6 +12,8 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
+%%--------------------------------------------------------------------
+
 
 -module(emqx_reason_codes_tests).
 
