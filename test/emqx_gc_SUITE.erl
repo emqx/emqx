f352f1948942a18401c8397857ab8cc343731624
Feng Lee <feng@emqx.io>
2019-08-08 22:39:16 +0800

    Improve emqx_pmon module and add more test cases

--- test/emqx_gc_SUITE.erl
+++ test/emqx_gc_SUITE.erl
@@ -33,6 +33,9 @@ t_init(_) ->
     ?assertEqual(#{cnt => {10, 10}, oct => {10, 10}}, emqx_gc:info(GC3)).
 
 t_run(_) ->
+    Undefined = emqx_gc:init(false),
+    ?assertEqual(undefined, Undefined),
+    ?assertEqual({false, undefined}, emqx_gc:run(1, 1, Undefined)),
     GC = emqx_gc:init(#{count => 10, bytes => 10}),
     ?assertEqual({true, GC}, emqx_gc:run(1, 1000, GC)),
     ?assertEqual({true, GC}, emqx_gc:run(1000, 1, GC)),
@@ -42,7 +45,10 @@ t_run(_) ->
     ?assertEqual(#{cnt => {10, 7}, oct => {10, 7}}, emqx_gc:info(GC2)),
     {false, GC3} = emqx_gc:run(3, 3, GC2),
     ?assertEqual(#{cnt => {10, 4}, oct => {10, 4}}, emqx_gc:info(GC3)),
-    ?assertEqual({true, GC}, emqx_gc:run(4, 4, GC3)).
+    ?assertEqual({true, GC}, emqx_gc:run(4, 4, GC3)),
+    %% Disabled?
+    DisabledGC = emqx_gc:init(#{count => 0, bytes => 0}),
+    ?assertEqual({false, DisabledGC}, emqx_gc:run(1, 1, DisabledGC)).
 
 t_info(_) ->
     ?assertEqual(undefined, emqx_gc:info(undefined)),
@@ -54,5 +60,7 @@ t_reset(_) ->
     GC = emqx_gc:init(#{count => 10, bytes => 10}),
     {false, GC1} = emqx_gc:run(5, 5, GC),
     ?assertEqual(#{cnt => {10, 5}, oct => {10, 5}}, emqx_gc:info(GC1)),
-    ?assertEqual(GC, emqx_gc:reset(GC1)).
+    ?assertEqual(GC, emqx_gc:reset(GC1)),
+    DisabledGC = emqx_gc:init(#{count => 0, bytes => 0}),
+    ?assertEqual(DisabledGC, emqx_gc:reset(DisabledGC)).
 
--- test/emqx_pmon_SUITE.erl
+++ test/emqx_pmon_SUITE.erl
@@ -28,13 +28,16 @@ t_monitor(_) ->
     PMon1 = emqx_pmon:monitor(self(), PMon),
     ?assertEqual(1, emqx_pmon:count(PMon1)),
     PMon2 = emqx_pmon:demonitor(self(), PMon1),
+    PMon2 = emqx_pmon:demonitor(self(), PMon2),
     ?assertEqual(0, emqx_pmon:count(PMon2)).
 
 t_find(_) ->
     PMon = emqx_pmon:new(),
     PMon1 = emqx_pmon:monitor(self(), val, PMon),
+    PMon1 = emqx_pmon:monitor(self(), val, PMon1),
     ?assertEqual(1, emqx_pmon:count(PMon1)),
     ?assertEqual({ok, val}, emqx_pmon:find(self(), PMon1)),
+    PMon2 = emqx_pmon:erase(self(), PMon1),
     PMon2 = emqx_pmon:erase(self(), PMon1),
     ?assertEqual(error, emqx_pmon:find(self(), PMon2)).
 
@@ -44,6 +47,7 @@ t_erase(_) ->
     PMon2 = emqx_pmon:erase(self(), PMon1),
     ?assertEqual(0, emqx_pmon:count(PMon2)),
     {Items, PMon3} = emqx_pmon:erase_all([self()], PMon1),
+    {[], PMon3} = emqx_pmon:erase_all([self()], PMon3),
     ?assertEqual([{self(), val}], Items),
     ?assertEqual(0, emqx_pmon:count(PMon3)).
 
