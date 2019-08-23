ebc1bd77a9e7124e52db152467a1e0ac685433a4
tigercl <zhouzb@emqx.io>
2019-08-23 14:41:52 +0800

    Send the will message immediately when the network connection is closed by the client (#2834)
    Send the will message immediately when the network connection is closed by the client

--- test/emqx_channel_SUITE.erl
+++ test/emqx_channel_SUITE.erl
@@ -143,7 +143,7 @@ t_handle_pingreq(_) ->
 t_handle_disconnect(_) ->
     with_channel(
       fun(Channel) ->
-              {stop, normal, Channel1} = handle_in(?DISCONNECT_PACKET(?RC_SUCCESS), Channel),
+              {stop, {shutdown, normal}, Channel1} = handle_in(?DISCONNECT_PACKET(?RC_SUCCESS), Channel),
               ?assertMatch(#{will_msg := undefined}, emqx_channel:info(protocol, Channel1))
       end).
 
