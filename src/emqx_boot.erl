2bf951d953996fcc2eabd65b1829f7d3d45d5623
Feng Lee <feng@emqx.io>
2019-09-10 16:24:38 +0800

    Fix the spec of function 'is_enabled/1'

--- src/emqx_boot.erl
+++ src/emqx_boot.erl
@@ -20,7 +20,7 @@
 
 -define(BOOT_MODULES, [router, broker, listeners]).
 
--spec(is_enabled(all|list(router|broker|listener)) -> boolean()).
+-spec(is_enabled(all|list(router|broker|listeners)) -> boolean()).
 is_enabled(Mod) ->
     (BootMods = boot_modules()) =:= all orelse lists:member(Mod, BootMods).
 
