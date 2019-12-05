867bfdee6d922a48862285f73edc94cdb81ceda4
Feng Lee <feng@emqx.io>
2019-12-05 15:11:29 +0800

    Add a new counter 'packets.pubcomp.inuse'

--- src/emqx_metrics.erl
+++ src/emqx_metrics.erl
@@ -101,6 +101,7 @@
     {counter, 'packets.pubrel.missed'},         % PUBREL packets missed
     {counter, 'packets.pubcomp.received'},      % PUBCOMP packets received
     {counter, 'packets.pubcomp.sent'},          % PUBCOMP packets sent
+    {counter, 'packets.pubcomp.inuse'},         % PUBCOMP packet_id inuse
     {counter, 'packets.pubcomp.missed'},        % PUBCOMP packets missed
     {counter, 'packets.subscribe.received'},    % SUBSCRIBE Packets received
     {counter, 'packets.subscribe.error'},       % SUBSCRIBE error
@@ -460,5 +461,6 @@ reserved_idx('messages.forward')             -> 51;
 reserved_idx('auth.mqtt.anonymous')          -> 52;
 reserved_idx('channel.gc.cnt')               -> 53;
 reserved_idx('packets.pubrec.inuse')         -> 54;
+reserved_idx('packets.pubcomp.inuse')        -> 55;
 reserved_idx(_)                              -> undefined.
 
