7f807c0b113029d5cc9513a577ee7e6513e44945
turtled <turtled@emqtt.io>
2019-12-30 11:18:26 +0800

    Fix test cases fail

--- test/emqx_connection_SUITE.erl
+++ test/emqx_connection_SUITE.erl
@@ -57,6 +57,7 @@ end_per_suite(_Config) ->
     ok = meck:unload(emqx_limiter),
     ok = meck:unload(emqx_pd),
     ok = meck:unload(emqx_metrics),
+    ok = meck:unload(emqx_hooks),
     ok.
 
 init_per_testcase(_TestCase, Config) ->
