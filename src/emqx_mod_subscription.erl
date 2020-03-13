f5ce673908947d9080e44d772831315d4a1984fc
zhanghongtong <zhanghongtong@foxmail.com>
2020-03-13 18:36:12 +0800

    Update proxy sub

--- src/emqx_mod_subscription.erl
+++ src/emqx_mod_subscription.erl
@@ -36,12 +36,12 @@
 load(Topics) ->
     emqx_hooks:add('client.connected', {?MODULE, on_client_connected, [Topics]}).
 
-on_client_connected(#{clientid := ClientId, username := Username}, _ConnInfo = #{proto_ver := Proto_ver}, Topics) ->
+on_client_connected(#{clientid := ClientId, username := Username}, _ConnInfo = #{proto_ver := ProtoVer}, Topics) ->
     Replace = fun(Topic) ->
                       rep(<<"%u">>, Username, rep(<<"%c">>, ClientId, Topic))
               end,
-    TopicFilters =  case Proto_ver of
-        5 -> [{Replace(Topic), Suboption} || {Topic, Suboption} <- Topics];
+    TopicFilters =  case ProtoVer of
+        ?MQTT_PROTO_V5 -> [{Replace(Topic), SubOpts} || {Topic, SubOpts} <- Topics];
         _ -> [{Replace(Topic), #{qos => Qos}} || {Topic, #{qos := Qos}} <- Topics]
     end,
     self() ! {subscribe, TopicFilters}.
