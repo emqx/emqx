1811ea017acc5221f052ee5638626b52091dc9ff
JianBo He <heeejianbo@gmail.com>
2020-03-17 12:01:14 +0800

    Move emqx_delay_publish here

--- src/emqx_sup.erl
+++ src/emqx_sup.erl
@@ -67,11 +67,12 @@ init([]) ->
     BrokerSup = child_spec(emqx_broker_sup, supervisor),
     CMSup = child_spec(emqx_cm_sup, supervisor),
     SysSup = child_spec(emqx_sys_sup, supervisor),
+    ModSup = child_spec(emqx_mod_sup, supervisor),
     Childs = [KernelSup] ++
              [RouterSup || emqx_boot:is_enabled(router)] ++
              [BrokerSup || emqx_boot:is_enabled(broker)] ++
              [CMSup || emqx_boot:is_enabled(broker)] ++
-             [SysSup],
+             [SysSup] ++ [ModSup],
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1
