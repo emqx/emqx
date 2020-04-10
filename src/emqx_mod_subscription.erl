686ea260de1932d137131fd12363c6d0fd3a383f
turtleDeng <deng@emqx.io>
2020-04-10 19:51:28 +0800

    Update emqx_mod_subscription.erl

--- src/emqx_mod_subscription.erl
+++ src/emqx_mod_subscription.erl
@@ -51,7 +51,7 @@ unload(_) ->
     emqx_hooks:del('client.connected', {?MODULE, on_client_connected}).
 
 description() ->
-    "EMQ X Subscription Topic Module".
+    "EMQ X Subscription Module".
 %%--------------------------------------------------------------------
 %% Internal functions
 %%--------------------------------------------------------------------
