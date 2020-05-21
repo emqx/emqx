5cd6eb249bbb553b059c303a17aab420cf599256
zhouzb <zhouzb@emqx.io>
2020-05-21 15:27:30 +0800

    Fix test case

--- test/emqx_channel_SUITE.erl
+++ test/emqx_channel_SUITE.erl
@@ -113,7 +113,7 @@ t_handle_in_unexpected_connect_packet(_) ->
         emqx_channel:handle_in(?CONNECT_PACKET(connpkt()), Channel).
 
 t_handle_in_unexpected_packet(_) ->
-    Channel = channel(),
+    Channel = emqx_channel:set_field(conn_state, idle, channel()),
     Packet = ?DISCONNECT_PACKET(?RC_PROTOCOL_ERROR),
     {ok, [{outgoing, Packet}, {close, protocol_error}], Channel} =
         emqx_channel:handle_in(?PUBLISH_PACKET(?QOS_0), Channel).
