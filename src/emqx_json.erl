48b60d715a9cb7981585b8693ef5d92af91f6903
JianBo He <heeejianbo@gmail.com>
2020-09-09 17:57:51 +0800

    fix(json): fix safe_encode for utf8 string

--- src/emqx_json.erl
+++ src/emqx_json.erl
@@ -60,7 +60,7 @@ encode(Term, Opts) ->
 -spec(safe_encode(json_term())
       -> {ok, json_text()} | {error, Reason :: term()}).
 safe_encode(Term) ->
-    safe_encode(Term, []).
+    safe_encode(Term, [force_utf8]).
 
 -spec(safe_encode(json_term(), encode_options())
       -> {ok, json_text()} | {error, Reason :: term()}).
