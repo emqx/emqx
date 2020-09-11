6867ebb9e0ca47a539048b312df3b57faf2a5c93
terry-xiaoyu <506895667@qq.com>
2020-09-12 02:17:34 +0800

    improve(rebar): move build helper mods to mods/

--- mods/mod_project.erl
+++ mods/mod_project.erl
@@ -1,10 +1,8 @@
--module('rebar.config.mod').
+-module(mod_project).
 
 -export([ get_vsn/1
         , coveralls_configs/1
         ]).
-
--export([render/1]).
 
 get_vsn(_Conf) ->
   ComparingFun = fun
@@ -72,42 +70,3 @@ coveralls_configs(_Config) ->
     _ ->
         []
     end.
-
-render(Config) ->
-    PlcHdlrs = proplists:get_value(rebar_place_holders, Config),
-    RealConfig = proplists:delete(rebar_place_holders, Config),
-    %[render_entry(Entry, PlcHdlrs, RealConfig) || Entry <- RealConfig].
-    render_entry(RealConfig, PlcHdlrs).
-
-render_entry(Config, PlcHdlrs) ->
-    render_entry(Config, PlcHdlrs, Config).
-
-render_entry(Entry, PlcHdlrs, Config) when is_tuple(Entry) ->
-    list_to_tuple(render_entry(tuple_to_list(Entry), PlcHdlrs, Config));
-render_entry(Entry, PlcHdlrs, Config) when is_list(Entry) ->
-    lists:foldl(fun(Item, Acc) ->
-            case render_item(Item, PlcHdlrs, Config) of
-                {var, Fun} when is_function(Fun) ->
-                    Acc ++ [Fun(Config)];
-                {var, Var} ->
-                    Acc ++ [render_entry(Var, PlcHdlrs, Config)];
-                {elems, Fun} when is_function(Fun) ->
-                    Acc ++ Fun(Config);
-                {elems, Elems} ->
-                    Acc ++ render_entry(Elems, PlcHdlrs, Config)
-            end
-        end, [], Entry);
-render_entry(Entry, _PlcHdlrs, _Config) ->
-    Entry.
-
-render_item("${"++Key0 = Entry0, PlcHdlrs, Config) ->
-    Key = string:trim(Key0, trailing, "}"),
-    case lists:keyfind(Key, 1, PlcHdlrs) of
-        false -> {var, Entry0};
-        {_, Type, Entry} ->
-            {Type, render_entry(Entry, PlcHdlrs, Config)}
-    end;
-render_item(Entry, PlcHdlrs, Config) when is_tuple(Entry); is_list(Entry) ->
-    {var, render_entry(Entry, PlcHdlrs, Config)};
-render_item(Entry, _PlcHdlrs, _Config) ->
-    {var, Entry}.
