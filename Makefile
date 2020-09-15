7cd75acc87110ffe06c81269bb18cdc76800f108
terry-xiaoyu <506895667@qq.com>
2020-09-15 19:53:12 +0800

    feature(makefile): add Makefile and rebar3

---
+++ Makefile
@@ -0,0 +1,56 @@
+REBAR = $(CURDIR)/rebar3
+
+PROFILE ?= emqx
+PROFILES := emqx emqx-edge
+PKG_PROFILES := emqx-pkg emqx-edge-pkg
+
+export DEFAULT_VSN ?= $(shell ./get-lastest-tag.escript)
+ifneq ($(shell echo $(DEFAULT_VSN) | grep -oE "^[ev0-9]+\.[0-9]+(\.[0-9]+)?"),)
+	export PKG_VSN := $(patsubst v%,%,$(patsubst e%,%,$(DEFAULT_VSN)))
+else
+	export PKG_VSN := $(patsubst v%,%,$(DEFAULT_VSN))
+endif
+
+.PHONY: default
+default: $(REBAR) $(PROFILE)
+
+.PHONY: all
+all: $(REBAR) $(PROFILES)
+
+.PHONY: distclean
+distclean:
+	@rm -rf _build
+
+.PHONY: $(PROFILES)
+$(PROFILES:%=%): $(REBAR)
+ifneq ($(shell echo $(@) |grep edge),)
+	export EMQX_DESC="EMQ X Edge"
+else
+	export EMQX_DESC="EMQ X Broker"
+endif
+	$(REBAR) as $(@) release
+
+.PHONY: $(PROFILES:%=build-%)
+$(PROFILES:%=build-%): $(REBAR)
+	$(REBAR) as $(@:build-%=%) compile
+
+.PHONY: clean $(PROFILES:%=clean-%)
+clean: $(PROFILES:%=clean-%)
+$(PROFILES:%=clean-%): $(REBAR)
+	@rm -rf _build/$(@:clean-%=%)
+	@rm -rf _build/$(@:clean-%=%)+test
+
+.PHONY: deps-all
+deps-all: $(REBAR) $(PROFILES:%=deps-%) $(PKG_PROFILES:%=deps-%)
+
+.PHONY: $(PROFILES:%=deps-%) $(PKG_PROFILES:%=deps-%)
+$(PROFILES:%=deps-%) $(PKG_PROFILES:%=deps-%): $(REBAR)
+ifneq ($(shell echo $(@) |grep edge),)
+	export EMQX_DESC="EMQ X Edge"
+else
+	export EMQX_DESC="EMQ X Broker"
+endif
+	$(REBAR) as $(@:deps-%=%) get-deps
+
+include packages.mk
+include docker.mk
--- mods/mod_project.erl
+++ mods/mod_project.erl
@@ -5,49 +5,11 @@
         ]).
 
 get_vsn(_Conf) ->
-  ComparingFun = fun
-    _Fun([C1|R1], [C2|R2]) when is_list(C1), is_list(C2);
-                                is_integer(C1), is_integer(C2) -> C1 < C2 orelse _Fun(R1, R2);
-    _Fun([C1|R1], [C2|R2]) when is_integer(C1), is_list(C2)    -> _Fun(R1, R2);
-    _Fun([C1|_], [C2|_]) when is_list(C1), is_integer(C2)    -> true;
-    _Fun(_, _) -> false
+  PkgVsn = case os:getenv("PKG_VSN") of
+    false -> error({env_undefined, "PKG_VSN"});
+    Vsn -> Vsn
   end,
-  SortFun = fun(T1, T2) ->
-    C = fun(T) ->
-          [case catch list_to_integer(E) of
-              I when is_integer(I) -> I;
-              _ -> E
-            end || E <- re:split(string:sub_string(T, 2), "[.-]", [{return, list}])]
-        end,
-    ComparingFun(C(T1), C(T2))
-  end,
-  Tag = os:cmd("git describe --abbrev=0 --tags") -- "\n",
-  LatestTagCommitId = os:cmd(io_lib:format("git rev-parse ~s", [Tag])) -- "\n",
-  Tags = string:tokens(os:cmd(io_lib:format("git tag -l \"v*\" --points-at ~s", [LatestTagCommitId])), "\n"),
-  LatestTag = lists:last(lists:sort(SortFun, Tags)),
-  Branch = case os:getenv("GITHUB_RUN_ID") of
-        false -> os:cmd("git branch | grep -e '^*' | cut -d' ' -f 2") -- "\n";
-        _ -> re:replace(os:getenv("GITHUB_REF"), "^refs/heads/|^refs/tags/", "", [global, {return ,list}])
-  end,
-  GitRef =  case re:run(Branch, "master|^dev/|^hotfix/", [{capture, none}]) of
-                match -> {branch, Branch};
-                _ -> {tag, LatestTag}
-            end,
-  DefaultDepRef =
-      case os:getenv("EMQX_DEPS_DEFAULT_VSN") of
-          false -> GitRef; %% not set
-          "" -> GitRef; %% set empty
-          MaybeTag ->
-              case re:run(MaybeTag, "^[ev0-9\]+\.\[0-9\]+\.*") of
-                  nomatch -> {branch, MaybeTag};
-                  _ -> {tag, MaybeTag}
-              end
-      end,
-  T = case DefaultDepRef of
-          {tag, EnvTag} -> EnvTag;
-          _Else -> LatestTag
-      end,
-  re:replace(T, "v", "", [{return ,list}]).
+  re:replace(PkgVsn, "v", "", [{return ,list}]).
 
 coveralls_configs(_Config) ->
     case {os:getenv("GITHUB_ACTIONS"), os:getenv("GITHUB_TOKEN")} of
