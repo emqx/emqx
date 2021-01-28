80afff13a7d2810148c237f46218e4ea97d37884
JianBo He <heeejianbo@gmail.com>
2021-01-28 10:28:16 +0800

    chore(bridge-mqtt): fix styles

--- apps/emqx_bridge_mqtt/src/emqx_bridge_rpc.erl
+++ apps/emqx_bridge_mqtt/src/emqx_bridge_rpc.erl
@@ -62,7 +62,7 @@ stop(#{client_pid := Pid}) when is_pid(Pid) ->
     ok.
 
 %% @doc Callback for `emqx_bridge_connect' behaviour
--spec send(#{address:=node_or_tuple(), _=>_}, batch()) -> {ok, ack_ref()} | {error, any()}.
+-spec send(#{address := node_or_tuple(), _ => _}, batch()) -> {ok, ack_ref()} | {error, any()}.
 send(#{address := Remote}, Batch) ->
     case ?RPC:call(Remote, ?MODULE, handle_send, [Batch]) of
         ok ->
