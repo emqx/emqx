7cece7729ad46ca2a064bea1ffe4d5934d85450e
Zaiming Shi <zmstone@gmail.com>
2021-03-20 10:57:56 +0100

    chore(test): make app-ct run easier

---
+++ scripts/find-apps.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# ensure dir
+cd -P -- "$(dirname -- "$0")/.."
+
+find_app() {
+    local appdir="$1"
+    find "${appdir}/" -mindepth 1 -maxdepth 1 -type d
+}
+
+find_app 'apps'
+if [ -f 'EMQX_ENTERPRISE' ]; then
+    find_app 'lib-ee'
+else
+    find_app 'lib-ce'
+fi
