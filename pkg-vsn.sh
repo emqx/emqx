7e3b7bb0fefeb0935508c21c04b800810d463b8e
zhanghongtong <rory-z@outlook.com>
2021-03-24 00:18:40 +0800

    chore(CI): fix pkg-vsn error

--- pkg-vsn.sh
+++ pkg-vsn.sh
@@ -14,7 +14,7 @@ fi
 
 RELEASE="$(grep -E "define.+EMQX_RELEASE.+${EDITION}" include/emqx_release.hrl | cut -d '"' -f2)"
 
-if [ -d .git ] && ! git describe --tags --match "${RELEASE}" --exact >/dev/null 2>&1; then
+if [ -d .git ] && ! git describe --tags --match "[e|v]${RELEASE}" --exact >/dev/null 2>&1; then
     SUFFIX="-$(git rev-parse HEAD | cut -b1-8)"
 fi
 
