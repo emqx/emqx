06b4da8269836b14428e373f5b1a43764e30b17d
zhanghongtong <rory-z@outlook.com>
2021-04-02 18:00:06 +0800

    chore(script): update find apps script, delete needless "/"

--- scripts/find-apps.sh
+++ scripts/find-apps.sh
@@ -7,7 +7,7 @@ cd -P -- "$(dirname -- "$0")/.."
 
 find_app() {
     local appdir="$1"
-    find "${appdir}/" -mindepth 1 -maxdepth 1 -type d
+    find "${appdir}" -mindepth 1 -maxdepth 1 -type d
 }
 
 find_app 'apps'
@@ -20,4 +20,4 @@ fi
 ## find directories in lib-extra
 find_app 'lib-extra'
 ## find symlinks in lib-extra
-find 'lib-extra/' -mindepth 1 -maxdepth 1 -type l -exec test -e {} \; -print
+find 'lib-extra' -mindepth 1 -maxdepth 1 -type l -exec test -e {} \; -print
