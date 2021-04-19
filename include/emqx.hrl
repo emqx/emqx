bc6a87946c99c426fe54898b8cdef0ddfd09912f
Zaiming Shi <zmstone@gmail.com>
2021-04-19 02:18:35 +0200

    refactor(emqx_trie): store only prefixes
    This commit refactors emqx_trie implementation
    with mainly two changes
    
    1. Deleted the edge table.
    
       In the old implementation, trie consist of nodes and edges.
       e.g. for topic 'a/b/+/d', node 'a' with edge 'b'
       points to node 'a/b' and so on.
    
       However, edges can be computed at runtime,
       so there is no need to store them in a table.

--- include/emqx.hrl
+++ include/emqx.hrl
@@ -90,31 +90,6 @@
          }).
 
 %%--------------------------------------------------------------------
-%% Trie
-%%--------------------------------------------------------------------
-
--type(trie_node_id() :: binary() | atom()).
-
--record(trie_node, {
-          node_id        :: trie_node_id(),
-          edge_count = 0 :: non_neg_integer(),
-          topic          :: binary() | undefined,
-          flags          :: list(atom()) | undefined
-        }).
-
--record(trie_edge, {
-          node_id :: trie_node_id(),
-          word    :: binary() | atom()
-        }).
-
--record(trie, {
-          edge    :: #trie_edge{},
-          node_id :: trie_node_id()
-        }).
-
--type(trie_node() :: #trie_node{}).
-
-%%--------------------------------------------------------------------
 %% Plugin
 %%--------------------------------------------------------------------
 
