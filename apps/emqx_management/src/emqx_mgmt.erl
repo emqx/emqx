42e775579044737700ee61873e8cc93720371573
Zaiming Shi <zmstone@gmail.com>
2021-04-30 11:45:36 +0200

    fix(dashboard): display full Erlang/OTP version

--- apps/emqx_management/src/emqx_mgmt.erl
+++ apps/emqx_management/src/emqx_mgmt.erl
@@ -553,7 +553,7 @@ rpc_call(Node, Fun, Args) ->
     end.
 
 otp_rel() ->
-    lists:concat(["R", erlang:system_info(otp_release), "/", erlang:system_info(version)]).
+    lists:concat([emqx_vm:get_otp_version(), "/", erlang:system_info(version)]).
 
 check_row_limit(Tables) ->
     check_row_limit(Tables, max_row_limit()).
