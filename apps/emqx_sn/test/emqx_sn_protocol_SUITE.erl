cb31d66bf2b186845f20d04be24ba773fd226eda
Shawn <506895667@qq.com>
2021-04-30 15:31:34 +0800

    Cache publishes before receiving the REGACK (#4695)
    * refactor(emqx_sn): return new state from send_message
    
    * fix(emqx_sn): send publish only after regack received

--- apps/emqx_sn/test/emqx_sn_protocol_SUITE.erl
+++ apps/emqx_sn/test/emqx_sn_protocol_SUITE.erl
@@ -1102,10 +1102,12 @@ t_asleep_test04_to_awake_qos1_dl_msg(_) ->
 
     %% send downlink data in asleep state. This message should be send to device once it wake up
     Payload1 = <<55, 66, 77, 88, 99>>,
+    Payload2 = <<55, 66, 77, 88, 100>>,
 
     {ok, C} = emqtt:start_link(),
     {ok, _} = emqtt:connect(C),
     {ok, _} = emqtt:publish(C, <<"a/b/c">>, Payload1, QoS),
+    {ok, _} = emqtt:publish(C, <<"a/b/c">>, Payload2, QoS),
     timer:sleep(100),
     ok = emqtt:disconnect(C),
 
@@ -1114,20 +1116,31 @@ t_asleep_test04_to_awake_qos1_dl_msg(_) ->
     % goto awake state, receive downlink messages, and go back to asleep
     send_pingreq_msg(Socket, <<"test">>),
 
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %% get REGISTER first, since this topic has never been registered
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    UdpData2 = receive_response(Socket),
-    {TopicIdNew, MsgId3} = check_register_msg_on_udp(<<"a/b/c">>, UdpData2),
+    %% 1. get REGISTER first, since this topic has never been registered
+    UdpData1 = receive_response(Socket),
+    {TopicIdNew, MsgId3} = check_register_msg_on_udp(<<"a/b/c">>, UdpData1),
+
+    %% 2. but before we reply the REGACK, the sn-gateway should not send any PUBLISH
+    ?assertError(_, receive_publish(Socket)),
+
     send_regack_msg(Socket, TopicIdNew, MsgId3),
 
-    UdpData = receive_response(Socket),
-    MsgId_udp = check_publish_msg_on_udp({Dup, QoS, Retain, WillBit, CleanSession, ?SN_NORMAL_TOPIC, TopicIdNew, Payload1}, UdpData),
-    send_puback_msg(Socket, TopicIdNew, MsgId_udp),
+    UdpData2 = receive_response(Socket),
+    MsgId_udp2 = check_publish_msg_on_udp({Dup, QoS, Retain, WillBit, CleanSession, ?SN_NORMAL_TOPIC, TopicIdNew, Payload1}, UdpData2),
+    send_puback_msg(Socket, TopicIdNew, MsgId_udp2),
+
+    UdpData3 = receive_response(Socket),
+    MsgId_udp3 = check_publish_msg_on_udp({Dup, QoS, Retain, WillBit, CleanSession, ?SN_NORMAL_TOPIC, TopicIdNew, Payload2}, UdpData3),
+    send_puback_msg(Socket, TopicIdNew, MsgId_udp3),
 
     ?assertEqual(<<2, ?SN_PINGRESP>>, receive_response(Socket)),
 
     gen_udp:close(Socket).
+
+receive_publish(Socket) ->
+    UdpData3 = receive_response(Socket, 1000),
+    <<HeaderUdp:5/binary, _:16, _/binary>> = UdpData3,
+    <<_:8, ?SN_PUBLISH, _/binary>> = HeaderUdp.
 
 t_asleep_test05_to_awake_qos1_dl_msg(_) ->
     QoS = 1,
