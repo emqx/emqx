ca2c773cea0df714e69bc108389d65c30279af93
DDDHuang <904897578@qq.com>
2021-05-14 13:53:48 +0800

    chore: node support info

--- apps/emqx_management/src/emqx_mgmt_api_nodes.erl
+++ apps/emqx_management/src/emqx_mgmt_api_nodes.erl
@@ -41,6 +41,34 @@ get(#{node := Node}, _Params) ->
 format(Node, {error, Reason}) -> #{node => Node, error => Reason};
 
 format(_Node, Info = #{memory_total := Total, memory_used := Used}) ->
-    Info#{memory_total := emqx_mgmt_util:kmg(Total),
-          memory_used  := emqx_mgmt_util:kmg(Used)}.
+    {ok, SysPathBinary} = file:get_cwd(),
+    SysPath = list_to_binary(SysPathBinary),
+    ConfigPath = <<SysPath/binary, "/etc/emqx.conf">>,
+    LogPath = case log_path() of
+                  undefined ->
+                      <<"not found">>;
+                  Path0 ->
+                      Path = list_to_binary(Path0),
+                      <<SysPath/binary, Path/binary>>
+              end,
+    Info#{ memory_total := emqx_mgmt_util:kmg(Total)
+         , memory_used := emqx_mgmt_util:kmg(Used)
+         , sys_path => SysPath
+         , config_path => ConfigPath
+         , log_path => LogPath}.
+
+log_path() ->
+    Configs = logger:get_handler_config(),
+    get_log_path(Configs).
+
+get_log_path([]) ->
+    undefined;
+get_log_path([#{id := ID} = LoggerConfig | LoggerConfigs]) ->
+    case ID of
+        file ->
+            Config = maps:get(config, LoggerConfig),
+            maps:get(file, Config);
+        _ ->
+            get_log_path(LoggerConfigs)
+    end.
 
