c1268d6b35f9dbddd8a816d7c628b5c7fe33d123
Zaiming Shi <zmstone@gmail.com>
2021-06-03 22:01:40 +0200

    build: fix app version check script

--- scripts/apps-version-check.sh
+++ scripts/apps-version-check.sh
@@ -6,11 +6,29 @@ latest_release=$(git describe --tags "$(git rev-list --tags --max-count=1 --remo
 
 bad_app_count=0
 
-while read -r app; do
-    app_path="$app"
-    src_file="$app_path/src/$(basename "$app").app.src"
-    old_app_version="$(git show "$latest_release":"$src_file" | grep vsn | grep -oE '"[0-9]+.[0-9]+.[0-9]+"' | tr -d '"')"
-    now_app_version=$(grep -E 'vsn' "$src_file" | grep -oE '"[0-9]+\.[0-9]+\.[0-9]+"' | tr -d '"')
+get_vsn() {
+    commit="$1"
+    app_src_file="$2"
+    git show "$commit":"$app_src_file" 2>/dev/null | grep vsn | grep -oE '"[0-9]+.[0-9]+.[0-9]+"' | tr -d '"' || true
+}
+
+while read -r app_path; do
+    app=$(basename "$app_path")
+    src_file="$app_path/src/$app.app.src"
+    old_app_version="$(get_vsn "$latest_release" "$src_file")"
+    ## TODO: delete it after 4.3.3 is released
+    if [ "$app" = 'emqx' ] && [ "$old_app_version" = '' ]; then
+        old_app_version="$(get_vsn "$latest_release" 'src/emqx.app.src')"
+    fi
+    if [ -z "$old_app_version" ]; then
+        echo "failed_to_get_old_app_vsn for $app"
+        exit 1
+    fi
+    now_app_version="$(get_vsn 'HEAD' "$src_file")"
+    if [ -z "$now_app_version" ]; then
+        echo "failed_to_get_new_app_vsn for $app"
+        exit 1
+    fi
     if [ "$old_app_version" = "$now_app_version" ]; then
         changed="$(git diff --name-only "$latest_release"...HEAD \
                     -- "$app_path/src" \
@@ -26,5 +44,5 @@ done < <(./scripts/find-apps.sh)
 if [ $bad_app_count -gt 0 ]; then
     exit 1
 else
-    echo "apps version check successfully"       
+    echo "apps version check successfully"
 fi
