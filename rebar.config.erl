0225156576a9402a5d6737424ad00c24175cafff
x1001100011 <x1001100011@gmail.com>
2021-06-09 21:30:00 -0700

    feat: upgrade depends to fix warning in erlang 24

--- rebar.config.erl
+++ rebar.config.erl
@@ -89,7 +89,7 @@ project_app_dirs() ->
 
 plugins(HasElixir) ->
     [ {relup_helper,{git,"https://github.com/emqx/relup_helper", {tag, "2.0.0"}}}
-    , {er_coap_client, {git, "https://github.com/emqx/er_coap_client", {tag, "v1.0"}}}
+    , {er_coap_client, {git, "https://github.com/emqx/er_coap_client", {tag, "v1.0.2"}}}
       %% emqx main project does not require port-compiler
       %% pin at root level for deterministic
     , {pc, {git, "https://github.com/emqx/port_compiler.git", {tag, "v1.11.1"}}}
