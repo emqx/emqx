a42605b27ba1df12a2fa1f45109e2c488859cbed
DDDHuang <44492639+DDDHuang@users.noreply.github.com>
2021-06-25 09:18:52 +0800

    fix: statsd hocon schema & add test suit (#5060)

--- apps/emqx_statsd/src/emqx_statsd_schema.erl
+++ apps/emqx_statsd/src/emqx_statsd_schema.erl
@@ -10,29 +10,35 @@
 structs() -> ["emqx_statsd"].
 
 fields("emqx_statsd") ->
-    [ {server, fun server/1}
+    [ {host, fun host/1}
+    , {port, fun port/1}
     , {prefix, fun prefix/1}
     , {tags, map()}
     , {batch_size, fun batch_size/1}
     , {sample_time_interval, fun duration_s/1}
     , {flush_time_interval,  fun duration_s/1}].
 
-server(type) -> string();
-server(default) -> "192.168.1.1:8125";
-server(not_nullable) -> true;
-server(_) -> undefined.
+host(type) -> string();
+host(default) -> "127.0.0.1";
+host(nullable) -> false;
+host(_) -> undefined.
+
+port(type) -> integer();
+port(default) -> 8125;
+port(nullable) -> true;
+port(_) -> undefined.
 
 prefix(type) -> string();
 prefix(default) -> "emqx";
-prefix(not_nullable) -> false;
+prefix(nullable) -> true;
 prefix(_) -> undefined.
 
 batch_size(type) -> integer();
-batch_size(not_nullable) -> true;
+batch_size(nullable) -> false;
 batch_size(default) -> 10;
 batch_size(_) -> undefined.
 
 duration_s(type) -> emqx_schema:duration_s();
-duration_s(not_nullable) -> true;
+duration_s(nullable) -> false;
 duration_s(default) -> "10s";
 duration_s(_) -> undefined.
