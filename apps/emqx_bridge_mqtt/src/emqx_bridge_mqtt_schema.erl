658320a17d1419f9b1dbd6049b72dff20a1735a6
Turtle <deng@emqx.io>
2021-06-28 20:13:06 +0800

    feat(bridge-mqtt): Update the configuration file to hocon

---
+++ apps/emqx_bridge_mqtt/src/emqx_bridge_mqtt_schema.erl
@@ -0,0 +1,38 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2020-2021 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+-module(emqx_bridge_mqtt_schema).
+
+-include_lib("typerefl/include/types.hrl").
+
+-behaviour(hocon_schema).
+
+-export([ structs/0
+        , fields/1]).
+
+structs() -> ["emqx_bridge_mqtt"].
+
+fields("emqx_bridge_mqtt") ->
+    [ {mqtt, hoconsc:array("mqtt")}
+    , {rpc, hoconsc:array("rpc")}];
+
+fields("mqtt") ->
+    [ {name, emqx_schema:t(string(), undefined, true)}
+    , {address, emqx_schema:t(string(), undefined, true)}];
+
+fields("rpc") ->
+    [ {name, emqx_schema:t(string(), undefined, true)}
+    , {node, emqx_schema:t(string(), undefined, true)}].
