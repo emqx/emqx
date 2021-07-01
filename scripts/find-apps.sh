c094e5ddcce1f4c474f06b7d0a9892d3f7abdcef
zhanghongtong <rory-z@outlook.com>
2021-07-01 18:29:53 +0800

    chore(ct): cancel needless apps ct

--- scripts/find-apps.sh
+++ scripts/find-apps.sh
@@ -7,7 +7,7 @@ cd -P -- "$(dirname -- "$0")/.."
 
 find_app() {
     local appdir="$1"
-    find "${appdir}" -mindepth 1 -maxdepth 1 -type d
+    find "${appdir}" -mindepth 1 -maxdepth 1 -type d | grep -vE "emqx_exhook|emqx_exproto|emqx_lwm2m|emqx_sn|emqx_coap|emqx_stomp"
 }
 
 find_app 'apps'
