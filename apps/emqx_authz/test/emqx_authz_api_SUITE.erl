b088ef68ce0a01fdf54a2a0c9b862b0fa45006d1
DDDHuang <904897578@qq.com>
2021-07-07 15:57:08 +0800

    fix: other SUIT conf

--- apps/emqx_authz/test/emqx_authz_api_SUITE.erl
+++ apps/emqx_authz/test/emqx_authz_api_SUITE.erl
@@ -59,9 +59,8 @@ set_special_configs(emqx_authz) ->
     ok;
 
 set_special_configs(emqx_management) ->
-    emqx_config:put([emqx_management], #{listeners => [#{protocol => "http", port => 8081}],
-                                         default_application_id => <<"admin">>,
-                                         default_application_secret => <<"public">>}),
+    emqx_config:put([emqx_management], #{listeners => [#{protocol => http, port => 8081}],
+        applications =>[#{id => "admin", secret => "public"}]}),
     ok;
 
 set_special_configs(_App) ->
