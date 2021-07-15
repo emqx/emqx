emqx/emqttd

refactor: stats api & metrics api; fix: clients api parameter type lose

apps/emqx_management/src/emqx_mgmt_api_stats.erl
@@ -13,33 +13,104 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 %%--------------------------------------------------------------------
-
 -module(emqx_mgmt_api_stats).
 
--rest_api(#{name   => list_stats,
-            method => 'GET',
-            path   => "/stats/",
-            func   => list,
-            descr  => "A list of stats of all nodes in the cluster"}).
+-behavior(minirest_api).
 
--rest_api(#{name   => lookup_node_stats,
-            method => 'GET',
-            path   => "/nodes/:atom:node/stats/",
-            func   => lookup,
-            descr  => "A list of stats of a node"}).
+-export([api_spec/0]).
 
--export([ list/2
-        , lookup/2
-        ]).
+-export([get_stats/2]).
 
-%% List stats of all nodes
-list(Bindings, _Params) when map_size(Bindings) == 0 ->
-    emqx_mgmt:return({ok, [#{node => Node, stats => maps:from_list(Stats)}
-                              || {Node, Stats} <- emqx_mgmt:get_stats()]}).
+api_spec() ->
+    {stats_api(), stats_schema()}.
 
-%% List stats of a node
-lookup(#{node := Node}, _Params) ->
-    case emqx_mgmt:get_stats(Node) of
-        {error, Reason} -> emqx_mgmt:return({error, Reason});
-        Stats -> emqx_mgmt:return({ok, maps:from_list(Stats)})
-    end.
+stats_schema() ->
+    DefinitionName = <<"stats">>,
+    DefinitionProperties = #{
+        <<"connections.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current connections">>},
+        <<"connections.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of connections">>},
+        <<"channels.count">> => #{
+            type => <<"integer">>,
+            description => <<"sessions.count">>},
+        <<"channels.max">> => #{
+            type => <<"integer">>,
+            description => <<"session.max">>},
+        <<"sessions.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current sessions">>},
+        <<"sessions.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of sessions">>},
+        <<"topics.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current topics">>},
+        <<"topics.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of topics">>},
+        <<"suboptions.count">> => #{
+            type => <<"integer">>,
+            description => <<"subscriptions.count">>},
+        <<"suboptions.max">> => #{
+            type => <<"integer">>,
+            description => <<"subscriptions.max">>},
+        <<"subscribers.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current subscribers">>},
+        <<"subscribers.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of subscribers">>},
+        <<"subscriptions.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current subscriptions, including shared subscriptions">>},
+        <<"subscriptions.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of subscriptions">>},
+        <<"subscriptions.shared.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current shared subscriptions">>},
+        <<"subscriptions.shared.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of shared subscriptions">>},
+        <<"routes.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current routes">>},
+        <<"routes.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of routes">>},
+        <<"retained.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of currently retained messages">>},
+        <<"retained.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of retained messages">>}},
+    [{DefinitionName, DefinitionProperties}].
+
+stats_api() ->
+    Metadata = #{
+        get => #{
+            description => "EMQ X stats",
+            parameters => [#{
+                name => node,
+                in => query,
+                type => string,
+                required => false
+            }],
+            responses => #{
+                <<"200">> => #{
+                    schema => cowboy_swagger:schema(<<"stats">>)}}}},
+    [{"/stats", Metadata, get_stats}].
+
+%%%==============================================================================================
+%% parameters trans
+get_stats(get, _Request) ->
+    list(#{}).
+
+%%%==============================================================================================
+%% api apply
+list(_) ->
+    Response = emqx_json:encode(emqx_mgmt:get_stats()),
+    {200, Response}.

emqx/emqx

apps/emqx_management/src/emqx_mgmt_api_stats.erl
@@ -13,33 +13,93 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 %%--------------------------------------------------------------------
-
 -module(emqx_mgmt_api_stats).
 
--rest_api(#{name   => list_stats,
-            method => 'GET',
-            path   => "/stats/",
-            func   => list,
-            descr  => "A list of stats of all nodes in the cluster"}).
+-behavior(minirest_api).
 
--rest_api(#{name   => lookup_node_stats,
-            method => 'GET',
-            path   => "/nodes/:atom:node/stats/",
-            func   => lookup,
-            descr  => "A list of stats of a node"}).
+-export([api_spec/0]).
 
--export([ list/2
-        , lookup/2
-        ]).
+-export([list/2]).
 
-%% List stats of all nodes
-list(Bindings, _Params) when map_size(Bindings) == 0 ->
-    emqx_mgmt:return({ok, [#{node => Node, stats => maps:from_list(Stats)}
-                              || {Node, Stats} <- emqx_mgmt:get_stats()]}).
+api_spec() ->
+    {stats_api(), stats_schema()}.
 
-%% List stats of a node
-lookup(#{node := Node}, _Params) ->
-    case emqx_mgmt:get_stats(Node) of
-        {error, Reason} -> emqx_mgmt:return({error, Reason});
-        Stats -> emqx_mgmt:return({ok, maps:from_list(Stats)})
-    end.
+stats_schema() ->
+    DefinitionName = <<"stats">>,
+    DefinitionProperties = #{
+        <<"connections.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current connections">>},
+        <<"connections.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of connections">>},
+        <<"channels.count">> => #{
+            type => <<"integer">>,
+            description => <<"sessions.count">>},
+        <<"channels.max">> => #{
+            type => <<"integer">>,
+            description => <<"session.max">>},
+        <<"sessions.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current sessions">>},
+        <<"sessions.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of sessions">>},
+        <<"topics.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current topics">>},
+        <<"topics.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of topics">>},
+        <<"suboptions.count">> => #{
+            type => <<"integer">>,
+            description => <<"subscriptions.count">>},
+        <<"suboptions.max">> => #{
+            type => <<"integer">>,
+            description => <<"subscriptions.max">>},
+        <<"subscribers.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current subscribers">>},
+        <<"subscribers.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of subscribers">>},
+        <<"subscriptions.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current subscriptions, including shared subscriptions">>},
+        <<"subscriptions.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of subscriptions">>},
+        <<"subscriptions.shared.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current shared subscriptions">>},
+        <<"subscriptions.shared.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of shared subscriptions">>},
+        <<"routes.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of current routes">>},
+        <<"routes.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of routes">>},
+        <<"retained.count">> => #{
+            type => <<"integer">>,
+            description => <<"Number of currently retained messages">>},
+        <<"retained.max">> => #{
+            type => <<"integer">>,
+            description => <<"Historical maximum number of retained messages">>}},
+    [{DefinitionName, DefinitionProperties}].
+
+stats_api() ->
+    Metadata = #{
+        get => #{
+            description => "EMQ X stats",
+            responses => #{
+                <<"200">> => #{
+                    schema => cowboy_swagger:schema(<<"stats">>)}}}},
+    [{"/stats", Metadata, list}].
+
+%%%==============================================================================================
+%% api apply
+list(get, _Request) ->
+    Response = emqx_json:encode(emqx_mgmt:get_stats()),
+    {200, Response}.