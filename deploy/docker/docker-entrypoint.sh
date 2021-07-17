0eb3be7f840364291f66277991f1a6f319c0c3d0
Shawn <506895667@qq.com>
2021-07-17 13:46:37 +0800

    fix(docker): update docker entrypoint.sh

--- deploy/docker/docker-entrypoint.sh
+++ deploy/docker/docker-entrypoint.sh
@@ -42,52 +42,6 @@ if [[ -z "$EMQX_NODE_NAME" ]]; then
     export EMQX_NODE_NAME="$EMQX_NAME@$EMQX_HOST"
 fi
 
-# Set hosts to prevent cluster mode failed
-
-if [[ -z "$EMQX_NODE__PROCESS_LIMIT" ]]; then
-    export EMQX_NODE__PROCESS_LIMIT=2097152
-fi
-
-if [[ -z "$EMQX_NODE__MAX_PORTS" ]]; then
-    export EMQX_NODE__MAX_PORTS=1048576
-fi
-
-if [[ -z "$EMQX_NODE__MAX_ETS_TABLES" ]]; then
-    export EMQX_NODE__MAX_ETS_TABLES=2097152
-fi
-
-if [[ -z "$EMQX_LISTENER__TCP__EXTERNAL__ACCEPTORS" ]]; then
-    export EMQX_LISTENER__TCP__EXTERNAL__ACCEPTORS=64
-fi
-
-if [[ -z "$EMQX_LISTENER__TCP__EXTERNAL__MAX_CONNECTIONS" ]]; then
-    export EMQX_LISTENER__TCP__EXTERNAL__MAX_CONNECTIONS=1024000
-fi
-
-if [[ -z "$EMQX_LISTENER__SSL__EXTERNAL__ACCEPTORS" ]]; then
-    export EMQX_LISTENER__SSL__EXTERNAL__ACCEPTORS=32
-fi
-
-if [[ -z "$EMQX_LISTENER__SSL__EXTERNAL__MAX_CONNECTIONS" ]]; then
-    export EMQX_LISTENER__SSL__EXTERNAL__MAX_CONNECTIONS=102400
-fi
-
-if [[ -z "$EMQX_LISTENER__WS__EXTERNAL__ACCEPTORS" ]]; then
-    export EMQX_LISTENER__WS__EXTERNAL__ACCEPTORS=16
-fi
-
-if [[ -z "$EMQX_LISTENER__WS__EXTERNAL__MAX_CONNECTIONS" ]]; then
-    export EMQX_LISTENER__WS__EXTERNAL__MAX_CONNECTIONS=102400
-fi
-
-if [[ -z "$EMQX_LISTENER__WSS__EXTERNAL__ACCEPTORS" ]]; then
-    export EMQX_LISTENER__WSS__EXTERNAL__ACCEPTORS=16
-fi
-
-if [[ -z "$EMQX_LISTENER__WSS__EXTERNAL__MAX_CONNECTIONS" ]]; then
-    export EMQX_LISTENER__WSS__EXTERNAL__MAX_CONNECTIONS=102400
-fi
-
 # fill tuples on specific file
 # SYNOPSIS
 #     fill_tuples FILE [ELEMENTS ...]
