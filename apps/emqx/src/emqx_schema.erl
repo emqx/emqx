7521d09079556ea66b17e053215db11f4ae94a28
William Yang <mscame@gmail.com>
2021-07-20 14:06:23 +0200

    feat(quic): toggle QUIC listener and fix ci test

--- apps/emqx/src/emqx_schema.erl
+++ apps/emqx/src/emqx_schema.erl
@@ -1,3 +1,19 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2017-2021 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
 -module(emqx_schema).
 
 -dialyzer(no_return).
@@ -374,7 +390,8 @@ fields("mqtt_ws_listener") ->
     ] ++ mqtt_listener();
 
 fields("mqtt_quic_listener") ->
-    [ {"type", t(quic)}
+    [ {"enabled", t(boolean(), undefined, true)}
+    , {"type", t(quic)}
     , {"certfile", t(string(), undefined, undefined)}
     , {"keyfile", t(string(), undefined, undefined)}
     , {"ciphers", t(comma_separated_list(), undefined, "TLS_AES_256_GCM_SHA384,"
