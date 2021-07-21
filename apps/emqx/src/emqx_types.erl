aa40a6ba6c09e677be8d8bc420594034f76abbaa
JianBo He <heeejianbo@163.com>
2021-07-21 14:09:07 +0800

    chore(emqx): enrich version type

--- apps/emqx/src/emqx_types.erl
+++ apps/emqx/src/emqx_types.erl
@@ -94,7 +94,9 @@
 -type(ver() :: ?MQTT_PROTO_V3
              | ?MQTT_PROTO_V4
              | ?MQTT_PROTO_V5
-             | non_neg_integer()).
+             | non_neg_integer()
+             | binary() % For lwm2m, mqtt-sn...
+     ).
 
 -type(qos() :: ?QOS_0 | ?QOS_1 | ?QOS_2).
 -type(qos_name() :: qos0 | at_most_once |
