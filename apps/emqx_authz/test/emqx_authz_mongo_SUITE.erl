ac94dd0216c571949cd0f02e36d29330863f6108
Zaiming Shi <zmstone@gmail.com>
2021-07-30 10:08:36 +0200

    test(authz): no need to mock emqx_config

--- apps/emqx_authz/test/emqx_authz_mongo_SUITE.erl
+++ apps/emqx_authz/test/emqx_authz_mongo_SUITE.erl
@@ -29,11 +29,6 @@ groups() ->
     [].
 
 init_per_suite(Config) ->
-    %% important! let emqx_schema include the current app!
-    meck:new(emqx_schema, [non_strict, passthrough, no_history, no_link]),
-    meck:expect(emqx_schema, includes, fun() -> ["authorization"] end ),
-    meck:expect(emqx_schema, extra_schema_fields, fun(FieldName) -> emqx_authz_schema:fields(FieldName) end),
-
     meck:new(emqx_resource, [non_strict, passthrough, no_history, no_link]),
     meck:expect(emqx_resource, create, fun(_, _, _) -> {ok, meck_data} end),
 
@@ -56,7 +51,6 @@ init_per_suite(Config) ->
 
 end_per_suite(_Config) ->
     emqx_ct_helpers:stop_apps([emqx_authz, emqx_resource]),
-    meck:unload(emqx_schema),
     meck:unload(emqx_resource).
 
 -define(RULE1,[#{<<"topics">> => [<<"#">>],
