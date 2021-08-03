3eb07635170b2c71918d9aa7570e87aa02199090
Turtle <deng@emqx.io>
2021-08-03 16:48:19 +0800

    chore(dashboard): update get-dashboard script

--- scripts/get-dashboard.sh
+++ scripts/get-dashboard.sh
@@ -6,21 +6,10 @@ set -euo pipefail
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 
 RELEASE_ASSET_FILE="emqx-dashboard.zip"
-
-if [ -f 'EMQX_ENTERPRISE' ]; then
-    VERSION="${EMQX_EE_DASHBOARD_VERSION}"
-    DASHBOARD_PATH='lib-ee/emqx_dashboard/priv'
-    DASHBOARD_REPO='emqx-enterprise-dashboard-frontend-src'
-    AUTH="Authorization: token $(cat scripts/git-token)"
-    # have to be resolved with auth and redirect
-    DIRECT_DOWNLOAD_URL=""
-else
-    VERSION="${EMQX_CE_DASHBOARD_VERSION}"
-    DASHBOARD_PATH='apps/emqx_dashboard/priv'
-    DASHBOARD_REPO='emqx-dashboard-frontend'
-    AUTH=""
-    DIRECT_DOWNLOAD_URL="https://github.com/emqx/${DASHBOARD_REPO}/releases/download/${VERSION}/emqx-dashboard.zip"
-fi
+VERSION="${EMQX_DASHBOARD_VERSION}"
+DASHBOARD_PATH='apps/emqx_dashboard/priv'
+DASHBOARD_REPO='emqx-dashboard-web'
+DIRECT_DOWNLOAD_URL="https://github.com/emqx/${DASHBOARD_REPO}/releases/download/${VERSION}/${RELEASE_ASSET_FILE}"
 
 case $(uname) in
     *Darwin*) SED="sed -E";;
@@ -33,27 +22,6 @@ version() {
 
 if [ -d "$DASHBOARD_PATH/www" ] && [ "$(version)" = "$VERSION" ]; then
     exit 0
-fi
-
-find_url() {
-    # Get the download URL of our desired asset
-    release_url="https://api.github.com/repos/emqx/${DASHBOARD_REPO}/releases/tags/${VERSION}"
-    release_info="$(curl --silent --show-error --header "${AUTH}" --header "Accept: application/vnd.github.v3+json" "$release_url")"
-    if ! download_url="$(echo "$release_info" | jq --raw-output ".assets[] | select(.name==\"${RELEASE_ASSET_FILE}\").url" | tr -d '\n' | tr -d '\r')"; then
-        echo "failed to query $release_url"
-        echo "${release_info}"
-        exit 1
-    fi
-    # Get GitHub's S3 redirect URL
-    curl --silent --show-error \
-         --header "${AUTH}" \
-         --header "Accept: application/octet-stream" \
-         --write-out "%{redirect_url}" \
-         "$download_url"
-}
-
-if [ -z "$DIRECT_DOWNLOAD_URL" ]; then
-    DIRECT_DOWNLOAD_URL="$(find_url)"
 fi
 
 curl -L --silent --show-error \
