87051c0e533f7da5125e83080c2bd892fc7d0088
zhouzb <zhouzb@emqx.io>
2021-08-19 14:40:36 +0800

    test(authn): fix test case

--- apps/emqx/test/emqx_channel_SUITE.erl
+++ apps/emqx/test/emqx_channel_SUITE.erl
@@ -181,7 +181,7 @@ init_per_suite(Config) ->
     %% Access Control Meck
     ok = meck:new(emqx_access_control, [passthrough, no_history, no_link]),
     ok = meck:expect(emqx_access_control, authenticate,
-                     fun(_) -> ok end),
+                     fun(_) -> {ok, #{superuser => false}} end),
     ok = meck:expect(emqx_access_control, authorize, fun(_, _, _) -> allow end),
     %% Broker Meck
     ok = meck:new(emqx_broker, [passthrough, no_history, no_link]),
