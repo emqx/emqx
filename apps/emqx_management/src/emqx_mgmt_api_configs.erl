f761681e2a9ad2abd50d0bf3c0b70b75f01f6d4e
Shawn <506895667@qq.com>
2021-08-27 17:18:23 +0800

    fix(config): move config_reset under tags 'configs'

--- apps/emqx_management/src/emqx_mgmt_api_configs.erl
+++ apps/emqx_management/src/emqx_mgmt_api_configs.erl
@@ -89,6 +89,7 @@ config_api(ConfPath, Schema) ->
 config_reset_api() ->
     Metadata = #{
         post => #{
+            tags => [configs],
             description => <<"Reset the config entry specified by the query string parameter `conf_path`.<br/>
 - For a config entry that has default value, this resets it to the default value;
 - For a config entry that has no default value, an error 400 will be returned">>,
