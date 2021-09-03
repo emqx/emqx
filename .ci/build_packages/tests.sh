6e7d3d05e4ca4ba877d8a4aa40619c2bbe83b331
William Yang <mscame@gmail.com>
2021-09-03 09:50:25 +0200

    ci: install openssl11 for centos7 amd64 only

--- .ci/build_packages/tests.sh
+++ .ci/build_packages/tests.sh
@@ -91,8 +91,12 @@ emqx_test(){
             ;;
             "rpm")
                 packagename=$(basename "${PACKAGE_PATH}/${EMQX_NAME}"-*.rpm)
-                # EMQX OTP requires openssl11 to have TLS1.3 support
-                yum install -y openssl11
+
+                if [[ "${ARCH}" == "amd64" && $(rpm -E '%{rhel}') == 7 ]] ;
+                then
+                    # EMQX OTP requires openssl11 to have TLS1.3 support
+                    yum install -y openssl11;
+                fi
                 rpm -ivh "${PACKAGE_PATH}/${packagename}"
                 if ! rpm -q emqx | grep -q emqx; then
                     echo "package install error"
