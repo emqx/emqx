6352acaf44922a89b402e0cd3c9fc8447801f22f
Shawn <506895667@qq.com>
2021-09-10 09:21:06 +0800

    fix(bridges): do not start any bridge by default

--- apps/emqx_rule_engine/src/emqx_rule_runtime.erl
+++ apps/emqx_rule_engine/src/emqx_rule_runtime.erl
@@ -222,7 +222,6 @@ do_compare('<=', L, R) -> L =< R;
 do_compare('>=', L, R) -> L >= R;
 do_compare('<>', L, R) -> L /= R;
 do_compare('!=', L, R) -> L /= R;
-do_compare('~=', T, F) -> emqx_topic:match(T, F);
 do_compare('=~', T, F) -> emqx_topic:match(T, F).
 
 number(Bin) ->
