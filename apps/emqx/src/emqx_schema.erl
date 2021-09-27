d376c0f9fc344e76aec9d7f0a6f9d9caa0414eed
Zaiming Shi <zmstone@gmail.com>
2021-09-27 08:50:16 +0200

    refactor(schema): call emqx_tls_lib for default tls versions

--- apps/emqx/src/emqx_schema.erl
+++ apps/emqx/src/emqx_schema.erl
@@ -1103,7 +1103,7 @@ verification check."""
 default_tls_vsns(dtls_all_available) ->
     proplists:get_value(available_dtls, ssl:versions());
 default_tls_vsns(tls_all_available) ->
-    proplists:get_value(available, ssl:versions()).
+    emqx_tls_lib:default_versions().
 
 -spec ciphers_schema(quic | dtls_all_available | tls_all_available | undefined) -> hocon_schema:field_schema().
 ciphers_schema(Default) ->
