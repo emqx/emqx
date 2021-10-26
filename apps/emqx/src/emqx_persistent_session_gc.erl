e3dc9b3f4f88365ddeaa788af0b77a6bddbedda3
Tobias Lindahl <tobias@happihacking.com>
2021-10-26 09:53:49 +0200

    fix(persistent_sessions): start the message gc timer

--- apps/emqx/src/emqx_persistent_session_gc.erl
+++ apps/emqx/src/emqx_persistent_session_gc.erl
@@ -54,7 +54,7 @@ start_link() ->
 
 init([]) ->
     process_flag(trap_exit, true),
-    {ok, start_session_gc_timer(#{})}.
+    {ok, start_message_gc_timer(start_session_gc_timer(#{}))}.
 
 handle_call(_Request, _From, State) ->
     Reply = ok,
