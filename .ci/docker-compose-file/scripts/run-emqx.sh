f8fc67b313a10ee5eda16be2d97b67ac94cf7cb7
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-11-07 16:54:38 -0300

    fix(lag): target only replica if rlog core+replicant
    there seems to be race conditions related to some tests with sessions
    hitting the core and the replicant alternately and rlog.
    
    for intance, if there is some delay in this replication, a new
    connection made to the replica with a just-created session in the core
    may not have been replicated to the replicant, resulting in a test
    failure if it expects the session to be present.
    
    since such replication lags are inherent to the core-replicant
    topology, we can try to target only the replicant to avoid seeing this
    inconsistent view of the system during the tests.

--- .ci/docker-compose-file/scripts/run-emqx.sh
+++ .ci/docker-compose-file/scripts/run-emqx.sh
@@ -14,9 +14,25 @@ fi
   echo "EMQX_ZONES__DEFAULT__MQTT__MAX_TOPIC_ALIAS=10"
 } >> .ci/docker-compose-file/conf.cluster.env
 
-is_cluster_up() {
-  docker exec -i node1.emqx.io \
+is_node_up() {
+  local node
+  node="$1"
+  docker exec -i "$node" \
          bash -c "emqx eval \"['emqx@node1.emqx.io','emqx@node2.emqx.io'] = maps:get(running_nodes, ekka_cluster:info()).\"" > /dev/null 2>&1
+}
+
+is_node_listening() {
+  local node
+  node="$1"
+  docker exec -i "$node" \
+         emqx eval "ok = case gen_tcp:connect(\"localhost\", 1883, []) of {ok, P} -> gen_tcp:close(P), ok; _ -> exit(1) end." > /dev/null 2>&1
+}
+
+is_cluster_up() {
+  is_node_up node1.emqx.io && \
+    is_node_up node2.emqx.io && \
+    is_node_listening node1.emqx.io && \
+    is_node_listening node2.emqx.io
 }
 
 docker-compose \
