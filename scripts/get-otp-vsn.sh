b68f01e7e718b4f4db66f6cad2d795f75a9b90d7
Zaiming Shi <zmstone@gmail.com>
2021-11-15 23:42:19 +0100

    build: do not print 'otp' prefix for otp version

--- scripts/get-otp-vsn.sh
+++ scripts/get-otp-vsn.sh
@@ -2,6 +2,4 @@
 
 set -euo pipefail
 
-vsn=$(erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' -noshell)
-
-echo "otp${vsn}"
+erl  -noshell -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().'
