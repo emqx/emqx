dfa45c909a7618f6779df23be1bdfb02ada96930
k32 <10274441+k32@users.noreply.github.com>
2021-12-03 18:17:45 +0100

    fix(crash_dump): Fix hocon schema related to the erlang crash dump

--- apps/emqx_conf/src/emqx_conf_schema.erl
+++ apps/emqx_conf/src/emqx_conf_schema.erl
@@ -278,6 +278,22 @@ fields("node") ->
     , {"crash_dump_file",
        sc(file(),
           #{ mapping => "vm_args.-env ERL_CRASH_DUMP"
+           , desc => "Location of the crash dump file"
+           })}
+    , {"crash_dump_seconds",
+       sc(emqx_schema:duration_s(),
+          #{ mapping => "vm_args.-env ERL_CRASH_DUMP_SECONDS"
+           , default => "30s"
+           , desc => """
+The number of seconds that the broker is allowed to spend writing
+a crash dump
+"""
+           })}
+    , {"crash_dump_bytes",
+       sc(emqx_schema:bytesize(),
+          #{ mapping => "vm_args.-env ERL_CRASH_DUMP_BYTES"
+           , default => "100MB"
+           , desc => "The maximum size of a crash dump file in bytes."
            })}
     , {"dist_net_ticktime",
        sc(emqx_schema:duration(),
