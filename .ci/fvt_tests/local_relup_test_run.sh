5c8b37e4b0cf8be8d6547c456367803b441af2dc
zhouzb <zhouzb@emqx.io>
2021-12-09 09:37:02 +0800

    build: update otp version

--- .ci/fvt_tests/local_relup_test_run.sh
+++ .ci/fvt_tests/local_relup_test_run.sh
@@ -15,8 +15,8 @@ PROFILE="$1"
 VSN="$2"
 OLD_VSN="$3"
 PACKAGE_PATH="$4"
-FROM_OTP_VSN="${5:-23.3.4.9-3}"
-TO_OTP_VSN="${6:-23.3.4.9-3}"
+FROM_OTP_VSN="${5:-24.1.5-3}"
+TO_OTP_VSN="${6:-24.1.5-3}"
 
 TEMPDIR=$(mktemp -d)
 trap '{ rm -rf -- "$TEMPDIR"; }' EXIT
