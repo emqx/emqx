fe444bf134d2d4b92cebca758de4f090b897ff09
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-12-28 16:49:24 -0300

    chore(shellcheck): use `git grep` to search only tracked files

--- scripts/shellcheck.sh
+++ scripts/shellcheck.sh
@@ -6,11 +6,7 @@ target_files=()
 while IFS='' read -r line;
 do
   target_files+=("$line");
-done < <(grep -r -l \
-              --exclude-dir=.git \
-              --exclude-dir=_build \
-              --exclude-dir=deps \
-              "^#!/bin/" .)
+done < <(git grep -r -l "^#!/bin/" .)
 return_code=0
 for i in "${target_files[@]}"; do
   echo checking "$i" ...
