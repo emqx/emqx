67ba3a06ea90ab3ad9d0a55b202e65bf89266554
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-01-05 20:41:45 +0100

    chore(gateway): update copyright

--- apps/emqx_gateway/src/lwm2m/emqx_lwm2m_cmd.erl
+++ apps/emqx_gateway/src/lwm2m/emqx_lwm2m_cmd.erl
@@ -1,5 +1,5 @@
 %%--------------------------------------------------------------------
-%% Copyright (c) 2016-2022 EMQ Enterprise, Inc. (http://emqtt.io)
+%% Copyright (c) 2016-2022 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
