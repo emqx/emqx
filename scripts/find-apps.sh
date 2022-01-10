b2684b995d996f4b5b17989355d6f3fd3e5a9432
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-01-10 07:52:18 +0100

    build: doc for win

--- scripts/find-apps.sh
+++ scripts/find-apps.sh
@@ -5,9 +5,16 @@ set -euo pipefail
 # ensure dir
 cd -P -- "$(dirname -- "$0")/.."
 
+if [ "$(./scripts/get-distro.sh)" = 'windows' ]; then
+    # Otherwise windows may resolve to find.exe
+    FIND="/usr/bin/find"
+else
+    FIND='find'
+fi
+
 find_app() {
     local appdir="$1"
-    find "${appdir}" -mindepth 1 -maxdepth 1 -type d
+    "$FIND" "${appdir}" -mindepth 1 -maxdepth 1 -type d
 }
 
 find_app 'apps'
