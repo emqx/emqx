6b020b4a019ed630d7b126c9d2712b2dbf7bb949
DDDHuang <44492639+DDDHuang@users.noreply.github.com>
2022-01-25 10:53:21 +0800

    fix(api): query client subscriptions from cluster empty check

--- apps/emqx_management/src/emqx_mgmt.erl
+++ apps/emqx_management/src/emqx_mgmt.erl
@@ -269,10 +269,14 @@ list_authz_cache(ClientId) ->
 
 list_client_subscriptions(ClientId) ->
     Results = [client_subscriptions(Node, ClientId) || Node <- mria_mnesia:running_nodes()],
-    Expected = lists:filter(fun({error, _}) -> false;
-                               ([]) -> false;
-                               (_) -> true
-                            end, Results),
+    Filter =
+        fun
+            ({error, _}) ->
+                false;
+            ({_Node, List}) ->
+                erlang:is_list(List) andalso 0 < erlang:length(List)
+        end,
+    Expected = lists:filter(Filter, Results),
     case Expected of
         [] -> [];
         [Result | _] -> Result
