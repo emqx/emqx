f34e87daeb0b3c76968bd641162440a0b640d99e
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-01-26 14:19:39 -0300

    chore(ekka): update ekka -> 0.11.3
    This ekka tag contains the core node discovery callback feature.

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule EMQXUmbrella.MixProject do
       {:cowboy, github: "emqx/cowboy", tag: "2.9.0", override: true},
       {:esockd, github: "emqx/esockd", tag: "5.9.0", override: true},
       {:mria, github: "emqx/mria", tag: "0.1.5", override: true},
-      {:ekka, github: "emqx/ekka", tag: "0.11.2", override: true},
+      {:ekka, github: "emqx/ekka", tag: "0.11.3", override: true},
       {:gen_rpc, github: "emqx/gen_rpc", tag: "2.8.0", override: true},
       {:minirest, github: "emqx/minirest", tag: "1.2.10", override: true},
       {:ecpool, github: "emqx/ecpool", tag: "0.5.2"},
