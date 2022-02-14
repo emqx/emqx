22ae8985c62db03554e4687b06c06ce8bcca7c5a
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-02-14 19:32:59 +0100

    chore: compare to rel-* versions for appup

--- scripts/apps-version-check.sh
+++ scripts/apps-version-check.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-latest_release=$(git describe --abbrev=0 --tags)
+## compare to the latest release version tag:
+##   match rel-e4.4.0, v4.4.*, or e4.4.* tags
+##   but do not include alpha, beta and rc versions
+##
+## NOTE: 'rel-' tags are the merge base of enterprise release in opensource repo.
+## i.e. if we are to release a new enterprise without cutting a new opensource release
+## we should tag rel-e4.4.X in the opensource repo, and merge this tag to enterprise
+## then cut a release from the enterprise repo.
+latest_release="$(git describe --abbrev=0 --tags --match 'rel-e4.4.*' --match '[v|e]4.4*' --exclude '*beta*' --exclude '*alpha*' --exclude '*rc*')"
 echo "Compare base: $latest_release"
 
 bad_app_count=0
