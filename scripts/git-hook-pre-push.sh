cc1734490f9e90be4989eaed2340857c74134401
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-02-17 11:20:19 +0100

    chore: trigger a version check before pushing a tag

--- scripts/git-hook-pre-push.sh
+++ scripts/git-hook-pre-push.sh
@@ -4,9 +4,13 @@ set -euo pipefail
 
 url="$2"
 
+## ensure enterprise code is not pushed to public repo
 if [ -f 'EMQX_ENTERPRISE' ]; then
     if [[ "$url" != *emqx-enterprise* ]]; then
         echo "$(tput setaf 1)error: enterprise_code_to_non_enterprise_repo"
         exit 1
     fi
 fi
+
+## this triggers a tag vs release version check before pushing a tag
+./pkg-vsn.sh
