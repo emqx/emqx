3690d1951ee5ccf79445f37f25a47805af24ff65
Shawn <506895667@qq.com>
2022-02-18 22:41:03 +0800

    chore: add more debug info for relup-base-packages.sh

--- scripts/relup-base-packages.sh
+++ scripts/relup-base-packages.sh
@@ -56,13 +56,15 @@ pushd _upgrade_base
 for tag in $(../scripts/relup-base-vsns.sh $EDITION | xargs echo -n); do
     filename="$PROFILE-$SYSTEM-${tag#[e|v]}-$ARCH.zip"
     url="https://www.emqx.com/downloads/$DIR/$tag/$filename"
-    echo "downloading ${filename} ..."
+    echo "downloading base package from ${url} ..."
     if [ ! -f "$filename" ] && curl -I -m 10 -o /dev/null -s -w "%{http_code}" "${url}" | grep -q -oE "^[23]+" ; then
         curl -L -o "${filename}" "${url}"
         if [ "$SYSTEM" != "centos6" ]; then
             curl -L -o "${filename}.sha256" "${url}.sha256"
+            SUMSTR=$(cat "${filename}.sha256")
+            echo "got sha265sum: ${SUMSTR}"
             ## https://askubuntu.com/questions/1202208/checking-sha256-checksum
-            echo "$(cat "${filename}.sha256")  ${filename}" | $SHASUM -c || exit 1
+            echo "${SUMSTR}  ${filename}" | $SHASUM -c || exit 1
         fi
     fi
 done
