3bd961ca732a2164b92b5c367c8c0e5183b3c1ed
Shawn <506895667@qq.com>
2022-02-18 23:05:35 +0800

    fix(CI): remove e|v from downloading urls of emqx packages

--- scripts/relup-base-packages.sh
+++ scripts/relup-base-packages.sh
@@ -55,7 +55,7 @@ pushd _upgrade_base
 
 for tag in $(../scripts/relup-base-vsns.sh $EDITION | xargs echo -n); do
     filename="$PROFILE-$SYSTEM-${tag#[e|v]}-$ARCH.zip"
-    url="https://www.emqx.com/downloads/$DIR/$tag/$filename"
+    url="https://www.emqx.com/downloads/$DIR/${tag#[e|v]}/$filename"
     echo "downloading base package from ${url} ..."
     if [ ! -f "$filename" ] && curl -I -m 10 -o /dev/null -s -w "%{http_code}" "${url}" | grep -q -oE "^[23]+" ; then
         curl -L -o "${filename}" "${url}"
