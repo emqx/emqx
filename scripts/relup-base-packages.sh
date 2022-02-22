18feec9030595bbd0bfceaa40706803cf8fe001d
JianBo He <heeejianbo@gmail.com>
2022-02-22 13:43:46 +0800

    chore(ci): fix the download filename

--- scripts/relup-base-packages.sh
+++ scripts/relup-base-packages.sh
@@ -54,7 +54,7 @@ mkdir -p _upgrade_base
 pushd _upgrade_base
 
 for tag in $(../scripts/relup-base-vsns.sh $EDITION | xargs echo -n); do
-    filename="$PROFILE-$SYSTEM-${tag#[e|v]}-$ARCH.zip"
+    filename="$PROFILE-${tag#[e|v]}-otp$OTP_VSN-$SYSTEM-$ARCH.zip"
     url="https://www.emqx.com/downloads/$DIR/${tag#[e|v]}/$filename"
     echo "downloading base package from ${url} ..."
     if [ ! -f "$filename" ] && curl -I -m 10 -o /dev/null -s -w "%{http_code}" "${url}" | grep -q -oE "^[23]+" ; then
