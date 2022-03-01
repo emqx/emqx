7698ad7c4b32701e8e3b3a25435d8d7d9bb135a1
Shawn <506895667@qq.com>
2022-03-01 17:15:19 +0800

    chore(relup): print more info after release upgrade complete

--- src/emqx_relup.erl
+++ src/emqx_relup.erl
@@ -4,26 +4,31 @@
         , post_release_downgrade/2
         ]).
 
+-define(INFO(FORMAT), io:format("[emqx_relup] " ++ FORMAT ++ "~n")).
+-define(INFO(FORMAT, ARGS), io:format("[emqx_relup] " ++ FORMAT ++ "~n", ARGS)).
+
 %% what to do after upgraded from a old release vsn.
 post_release_upgrade(_FromRelVsn, _) ->
+    ?INFO("emqx has been upgraded to ~s", [emqx_app:get_release()]),
     reload_components().
 
 %% what to do after downgraded to a old release vsn.
 post_release_downgrade(_ToRelVsn, _) ->
+    ?INFO("emqx has been downgrade to ~s", [emqx_app:get_release()]),
     reload_components().
 
 -ifdef(EMQX_ENTERPRISE).
 reload_components() ->
-    io:format("reloading resource providers ...~n"),
+    ?INFO("reloading resource providers ..."),
     emqx_rule_engine:load_providers(),
-    io:format("reloading module providers ...~n"),
+    ?INFO("reloading module providers ..."),
     emqx_modules:load_providers(),
-    io:format("loading plugins ...~n"),
+    ?INFO("loading plugins ..."),
     emqx_plugins:load().
 -else.
 reload_components() ->
-    io:format("reloading resource providers ...~n"),
+    ?INFO("reloading resource providers ..."),
     emqx_rule_engine:load_providers(),
-    io:format("loading plugins ...~n"),
+    ?INFO("loading plugins ..."),
     emqx_plugins:load().
 -endif.
