a125bf9469cb97f2d15c01d46ac777d450c63531
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-03-25 16:55:59 +0100

    style: add emqx_enterprise_conf erlfmt commit to git blame ignore

--- scripts/check-format.sh
+++ scripts/check-format.sh
@@ -9,7 +9,7 @@ cd -P -- "$(dirname -- "$0")/.."
 
 APPS=()
 APPS+=( 'apps/emqx' 'apps/emqx_modules' )
-APPS+=( 'lib-ee/emqx_license' )
+APPS+=( 'lib-ee/emqx_enterprise_conf' 'lib-ee/emqx_license' )
 
 for app in "${APPS[@]}"; do
     echo "$app ..."
