8d46332b300208f11d400e355fa4c52bc4144388
Zhongwen Deng <zhongwencool@gmail.com>
2022-03-25 14:18:13 +0800

    chore: upgrade hocon to 0.26.4 to limit HOCON_MAP's key format

--- apps/emqx_dashboard/src/emqx_dashboard_monitor.erl
+++ apps/emqx_dashboard/src/emqx_dashboard_monitor.erl
@@ -243,8 +243,8 @@ format(TimeStamp, Data, All) ->
     [Data#{time_stamp => TimeStamp} | All].
 
 cal_rate(_Now, undefined) ->
-    AllSamples = ?GAUGE_SAMPLER_LIST ++ maps:keys(?DELTA_SAMPLER_RATE_MAP),
-    {ok, lists:foldl(fun(Key, Acc) -> Acc#{Key => 0} end, #{}, AllSamples)};
+    AllSamples = ?GAUGE_SAMPLER_LIST ++ maps:values(?DELTA_SAMPLER_RATE_MAP),
+    lists:foldl(fun(Key, Acc) -> Acc#{Key => 0} end, #{}, AllSamples);
 cal_rate( #emqx_monit{data = NowData, time = NowTime}
         , #emqx_monit{data = LastData, time = LastTime}) ->
     TimeDelta = NowTime - LastTime,
