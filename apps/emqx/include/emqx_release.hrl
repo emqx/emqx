098ba913512ce3251c978168d3bd1eec04ab1dde
zhouzb <zhouzb@emqx.io>
2022-03-25 19:43:22 +0800

    chore: bump release vsn

--- apps/emqx/include/emqx_release.hrl
+++ apps/emqx/include/emqx_release.hrl
@@ -29,7 +29,7 @@
 %% hyphen.
 
 %% Community edition
--define(EMQX_RELEASE_CE, "5.0.0-beta.4").
+-define(EMQX_RELEASE_CE, "5.0.0-rc.1").
 
 %% Enterprise edition
--define(EMQX_RELEASE_EE, "5.0.0-alpha.1").
+-define(EMQX_RELEASE_EE, "5.0.0-alpha.2").
