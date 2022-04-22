625223c2b5cd1686b410838d7b73cf701c636bac
EMQ-YangM <yangm@emqx.io>
2022-04-22 11:07:50 +0800

    test: fix redis_type

--- apps/emqx_authz/test/emqx_authz_api_sources_SUITE.erl
+++ apps/emqx_authz/test/emqx_authz_api_sources_SUITE.erl
@@ -77,7 +77,7 @@
     <<"type">> => <<"redis">>,
     <<"enable">> => true,
     <<"servers">> => <<?REDIS_SINGLE_HOST, ",127.0.0.1:6380">>,
-    <<"redis_type">> => <<"single">>,
+    <<"redis_type">> => <<"cluster">>,
     <<"pool_size">> => 1,
     <<"database">> => 0,
     <<"password">> => <<"ee">>,
