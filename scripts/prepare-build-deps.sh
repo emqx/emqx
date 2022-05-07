fc645f8d3e2faa98926ec67c7322f79af8d4843e
Shawn <506895667@qq.com>
2022-05-07 23:44:36 +0800

    fix: please the spell checker

--- scripts/prepare-build-deps.sh
+++ scripts/prepare-build-deps.sh
@@ -11,7 +11,7 @@ dependency_missing() {
     echo "error: $1 is not found in the system"
     if [ "${AUTO_INSTALL_BUILD_DEPS}" = "1" ]; then
         echo "brew install $1"
-        brew install $1
+        brew install "$1"
     else
         echo "You can install it by running:"
         echo "  brew install $1"
@@ -23,14 +23,14 @@ prepare_mac_osx() {
     current_packages=$(brew list)
     for package in ${required_packages_mac_osx}
     do
-        if ! echo ${current_packages} | grep -q "${package}"; then
-            dependency_missing ${package}
+        if ! echo "${current_packages}" | grep -q "${package}"; then
+            dependency_missing "${package}"
         fi
     done
     for cmd in ${required_cmds_mac_osx}
     do
         if ! command -v "${cmd}" &> /dev/null; then
-            dependency_missing ${cmd}
+            dependency_missing "${cmd}"
         fi
     done
 }
