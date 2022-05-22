86508429946562c2324952df70c502e28db72f5c
Shawn <506895667@qq.com>
2022-05-22 14:14:02 +0800

    fix: pin jq v0.3.2

--- mix.exs
+++ mix.exs
@@ -611,7 +611,7 @@ defmodule EMQXUmbrella.MixProject do
 
   defp jq_dep() do
     if enable_jq?(),
-      do: [{:jq, github: "emqx/jq", tag: "v0.3.1", override: true}],
+      do: [{:jq, github: "emqx/jq", tag: "v0.3.2", override: true}],
       else: []
   end
 
