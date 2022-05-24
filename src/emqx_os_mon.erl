17b9baf509ab1908249e846b02287b6342066ed6
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-05-24 18:56:28 +0200

    fix(alarm): fix false sysmem usage alarm
    Use information from lc app to trigger sysmem alarm set/clear actions

--- src/emqx_os_mon.erl
+++ src/emqx_os_mon.erl
@@ -78,32 +78,30 @@ set_cpu_low_watermark(Float) ->
     call({set_cpu_low_watermark, Float}).
 
 get_mem_check_interval() ->
-    memsup:get_check_interval() div 1000.
+    call(?FUNCTION_NAME).
 
-set_mem_check_interval(Seconds) when Seconds < 60 ->
-    memsup:set_check_interval(1);
 set_mem_check_interval(Seconds) ->
-    memsup:set_check_interval(Seconds div 60).
+    call({?FUNCTION_NAME, Seconds}).
 
 get_sysmem_high_watermark() ->
-    memsup:get_sysmem_high_watermark().
+    call(?FUNCTION_NAME).
 
-set_sysmem_high_watermark(Float) ->
-    V = Float/100,
+set_sysmem_high_watermark(HW) ->
+    V = resolve_watermark(HW),
     case load_ctl:get_config() of
         #{ ?MEM_MON_F0 := true } = OldLC ->
             ok = load_ctl:put_config(OldLC#{ ?MEM_MON_F0 => true
-                                           , ?MEM_MON_F1 => V});
+                                           , ?MEM_MON_F1 => V / 100});
         _ ->
             skip
     end,
-    memsup:set_sysmem_high_watermark(V).
+    gen_server:call(?OS_MON, {?FUNCTION_NAME, V}, infinity).
 
 get_procmem_high_watermark() ->
     memsup:get_procmem_high_watermark().
 
-set_procmem_high_watermark(Float) ->
-    memsup:set_procmem_high_watermark(Float / 100).
+set_procmem_high_watermark(HW) ->
+    memsup:set_procmem_high_watermark(resolve_watermark(HW) / 100).
 
 call(Req) ->
     gen_server:call(?OS_MON, Req, infinity).
@@ -113,16 +111,38 @@ call(Req) ->
 %%--------------------------------------------------------------------
 
 init([Opts]) ->
-    set_mem_check_interval(proplists:get_value(mem_check_interval, Opts)),
-    SysHW = proplists:get_value(sysmem_high_watermark, Opts),
-    set_sysmem_high_watermark(SysHW),
+    process_flag(trap_exit, true),
+    %% make sure memsup will not emit system memory alarms
+    memsup:set_sysmem_high_watermark(1),
     set_procmem_high_watermark(proplists:get_value(procmem_high_watermark, Opts)),
-    ensure_system_memory_alarm(SysHW),
-    {ok, ensure_check_timer(#{cpu_high_watermark => proplists:get_value(cpu_high_watermark, Opts),
+    MemCehckInterval = do_resolve_mem_check_interval(proplists:get_value(mem_check_interval, Opts)),
+    SysHW = resolve_watermark(proplists:get_value(sysmem_high_watermark, Opts)),
+    St = ensure_check_timer(#{cpu_high_watermark => proplists:get_value(cpu_high_watermark, Opts),
                               cpu_low_watermark => proplists:get_value(cpu_low_watermark, Opts),
                               cpu_check_interval => proplists:get_value(cpu_check_interval, Opts),
-                              timer => undefined})}.
+                              sysmem_high_watermark => SysHW,
+                              mem_check_interval => MemCehckInterval,
+                              timer => undefined}),
+    ok = do_set_mem_check_interval(MemCehckInterval),
+    %% update immediately after start/restart
+    ok = update_mem_alarm_status(SysHW),
+    {ok, ensure_mem_check_timer(St)}.
 
+handle_call(get_sysmem_high_watermark, _From, State) ->
+    #{sysmem_high_watermark := SysHW} = State,
+    {reply, maybe_round(SysHW), State};
+handle_call(get_mem_check_interval, _From, State) ->
+    #{mem_check_interval := Interval} = State,
+    {reply, Interval, State};
+handle_call({set_sysmem_high_watermark, SysHW}, _From, State) ->
+    %% update immediately after start/restart
+    ok = update_mem_alarm_status(SysHW),
+    {reply, ok, State#{sysmem_high_watermark => SysHW}};
+handle_call({set_mem_check_interval, Seconds0}, _From, State) ->
+    Seconds = do_resolve_mem_check_interval(Seconds0),
+    ok = do_set_mem_check_interval(Seconds),
+    %% will start taking effect when the current timer expires
+    {reply, ok, State#{mem_check_interval => Seconds}};
 handle_call(get_cpu_check_interval, _From, State) ->
     {reply, maps:get(cpu_check_interval, State, undefined), State};
 
@@ -145,6 +165,8 @@ handle_call(Req, _From, State) ->
     ?LOG(error, "Unexpected call: ~p", [Req]),
     {reply, ignored, State}.
 
+handle_cast(upgrade, State) ->
+    {noreply, State};
 handle_cast(Msg, State) ->
     ?LOG(error, "Unexpected cast: ~p", [Msg]),
     {noreply, State}.
@@ -168,16 +190,28 @@ handle_info({timeout, Timer, check}, State = #{timer := Timer,
             ensure_check_timer(State)
     end,
     {noreply, NState};
-
+handle_info({timeout, Timer, check_mem}, #{mem_check_timer := Timer,
+                                           sysmem_high_watermark := SysHW
+                                          } = State) ->
+    ok = update_mem_alarm_status(SysHW),
+    NState = ensure_mem_check_timer(State#{mem_check_timer := undefined}),
+    {noreply, NState};
 handle_info(Info, State) ->
     ?LOG(error, "unexpected info: ~p", [Info]),
     {noreply, State}.
 
-terminate(_Reason, #{timer := Timer}) ->
+terminate(_Reason, #{timer := Timer} = St) ->
+    emqx_misc:cancel_timer(maps:get(mem_check_timer, St, undefined)),
     emqx_misc:cancel_timer(Timer).
 
 code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+    %% NOTE: downgrade is not handled as the extra fields added to State
+    %% does not affect old version code.
+    %% The only thing which may slip through is that a started timer
+    %% will result in a "unexpected info" error log for the old version code
+    NewState = ensure_mem_check_timer(State),
+    SysHW = resolve_sysmem_high_watermark(State),
+    {ok, NewState#{sysmem_high_watermark => SysHW}}.
 
 %%--------------------------------------------------------------------
 %% Internal functions
@@ -189,19 +223,93 @@ ensure_check_timer(State = #{cpu_check_interval := Interval}) ->
         _ -> State#{timer := emqx_misc:start_timer(timer:seconds(Interval), check)}
     end.
 
-%% At startup, memsup starts first and checks for memory alarms,
-%% but emqx_alarm_handler is not yet used instead of alarm_handler,
-%% so alarm_handler is used directly for notification (normally emqx_alarm_handler should be used).
-%%The internal memsup will no longer trigger events that have been alerted,
-%% and there is no exported function to remove the alerted flag,
-%% so it can only be checked again at startup.
-ensure_system_memory_alarm(HW) ->
-    case erlang:whereis(memsup) of
-        undefined -> ok;
-        _Pid ->
-            {Total, Allocated, _Worst} = memsup:get_memory_data(),
-            case Total =/= 0 andalso Allocated/Total * 100 > HW of
-                true -> emqx_alarm:activate(high_system_memory_usage, #{high_watermark => HW});
-                false -> ok
-            end
+ensure_mem_check_timer(#{mem_check_timer := Ref} = State) when is_reference(Ref) ->
+    %% timer already started
+    State;
+ensure_mem_check_timer(State) ->
+    Interval = resolve_mem_check_interval(State),
+    case is_sysmem_check_supported() of
+        true ->
+            State#{mem_check_timer => emqx_misc:start_timer(timer:seconds(Interval), check_mem),
+                   mem_check_interval => Interval
+                  };
+        false ->
+            State
+    end.
+
+resolve_mem_check_interval(#{mem_check_interval := Seconds}) when is_integer(Seconds) ->
+    Seconds;
+resolve_mem_check_interval(_) ->
+    %% this only happens when hot-upgrade from older version (< 4.3.14, or < 4.4.4)
+    try
+        %% memsup has interval set API using minutes, but returns in milliseconds from get API
+        IntervalMs = memsup:get_check_interval(),
+        true = (IntervalMs > 1000),
+        IntervalMs div 1000
+    catch
+        _ : _ ->
+            %% this is the memsup default
+            60
+    end.
+
+is_sysmem_check_supported() ->
+    %% sorry Mac and Windows, for now
+    {unix, linux} =:= os:type().
+
+%% we still need to set memsup interval for process (not system) memory check
+do_set_mem_check_interval(Seconds) ->
+    Minutes = Seconds div 60,
+    _ = memsup:set_check_interval(Minutes),
+    ok.
+
+%% keep the time unit alignment with memsup, minmum interval is 60 seconds.
+do_resolve_mem_check_interval(Seconds) ->
+    case is_integer(Seconds) andalso Seconds >= 60 of
+        true -> Seconds;
+        false -> 60
+    end.
+
+resolve_sysmem_high_watermark(#{sysmem_high_watermark := SysHW}) -> SysHW;
+resolve_sysmem_high_watermark(_) ->
+    %% sysmem_high_watermark is not found in state map
+    %% get it from memsup
+    resolve_watermark(memsup:get_sysmem_high_watermark()).
+
+resolve_watermark(W) when W > 0 andalso W =< 1 ->
+    W * 100;
+resolve_watermark(W) when W > 0 andalso W =< 100 ->
+    W.
+
+update_mem_alarm_status(SysHW) ->
+    Usage = current_sysmem_percent(),
+    case Usage > SysHW of
+        true ->
+            _ = emqx_alarm:activate(
+                high_system_memory_usage,
+                #{
+                    usage => Usage,
+                    high_watermark => SysHW
+                }
+            );
+        _ ->
+            ok = emqx_alarm:ensure_deactivated(
+                high_system_memory_usage,
+                #{
+                    usage => Usage,
+                    high_watermark => SysHW
+                }
+            )
+    end,
+    ok.
+
+current_sysmem_percent() ->
+    Ratio = load_ctl:get_memory_usage(),
+    erlang:floor(Ratio * 10000) / 100.
+
+maybe_round(X) when is_integer(X) -> X;
+maybe_round(X) when is_float(X) ->
+    R = erlang:round(X),
+    case erlang:abs(X - R) > 1.0e-6 of
+        true -> X;
+        false -> R
     end.
