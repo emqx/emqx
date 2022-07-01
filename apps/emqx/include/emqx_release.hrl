264cf036c75dfd2b2ce312846b8e67d254f92e2a
Shawn <506895667@qq.com>
2022-07-01 11:12:08 +0800

    chore: release 5.0.1-beta.1

--- apps/emqx/include/emqx_release.hrl
+++ apps/emqx/include/emqx_release.hrl
@@ -32,7 +32,7 @@
 %% `apps/emqx/src/bpapi/README.md'
 
 %% Community edition
--define(EMQX_RELEASE_CE, "5.0.1").
+-define(EMQX_RELEASE_CE, "5.0.1-beta.1").
 
 %% Enterprise edition
 -define(EMQX_RELEASE_EE, "5.0.0-alpha.1").
