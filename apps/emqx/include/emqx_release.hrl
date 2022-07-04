fa89b633d80668c7729d2c92efe94e95d1975c19
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-07-04 23:23:20 +0200

    chore: bump version to 5.0.3

--- apps/emqx/include/emqx_release.hrl
+++ apps/emqx/include/emqx_release.hrl
@@ -32,7 +32,7 @@
 %% `apps/emqx/src/bpapi/README.md'
 
 %% Community edition
--define(EMQX_RELEASE_CE, "5.0.2").
+-define(EMQX_RELEASE_CE, "5.0.3").
 
 %% Enterprise edition
 -define(EMQX_RELEASE_EE, "5.0.0-alpha.1").
