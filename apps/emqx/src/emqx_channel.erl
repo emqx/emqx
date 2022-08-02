cffaf95d00a39908fa4687afa4e21cd08cb86949
JianBo He <heeejianbo@163.com>
2022-08-02 17:20:58 +0800

    chore: update apps/emqx/src/emqx_channel.erl

--- apps/emqx/src/emqx_channel.erl
+++ apps/emqx/src/emqx_channel.erl
@@ -1,4 +1,4 @@
-%--------------------------------------------------------------------
+%%--------------------------------------------------------------------
 %% Copyright (c) 2019-2022 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
