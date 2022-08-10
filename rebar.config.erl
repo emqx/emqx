dd5eea5ada7fd1ec5603ca9fd73f90a7426523bd
William Yang <mscame@gmail.com>
2022-08-10 14:34:35 +0200

    build: bump relup helper to 2.1.0

--- rebar.config.erl
+++ rebar.config.erl
@@ -88,7 +88,7 @@ project_app_dirs() ->
     ["apps/*", alternative_lib_dir() ++ "/*", "."].
 
 plugins(HasElixir) ->
-    [ {relup_helper,{git,"https://github.com/emqx/relup_helper", {tag, "2.0.0"}}}
+    [ {relup_helper,{git,"https://github.com/emqx/relup_helper", {tag, "2.1.0"}}}
     , {er_coap_client, {git, "https://github.com/emqx/er_coap_client", {tag, "v1.0"}}}
       %% emqx main project does not require port-compiler
       %% pin at root level for deterministic
