ba1f5eecd32028c426b055bc4a211d0f2f5f381b
Shawn <506895667@qq.com>
2022-08-31 11:14:36 +0800

    fix: update the swagger for new resource metrics

--- lib-ee/emqx_ee_bridge/src/emqx_ee_bridge_mysql.erl
+++ lib-ee/emqx_ee_bridge/src/emqx_ee_bridge_mysql.erl
@@ -5,7 +5,7 @@
 
 -include_lib("typerefl/include/types.hrl").
 -include_lib("hocon/include/hoconsc.hrl").
--include("emqx_ee_bridge.hrl").
+-include_lib("emqx_bridge/include/emqx_bridge.hrl").
 -include_lib("emqx_resource/include/emqx_resource.hrl").
 
 -import(hoconsc, [mk/2, enum/1, ref/2]).
