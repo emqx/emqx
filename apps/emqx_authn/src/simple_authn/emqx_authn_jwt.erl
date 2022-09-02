a41461ac930a22c4ef515304f5c2e81f87a7ed7b
JianBo He <heeejianbo@163.com>
2022-09-02 16:17:25 +0800

    chore: update apps/emqx_authn/src/simple_authn/emqx_authn_jwt.erl

--- apps/emqx_authn/src/simple_authn/emqx_authn_jwt.erl
+++ apps/emqx_authn/src/simple_authn/emqx_authn_jwt.erl
@@ -1,4 +1,4 @@
-%--------------------------------------------------------------------
+%%--------------------------------------------------------------------
 %% Copyright (c) 2021-2022 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
