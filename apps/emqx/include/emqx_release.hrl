19e3f5919e23c36fb293f187d8f28a75b52809af
Shawn <506895667@qq.com>
2022-09-26 19:54:01 +0800

    chore: release e5.0.0-beta.4

--- apps/emqx/include/emqx_release.hrl
+++ apps/emqx/include/emqx_release.hrl
@@ -35,7 +35,7 @@
 -define(EMQX_RELEASE_CE, "5.0.8").
 
 %% Enterprise edition
--define(EMQX_RELEASE_EE, "5.0.0-beta.3").
+-define(EMQX_RELEASE_EE, "5.0.0-beta.4").
 
 %% the HTTP API version
 -define(EMQX_API_VERSION, "5.0").
