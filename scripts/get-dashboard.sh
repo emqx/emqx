efda32c945bc78e3492326279b228e894f640371
JianBo He <heeejianbo@gmail.com>
2022-10-12 11:32:39 +0800

    chore: update dashboard vsn

--- scripts/get-dashboard.sh
+++ scripts/get-dashboard.sh
@@ -13,8 +13,8 @@ case "${PKG_VSN}" in
         ;;
     4.4*)
         # keep the above 4.3 untouched, otherwise conflicts!
-        EMQX_CE_DASHBOARD_VERSION='v4.4.5'
-        EMQX_EE_DASHBOARD_VERSION='v4.4.15'
+        EMQX_CE_DASHBOARD_VERSION='v4.4.6'
+        EMQX_EE_DASHBOARD_VERSION='v4.4.17'
         ;;
     *)
         echo "Unsupported version $PKG_VSN" >&2
