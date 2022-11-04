6ae2b06ba12599c054a6cc9b77af4f88c11c8223
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-11-04 22:11:41 +0100

    chore: upgrade dashboard version

--- scripts/get-dashboard.sh
+++ scripts/get-dashboard.sh
@@ -8,8 +8,8 @@ cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 PKG_VSN="${PKG_VSN:-$(./pkg-vsn.sh)}"
 case "${PKG_VSN}" in
     4.3*)
-        EMQX_CE_DASHBOARD_VERSION='v4.3.11'
-        EMQX_EE_DASHBOARD_VERSION='v4.3.26'
+        EMQX_CE_DASHBOARD_VERSION='v4.3.12'
+        EMQX_EE_DASHBOARD_VERSION='v4.3.27'
         ;;
     *)
         echo "Unsupported version $PKG_VSN" >&2
