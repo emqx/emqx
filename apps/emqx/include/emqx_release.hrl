15bd797b36f1762d2c75195e10be0ada758667b2
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-12-27 11:24:47 +0100

    ci: bump to v5.0.13-rc.3

--- apps/emqx/include/emqx_release.hrl
+++ apps/emqx/include/emqx_release.hrl
@@ -32,7 +32,7 @@
 %% `apps/emqx/src/bpapi/README.md'
 
 %% Community edition
--define(EMQX_RELEASE_CE, "5.0.13-rc.2").
+-define(EMQX_RELEASE_CE, "5.0.13-rc.3").
 
 %% Enterprise edition
 -define(EMQX_RELEASE_EE, "5.0.0-beta.6").
