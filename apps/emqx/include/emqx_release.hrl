1716a5da995c46889c11e5d58f0f94156daae4bd
Zaiming (Stone) Shi <zmstone@gmail.com>
2023-01-18 17:22:05 +0100

    chore: bump version to e5.0.0-rc.1

--- apps/emqx/include/emqx_release.hrl
+++ apps/emqx/include/emqx_release.hrl
@@ -35,7 +35,7 @@
 -define(EMQX_RELEASE_CE, "5.0.14").
 
 %% Enterprise edition
--define(EMQX_RELEASE_EE, "5.0.0-beta.6").
+-define(EMQX_RELEASE_EE, "5.0.0-rc.1").
 
 %% the HTTP API version
 -define(EMQX_API_VERSION, "5.0").
