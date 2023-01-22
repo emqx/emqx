8f544041e4a123adbbd139a691052b826ec19842
Ilya Averyanov <av@rubybox.ru>
2023-01-23 00:30:26 +0200

    tie file transfer frontend and backend together

---
+++ apps/emqx/test/emqx_channel_delayed_puback_SUITE.erl
@@ -0,0 +1,70 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2018-2022 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+-module(emqx_channel_delayed_puback_SUITE).
+
+-compile(export_all).
+-compile(nowarn_export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("common_test/include/ct.hrl").
+-include_lib("snabbkaffe/include/snabbkaffe.hrl").
+
+-include_lib("emqx/include/emqx.hrl").
+-include_lib("emqx/include/emqx_mqtt.hrl").
+-include_lib("emqx/include/emqx_hooks.hrl").
+
+all() ->
+    emqx_common_test_helpers:all(?MODULE).
+
+init_per_suite(Config) ->
+    emqx_common_test_helpers:boot_modules(all),
+    emqx_common_test_helpers:start_apps([]),
+    Config.
+
+end_per_suite(_Config) ->
+    emqx_common_test_helpers:stop_apps([]).
+
+init_per_testcase(Case, Config) ->
+    ?MODULE:Case({init, Config}).
+
+end_per_testcase(Case, Config) ->
+    ?MODULE:Case({'end', Config}).
+
+%%--------------------------------------------------------------------
+%% Test cases
+%%--------------------------------------------------------------------
+
+t_delayed_puback({init, Config}) ->
+    emqx_hooks:put('message.puback', {?MODULE, on_message_puback, []}, ?HP_LOWEST),
+    Config;
+t_delayed_puback({'end', _Config}) ->
+    emqx_hooks:del('message.puback', {?MODULE, on_message_puback});
+t_delayed_puback(_Config) ->
+    {ok, ConnPid} = emqtt:start_link([{clientid, <<"clientid">>}, {proto_ver, v5}]),
+    {ok, _} = emqtt:connect(ConnPid),
+    {ok, #{reason_code := ?RC_UNSPECIFIED_ERROR}} = emqtt:publish(
+        ConnPid, <<"topic">>, <<"hello">>, 1
+    ),
+    emqtt:disconnect(ConnPid).
+
+%%--------------------------------------------------------------------
+%% Helpers
+%%--------------------------------------------------------------------
+
+on_message_puback(PacketId, _Msg, PubRes, _RC) ->
+    erlang:send(self(), {puback, PacketId, PubRes, ?RC_UNSPECIFIED_ERROR}),
+    {stop, undefined}.
--- apps/emqx_ft/src/emqx_ft.erl
+++ apps/emqx_ft/src/emqx_ft.erl
@@ -17,6 +17,10 @@
 -module(emqx_ft).
 
 -include("emqx_ft.hrl").
+-include_lib("emqx/include/emqx.hrl").
+-include_lib("emqx/include/logger.hrl").
+-include_lib("emqx/include/emqx_mqtt.hrl").
+-include_lib("emqx/include/emqx_hooks.hrl").
 
 -export([
     create_tab/0,
@@ -27,8 +31,13 @@
 -export([
     on_channel_unregistered/1,
     on_channel_takeover/3,
-    on_channel_takeovered/3
+    on_channel_takenover/3,
+    on_message_publish/1,
+    on_message_puback/4
 ]).
+
+%% For Debug
+-export([transfer/2, storage/0]).
 
 -export_type([clientid/0]).
 -export_type([transfer/0]).
@@ -67,16 +76,20 @@ create_tab() ->
     ok.
 
 hook() ->
-    % ok = emqx_hooks:put('channel.registered', {?MODULE, on_channel_registered, []}),
-    ok = emqx_hooks:put('channel.unregistered', {?MODULE, on_channel_unregistered, []}),
-    ok = emqx_hooks:put('channel.takeover', {?MODULE, on_channel_takeover, []}),
-    ok = emqx_hooks:put('channel.takeovered', {?MODULE, on_channel_takeovered, []}).
+    ok = emqx_hooks:put('channel.unregistered', {?MODULE, on_channel_unregistered, []}, ?HP_LOWEST),
+    ok = emqx_hooks:put('channel.takeover', {?MODULE, on_channel_takeover, []}, ?HP_LOWEST),
+    ok = emqx_hooks:put('channel.takenover', {?MODULE, on_channel_takenover, []}, ?HP_LOWEST),
+
+    ok = emqx_hooks:put('message.publish', {?MODULE, on_message_publish, []}, ?HP_LOWEST),
+    ok = emqx_hooks:put('message.puback', {?MODULE, on_message_puback, []}, ?HP_LOWEST).
 
 unhook() ->
-    % ok = emqx_hooks:del('channel.registered', {?MODULE, on_channel_registered}),
     ok = emqx_hooks:del('channel.unregistered', {?MODULE, on_channel_unregistered}),
     ok = emqx_hooks:del('channel.takeover', {?MODULE, on_channel_takeover}),
-    ok = emqx_hooks:del('channel.takeovered', {?MODULE, on_channel_takeovered}).
+    ok = emqx_hooks:del('channel.takenover', {?MODULE, on_channel_takenover}),
+
+    ok = emqx_hooks:del('message.publish', {?MODULE, on_message_publish}),
+    ok = emqx_hooks:del('message.puback', {?MODULE, on_message_puback}).
 
 %%--------------------------------------------------------------------
 %% Hooks
@@ -93,10 +106,29 @@ on_channel_takeover(_ConnMod, ChanPid, TakeoverData) ->
             ok
     end.
 
-on_channel_takeovered(_ConnMod, ChanPid, #{ft_data := FTData}) ->
+on_channel_takenover(_ConnMod, ChanPid, #{ft_data := FTData}) ->
     ok = put_ft_data(ChanPid, FTData);
-on_channel_takeovered(_ConnMod, _ChanPid, _) ->
+on_channel_takenover(_ConnMod, _ChanPid, _) ->
     ok.
+
+on_message_publish(
+    Msg = #message{
+        id = _Id,
+        topic = <<"$file/", _/binary>>
+    }
+) ->
+    Headers = Msg#message.headers,
+    {stop, Msg#message{headers = Headers#{allow_publish => false}}};
+on_message_publish(Msg) ->
+    {ok, Msg}.
+
+on_message_puback(PacketId, #message{topic = Topic} = Msg, _PubRes, _RC) ->
+    case Topic of
+        <<"$file/", FileCommand/binary>> ->
+            {stop, on_file_command(PacketId, Msg, FileCommand)};
+        _ ->
+            ignore
+    end.
 
 %%--------------------------------------------------------------------
 %% Private funs
@@ -115,3 +147,92 @@ delete_ft_data(ChanPid) ->
 put_ft_data(ChanPid, FTData) ->
     true = ets:insert(?FT_TAB, #emqx_ft{chan_pid = ChanPid, ft_data = FTData}),
     ok.
+
+on_file_command(PacketId, Msg, FileCommand) ->
+    case string:split(FileCommand, <<"/">>, all) of
+        [FileId, <<"init">>] ->
+            on_init(Msg, FileId);
+        [FileId, <<"fin">>] ->
+            on_fin(PacketId, Msg, FileId, undefined);
+        [FileId, <<"fin">>, Checksum] ->
+            on_fin(PacketId, Msg, FileId, Checksum);
+        [FileId, <<"abort">>] ->
+            on_abort(Msg, FileId);
+        [FileId, Offset] ->
+            on_segment(Msg, FileId, Offset, undefined);
+        [FileId, Offset, Checksum] ->
+            on_segment(Msg, FileId, Offset, Checksum);
+        _ ->
+            ?RC_UNSPECIFIED_ERROR
+    end.
+
+on_init(Msg, FileId) ->
+    ?SLOG(info, #{
+        msg => "on_init",
+        mqtt_msg => Msg,
+        file_id => FileId
+    }),
+    % Payload = Msg#message.payload,
+    % %% Add validations here
+    % Meta = emqx_json:decode(Payload, [return_maps]),
+    % ok = emqx_ft_storage_fs:store_filemeta(storage(), transfer(Msg, FileId), Meta),
+    % ?RC_SUCCESS.
+    ?RC_UNSPECIFIED_ERROR.
+
+on_abort(_Msg, _FileId) ->
+    %% TODO
+    ?RC_SUCCESS.
+
+on_segment(Msg, FileId, Offset, Checksum) ->
+    ?SLOG(info, #{
+        msg => "on_segment",
+        mqtt_msg => Msg,
+        file_id => FileId,
+        offset => Offset,
+        checksum => Checksum
+    }),
+    % %% TODO: handle checksum
+    % Payload = Msg#message.payload,
+    % %% Add offset/checksum validations
+    % ok = emqx_ft_storage_fs:store_segment(
+    %     storage(),
+    %     transfer(Msg, FileId),
+    %     {binary_to_integer(Offset), Payload}
+    % ),
+    % ?RC_SUCCESS.
+    ?RC_UNSPECIFIED_ERROR.
+
+on_fin(PacketId, Msg, FileId, Checksum) ->
+    ?SLOG(info, #{
+        msg => "on_fin",
+        mqtt_msg => Msg,
+        file_id => FileId,
+        checksum => Checksum,
+        packet_id => PacketId
+    }),
+    % %% TODO: handle checksum? Do we need it?
+    % {ok, _} = emqx_ft_storage_fs:assemble(
+    %     storage(),
+    %     transfer(Msg, FileId),
+    %     callback(FileId, Msg)
+    % ),
+    Callback = callback(FileId, PacketId),
+    spawn(fun() -> Callback({error, not_implemented}) end),
+    undefined.
+
+callback(_FileId, PacketId) ->
+    ChanPid = self(),
+    fun
+        (ok) ->
+            erlang:send(ChanPid, {puback, PacketId, [], ?RC_SUCCESS});
+        ({error, _}) ->
+            erlang:send(ChanPid, {puback, PacketId, [], ?RC_UNSPECIFIED_ERROR})
+    end.
+
+transfer(Msg, FileId) ->
+    ClientId = Msg#message.from,
+    {ClientId, FileId}.
+
+%% TODO: configure
+storage() ->
+    filename:join(emqx:data_dir(), "file_transfer").
