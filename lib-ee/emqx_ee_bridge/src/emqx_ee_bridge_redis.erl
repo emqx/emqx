e77675186036dfa1432b2088de64482db4b9ba72
Zhongwen Deng <zhongwencool@gmail.com>
2023-02-02 16:55:11 +0800

    fix: delete enable_batch from redis bridge

--- lib-ee/emqx_ee_bridge/src/emqx_ee_bridge_redis.erl
+++ lib-ee/emqx_ee_bridge/src/emqx_ee_bridge_redis.erl
@@ -182,7 +182,7 @@ resource_creation_fields("redis_cluster") ->
     % TODO
     % Cluster bridge is currently incompatible with batching.
     Fields = emqx_resource_schema:fields("creation_opts_sync_only"),
-    lists:foldl(fun proplists:delete/2, Fields, [batch_size, batch_time]);
+    lists:foldl(fun proplists:delete/2, Fields, [batch_size, batch_time, enable_batch]);
 resource_creation_fields(_) ->
     emqx_resource_schema:fields("creation_opts_sync_only").
 
