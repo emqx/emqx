c22e2a0d185d108ad5f1b0f86ec700df753d6987
Shawn <506895667@qq.com>
2023-02-04 12:48:12 +0800

    fix: add retry for rules

--- apps/emqx_rule_engine/test/emqx_rule_monitor_SUITE.erl
+++ apps/emqx_rule_engine/test/emqx_rule_monitor_SUITE.erl
@@ -48,7 +48,7 @@ end_per_suite(_Config) ->
     ok.
 
 init_per_testcase(t_restart_resource, Config) ->
-    emqx_rule_monitor:put_retry_interval(100),
+    emqx_rule_monitor:put_resource_retry_interval(100),
     Opts = [public, named_table, set, {read_concurrency, true}],
     _ = ets:new(?RES_PARAMS_TAB, [{keypos, #resource_params.id}|Opts]),
     ets:new(t_restart_resource, [named_table, public]),
@@ -95,7 +95,7 @@ common_init_per_testcase() ->
 
 common_end_per_testcases() ->
     ok = emqx_alarm:stop(),
-    emqx_rule_monitor:erase_retry_interval(),
+    emqx_rule_monitor:erase_resource_retry_interval(),
     emqx_rule_monitor:stop().
 
 t_restart_resource(_) ->
