b81b62c63939b38cd3f7c349e13909087f4bc6fc
William Yang <mscame@gmail.com>
2023-02-14 10:56:31 +0100

    chore(quic): doc about deprecated fields.

--- apps/emqx/src/emqx_schema.erl
+++ apps/emqx/src/emqx_schema.erl
@@ -1784,6 +1784,10 @@ desc("listener_ssl_opts") ->
     "Socket options for SSL connections.";
 desc("listener_wss_opts") ->
     "Socket options for WebSocket/SSL connections.";
+desc("fields_mqtt_quic_listener_certfile") ->
+    "Path to the certificate file. Will be deprecated in 5.1, use .ssl_options.certfile instead.";
+desc("fields_mqtt_quic_listener_keyfile") ->
+    "Path to the secret key file. Will be deprecated in 5.1, use .ssl_options.keyfile instead.";
 desc("listener_quic_ssl_opts") ->
     "TLS options for QUIC transport.";
 desc("ssl_client_opts") ->
