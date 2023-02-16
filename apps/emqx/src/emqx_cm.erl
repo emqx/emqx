53f99b1d0a2d543333c9c9ca2a943ff7234d994d
Ilya Averyanov <av@rubybox.dev>
2023-02-16 17:38:54 +0300

    fix(ft): fix emqx_cm takover

--- apps/emqx/src/emqx_cm.erl
+++ apps/emqx/src/emqx_cm.erl
@@ -312,6 +312,7 @@ open_session(false, ClientInfo = #{clientid := ClientId}, ConnInfo) ->
                         Session1 = emqx_persistent_session:persist(
                             ClientInfo, ConnInfo, Session
                         ),
+                        register_channel(ClientId, Self, ConnInfo),
                         {ok, #{
                             session => clean_session(Session1),
                             present => true,
