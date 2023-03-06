27c5389fdc914353a31ae2e3c73a4c03cf726d0e
William Yang <mscame@gmail.com>
2023-03-06 14:05:33 +0100

    fix(quic): inval listener option casue segfault
    bump quicer to 0.0.113

--- mix.exs
+++ mix.exs
@@ -648,7 +648,7 @@ defmodule EMQXUmbrella.MixProject do
   defp quicer_dep() do
     if enable_quicer?(),
       # in conflict with emqx and emqtt
-      do: [{:quicer, github: "emqx/quic", tag: "0.0.111", override: true}],
+      do: [{:quicer, github: "emqx/quic", tag: "0.0.113", override: true}],
       else: []
   end
 
