0b6fd7fe145aaee2c39ae12998c6706cf5e3daec
Thales Macedo Garitezi <thalesmg@gmail.com>
2023-03-16 13:33:52 -0300

    fix(buffer_worker): check request timeout and health check interval
    Port of https://github.com/emqx/emqx/pull/10154 for `release-50`
    
    Fixes https://emqx.atlassian.net/browse/EMQX-9099
    
    Originally, the `resume_interval`, which is what defines how often a
    buffer worker will attempt to retry its inflight window, was set to
    the same as the `health_check_interval`.  This had the problem that,
    with default values, `health_check_interval = request_timeout`.  This
    meant that, if a buffer worker with those configs were ever blocked,
    all requests would have timed out by the time it retried them.
    
    Here we change the default `resume_interval` to a reasonable value
    dependent on `health_check_interval` and `request_timeout`, and also
    expose that as a hidden parameter for fine tuning if necessary.

--- apps/emqx_resource/src/schema/emqx_resource_schema.erl
+++ apps/emqx_resource/src/schema/emqx_resource_schema.erl
@@ -55,6 +55,7 @@ fields("creation_opts") ->
     [
         {worker_pool_size, fun worker_pool_size/1},
         {health_check_interval, fun health_check_interval/1},
+        {resume_interval, fun resume_interval/1},
         {start_after_created, fun start_after_created/1},
         {start_timeout, fun start_timeout/1},
         {auto_restart_interval, fun auto_restart_interval/1},
@@ -80,6 +81,12 @@ worker_pool_size(desc) -> ?DESC("worker_pool_size");
 worker_pool_size(default) -> ?WORKER_POOL_SIZE;
 worker_pool_size(required) -> false;
 worker_pool_size(_) -> undefined.
+
+resume_interval(type) -> emqx_schema:duration_ms();
+resume_interval(hidden) -> true;
+resume_interval(desc) -> ?DESC("resume_interval");
+resume_interval(required) -> false;
+resume_interval(_) -> undefined.
 
 health_check_interval(type) -> emqx_schema:duration_ms();
 health_check_interval(desc) -> ?DESC("health_check_interval");
