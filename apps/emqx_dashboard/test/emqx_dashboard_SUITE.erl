40f7fd691ea092d701b7e730a8a6c8ff1e802765
Andrew Mayorov <encube.ul@gmail.com>
2023-03-23 15:23:42 +0300

    test(dashboard): reuse `emqx_mgmt_api_test_util` in testsuites
    To reduce the init / teardown complexity.

--- apps/emqx_dashboard/test/emqx_dashboard_SUITE.erl
+++ apps/emqx_dashboard/test/emqx_dashboard_SUITE.erl
@@ -57,22 +57,11 @@ all() ->
     emqx_common_test_helpers:all(?MODULE).
 
 init_per_suite(Config) ->
-    emqx_common_test_helpers:start_apps(
-        [emqx_management, emqx_dashboard],
-        fun set_special_configs/1
-    ),
+    emqx_mgmt_api_test_util:init_suite([emqx_management]),
     Config.
 
 end_per_suite(_Config) ->
-    mnesia:clear_table(?ADMIN),
-    emqx_common_test_helpers:stop_apps([emqx_dashboard, emqx_management]),
-    mria:stop().
-
-set_special_configs(emqx_dashboard) ->
-    emqx_dashboard_api_test_helpers:set_default_config(),
-    ok;
-set_special_configs(_) ->
-    ok.
+    emqx_mgmt_api_test_util:end_suite([emqx_management]).
 
 t_overview(_) ->
     mnesia:clear_table(?ADMIN),
--- apps/emqx_dashboard/test/emqx_dashboard_error_code_SUITE.erl
+++ apps/emqx_dashboard/test/emqx_dashboard_error_code_SUITE.erl
@@ -29,24 +29,11 @@ all() ->
     emqx_common_test_helpers:all(?MODULE).
 
 init_per_suite(Config) ->
-    mria:start(),
-    application:load(emqx_dashboard),
-    emqx_common_test_helpers:start_apps([emqx_conf, emqx_dashboard], fun set_special_configs/1),
+    emqx_mgmt_api_test_util:init_suite([emqx_conf]),
     Config.
 
-set_special_configs(emqx_dashboard) ->
-    emqx_dashboard_api_test_helpers:set_default_config(),
-    ok;
-set_special_configs(_) ->
-    ok.
-
-end_per_suite(Config) ->
-    end_suite(),
-    Config.
-
-end_suite() ->
-    application:unload(emqx_management),
-    emqx_common_test_helpers:stop_apps([emqx_dashboard]).
+end_per_suite(_Config) ->
+    emqx_mgmt_api_test_util:end_suite([emqx_conf]).
 
 t_all_code(_) ->
     HrlDef = ?ERROR_CODES,
