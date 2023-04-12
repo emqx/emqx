1e3360019041a4c0937ffe5c7f2a098c4c863751
Ivan Dyachkov <dev@dyachkov.org>
2023-04-12 17:33:53 +0200

    chore: bump version to v5.0.22

--- apps/emqx/include/emqx_release.hrl
+++ apps/emqx/include/emqx_release.hrl
@@ -32,7 +32,7 @@
 %% `apps/emqx/src/bpapi/README.md'
 
 %% Community edition
--define(EMQX_RELEASE_CE, "5.0.21").
+-define(EMQX_RELEASE_CE, "5.0.22").
 
 %% Enterprise edition
 -define(EMQX_RELEASE_EE, "5.0.2").
