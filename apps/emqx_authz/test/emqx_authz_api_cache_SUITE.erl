16c49b2cc185f703b15590d443382b642d74e6af
Stefan Strigler <stefan.strigler@emqx.io>
2023-04-14 17:21:38 +0200

    fix: undo wrong thinking about returned types from decode

--- apps/emqx_authz/test/emqx_authz_api_cache_SUITE.erl
+++ apps/emqx_authz/test/emqx_authz_api_cache_SUITE.erl
@@ -60,19 +60,19 @@ set_special_configs(emqx_authz) ->
 set_special_configs(_App) ->
     ok.
 
-t_clean_cahce(_) ->
+t_clean_cache(_) ->
     {ok, C} = emqtt:start_link([{clientid, <<"emqx0">>}, {username, <<"emqx0">>}]),
     {ok, _} = emqtt:connect(C),
     {ok, _, _} = emqtt:subscribe(C, <<"a/b/c">>, 0),
     ok = emqtt:publish(C, <<"a/b/c">>, <<"{\"x\":1,\"y\":1}">>, 0),
 
     {ok, 200, Result3} = request(get, uri(["clients", "emqx0", "authorization", "cache"])),
-    ?assertEqual(2, maps:size(emqx_utils_json:decode(Result3))),
+    ?assertEqual(2, length(emqx_utils_json:decode(Result3))),
 
     request(delete, uri(["authorization", "cache"])),
 
     {ok, 200, Result4} = request(get, uri(["clients", "emqx0", "authorization", "cache"])),
-    ?assertEqual(0, maps:size(emqx_utils_json:decode(Result4))),
+    ?assertEqual(0, length(emqx_utils_json:decode(Result4))),
 
     ok.
 
