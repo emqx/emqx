180b6acd9e8d12076be8b1d9424150248db01876
Thales Macedo Garitezi <thalesmg@gmail.com>
2023-04-18 13:03:06 -0300

    docs: remove auto-generated comment

--- apps/emqx_bridge_pulsar/src/emqx_bridge_pulsar_sup.erl
+++ apps/emqx_bridge_pulsar/src/emqx_bridge_pulsar_sup.erl
@@ -14,15 +14,6 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
 init([]) ->
     SupFlags = #{
         strategy => one_for_all,
