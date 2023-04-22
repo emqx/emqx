8f71f8d016482ebeb3a707b306e66435fbe079e6
Ivan Dyachkov <dev@dyachkov.org>
2023-04-22 20:36:55 +0200

    chore: v5.0.24

--- apps/emqx/include/emqx_release.hrl
+++ apps/emqx/include/emqx_release.hrl
@@ -32,7 +32,7 @@
 %% `apps/emqx/src/bpapi/README.md'
 
 %% Community edition
--define(EMQX_RELEASE_CE, "5.0.23").
+-define(EMQX_RELEASE_CE, "5.0.24").
 
 %% Enterprise edition
 -define(EMQX_RELEASE_EE, "5.0.3-alpha.1").
