5a21fddfc7c108d249986ae789eb0e26ff031450
William Yang <mscame@gmail.com>
2023-04-27 16:33:36 +0200

    ci(fvt): remove MQTT 'retry interval' and 'max_topic_alias'

--- .ci/docker-compose-file/scripts/run-emqx.sh
+++ .ci/docker-compose-file/scripts/run-emqx.sh
@@ -20,8 +20,6 @@ esac
 
 {
   echo "HOCON_ENV_OVERRIDE_PREFIX=EMQX_"
-  echo "EMQX_ZONES__DEFAULT__MQTT__RETRY_INTERVAL=2s"
-  echo "EMQX_ZONES__DEFAULT__MQTT__MAX_TOPIC_ALIAS=10"
   echo "EMQX_AUTHORIZATION__SOURCES=[]"
   echo "EMQX_AUTHORIZATION__NO_MATCH=allow"
 } >> .ci/docker-compose-file/conf.cluster.env
