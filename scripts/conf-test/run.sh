07e46592a8398f11480dd781340699c03b789451
JianBo He <heeejianbo@gmail.com>
2023-05-11 14:56:24 +0800

    chore(ci): add conf tests

---
+++ scripts/conf-test/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+PROFILE="${PROFILE:-emqx}"
+EMQX_ROOT="${EMQX_ROOT:-_build/$PROFILE/rel/emqx}"
+EMQX_WAIT_FOR_START="${EMQX_WAIT_FOR_START:-30}"
+export EMQX_WAIT_FOR_START
+
+start_emqx_with_conf() {
+    echo "Starting $PROFILE with $1"
+    $EMQX_ROOT/bin/emqx start
+    $EMQX_ROOT/bin/emqx stop
+}
+
+MINOR_VSN=$(./pkg-vsn.sh "$PROFILE" | cut -d. -f1,2)
+
+if [ "$PROFILE" = "emqx" ]; then
+  EDITION="ce"
+else
+  EDITION="ee"
+fi
+
+FILES=$(ls ./scripts/conf-test/old-confs/$EDITION-v"$MINOR_VSN"*)
+
+cp $EMQX_ROOT/etc/emqx.conf $EMQX_ROOT/etc/emqx.conf.bak
+cleanup() {
+    cp $EMQX_ROOT/etc/emqx.conf.bak $EMQX_ROOT/etc/emqx.conf
+}
+trap cleanup EXIT
+
+for file in $FILES; do
+    cp $file $EMQX_ROOT/etc/emqx.conf
+    start_emqx_with_conf $file
+done
