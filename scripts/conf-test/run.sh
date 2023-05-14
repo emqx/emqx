46611c65be4e14fd220665ae346524fc9ef5e268
JianBo He <heeejianbo@gmail.com>
2023-05-14 13:03:25 +0800

    chore: make spellcheck happy

--- scripts/conf-test/run.sh
+++ scripts/conf-test/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 PROFILE="${PROFILE:-emqx}"
 EMQX_ROOT="${EMQX_ROOT:-_build/$PROFILE/rel/emqx}"
 EMQX_WAIT_FOR_START="${EMQX_WAIT_FOR_START:-30}"
@@ -7,8 +9,8 @@ export EMQX_WAIT_FOR_START
 
 start_emqx_with_conf() {
     echo "Starting $PROFILE with $1"
-    $EMQX_ROOT/bin/emqx start
-    $EMQX_ROOT/bin/emqx stop
+    "$EMQX_ROOT"/bin/emqx start
+    "$EMQX_ROOT"/bin/emqx stop
 }
 
 MINOR_VSN=$(./pkg-vsn.sh "$PROFILE" | cut -d. -f1,2)
@@ -21,13 +23,13 @@ fi
 
 FILES=$(ls ./scripts/conf-test/old-confs/$EDITION-v"$MINOR_VSN"*)
 
-cp $EMQX_ROOT/etc/emqx.conf $EMQX_ROOT/etc/emqx.conf.bak
+cp "$EMQX_ROOT"/etc/emqx.conf "$EMQX_ROOT"/etc/emqx.conf.bak
 cleanup() {
-    cp $EMQX_ROOT/etc/emqx.conf.bak $EMQX_ROOT/etc/emqx.conf
+    cp "$EMQX_ROOT"/etc/emqx.conf.bak "$EMQX_ROOT"/etc/emqx.conf
 }
 trap cleanup EXIT
 
 for file in $FILES; do
-    cp $file $EMQX_ROOT/etc/emqx.conf
-    start_emqx_with_conf $file
+    cp "$file" "$EMQX_ROOT"/etc/emqx.conf
+    start_emqx_with_conf "$file"
 done
