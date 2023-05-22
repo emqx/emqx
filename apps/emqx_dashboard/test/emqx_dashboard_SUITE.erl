cb5a596c5715719e4bfa4052a9bde88fb3a75d3f
Zhongwen Deng <zhongwencool@gmail.com>
2023-05-22 16:58:13 +0800

    fix: make sure swagger.json is fully generated

--- apps/emqx_dashboard/test/emqx_dashboard_SUITE.erl
+++ apps/emqx_dashboard/test/emqx_dashboard_SUITE.erl
@@ -55,6 +55,10 @@ all() ->
     emqx_common_test_helpers:all(?MODULE).
 
 init_per_suite(Config) ->
+    %% Load all applications to ensure swagger.json is fully generated.
+    Apps = emqx_machine_boot:reboot_apps(),
+    ct:pal("load apps:~p~n", [Apps]),
+    lists:foreach(fun(App) -> application:load(App) end, Apps),
     emqx_mgmt_api_test_util:init_suite([emqx_management]),
     Config.
 
