d167d6dd412d73722d87f37ca3e0692ddccd7077
Zaiming (Stone) Shi <zmstone@gmail.com>
2023-05-26 15:26:27 +0200

    chore(emqx_telemetry): update supervisor's child restart strategy

--- apps/emqx_telemetry/src/emqx_telemetry_sup.erl
+++ apps/emqx_telemetry/src/emqx_telemetry_sup.erl
@@ -38,8 +38,8 @@ start_link() ->
 init([]) ->
     SupFlags = #{
         strategy => one_for_one,
-        intensity => 0,
-        period => 1
+        intensity => 10,
+        period => 5
     },
     ChildSpecs = [?CHILD(emqx_telemetry)],
     {ok, {SupFlags, ChildSpecs}}.
