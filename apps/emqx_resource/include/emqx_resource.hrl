7f850f74991fc8f50c11ecddcfc81757f033aeda
Thales Macedo Garitezi <thalesmg@gmail.com>
2023-06-13 17:11:39 -0300

    fix(resource): fix `query_mode/0` type and usage

--- apps/emqx_resource/include/emqx_resource.hrl
+++ apps/emqx_resource/include/emqx_resource.hrl
@@ -22,7 +22,7 @@
 -type resource_state() :: term().
 -type resource_status() :: connected | disconnected | connecting | stopped.
 -type callback_mode() :: always_sync | async_if_possible.
--type query_mode() :: async | sync | simple_async | simple_sync | dynamic.
+-type query_mode() :: simple_sync | simple_async | sync | async | no_queries.
 -type result() :: term().
 -type reply_fun() :: {fun((result(), Args :: term()) -> any()), Args :: term()} | undefined.
 -type query_opts() :: #{
