fb96c1e20de9b5ca4cfc4c6d72329ac8b3b07390
Ivan Dyachkov <dev@dyachkov.org>
2023-07-10 17:24:00 +0200

    chore: bump to e5.1.1-alpha.1

--- apps/emqx/include/emqx_release.hrl
+++ apps/emqx/include/emqx_release.hrl
@@ -35,7 +35,7 @@
 -define(EMQX_RELEASE_CE, "5.1.1").
 
 %% Enterprise edition
--define(EMQX_RELEASE_EE, "5.1.0").
+-define(EMQX_RELEASE_EE, "5.1.1-alpha.1").
 
 %% The HTTP API version
 -define(EMQX_API_VERSION, "5.0").
