fea73cf17d20d3fd3914a34ad508a0d608cf4493
firest <enlanubo@gmail.com>
2023-07-14 17:19:50 +0800

    fix(log): fix the `pubrel` typo in the log

--- apps/emqx/src/emqx_channel.erl
+++ apps/emqx/src/emqx_channel.erl
@@ -461,7 +461,7 @@ handle_in(
             NChannel = set_session(NSession, Channel),
             handle_out(pubcomp, {PacketId, ?RC_SUCCESS}, NChannel);
         {error, RC = ?RC_PACKET_IDENTIFIER_NOT_FOUND} ->
-            ?SLOG(warning, #{msg => "pubrec_packetId_not_found", packetId => PacketId}),
+            ?SLOG(warning, #{msg => "pubrel_packetId_not_found", packetId => PacketId}),
             ok = emqx_metrics:inc('packets.pubrel.missed'),
             handle_out(pubcomp, {PacketId, RC}, Channel)
     end;
