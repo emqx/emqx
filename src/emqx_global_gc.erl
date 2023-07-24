a1868d6e5bde009526f84ce13e57e14c83eef206
William Yang <mscame@gmail.com>
2023-07-24 14:43:01 +0200

    perf(tmp): major GC global

--- src/emqx_global_gc.erl
+++ src/emqx_global_gc.erl
@@ -94,7 +94,7 @@ ensure_timer(State) ->
 run_gc() -> lists:foreach(fun do_gc/1, processes()).
 
 do_gc(Pid) ->
-    is_waiting(Pid) andalso garbage_collect(Pid).
+    is_waiting(Pid) andalso garbage_collect(Pid, [{type,major}]).
 
 -compile({inline, [is_waiting/1]}).
 is_waiting(Pid) ->
