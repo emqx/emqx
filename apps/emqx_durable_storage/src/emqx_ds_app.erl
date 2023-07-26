14b76916dfe22f015a7276d495c867592f5e21f9
firest <enlanubo@gmail.com>
2023-07-26 15:37:58 +0800

    fix(ds): avoid crashes when starting on Windows

--- apps/emqx_durable_storage/src/emqx_ds_app.erl
+++ apps/emqx_durable_storage/src/emqx_ds_app.erl
@@ -13,12 +13,20 @@ start(_Type, _Args) ->
     emqx_ds_sup:start_link().
 
 init_mnesia() ->
+    %% FIXME: This is a temporary workaround to avoid crashes when starting on Windows
+    Storage =
+        case mria:rocksdb_backend_available() of
+            true ->
+                rocksdb_copies;
+            _ ->
+                disc_copies
+        end,
     ok = mria:create_table(
         ?SESSION_TAB,
         [
             {rlog_shard, ?DS_SHARD},
             {type, set},
-            {storage, rocksdb_copies},
+            {storage, Storage},
             {record_name, session},
             {attributes, record_info(fields, session)}
         ]
