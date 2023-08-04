0571fd8cac478a057597d8c233643ca884deb3ee
firest <enlanubo@gmail.com>
2023-08-04 14:51:45 +0800

    feat(ldap-authz): integrate the LDAP authorization

--- apps/emqx_ldap/test/emqx_ldap_authn_SUITE.erl
+++ apps/emqx_ldap/test/emqx_ldap_authn_SUITE.erl
@@ -1,7 +1,6 @@
 %%--------------------------------------------------------------------
 %% Copyright (c) 2023 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%--------------------------------------------------------------------
-
 -module(emqx_ldap_authn_SUITE).
 
 -compile(nowarn_export_all).
