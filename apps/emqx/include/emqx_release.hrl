1146ba9f372ab68612d309d6b7784195105ed7c4
Thales Macedo Garitezi <thalesmg@gmail.com>
2023-08-09 12:03:07 -0300

    docs: Generate changelog for v5.1.5-2

--- apps/emqx/include/emqx_release.hrl
+++ apps/emqx/include/emqx_release.hrl
@@ -32,7 +32,7 @@
 %% `apps/emqx/src/bpapi/README.md'
 
 %% Opensource edition
--define(EMQX_RELEASE_CE, "5.1.5").
+-define(EMQX_RELEASE_CE, "5.1.5-2").
 
 %% Enterprise edition
 -define(EMQX_RELEASE_EE, "5.1.1").
