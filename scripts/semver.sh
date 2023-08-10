801c93dd46acdb3bbf700c10e72bae9a898ade60
Ivan Dyachkov <dev@dyachkov.org>
2023-08-10 12:20:17 +0200

    chore: v5.1.5-build.3

--- scripts/semver.sh
+++ scripts/semver.sh
@@ -6,21 +6,21 @@ function parseSemver() {
     local RE='^([0-9]+)\.([0-9]+)\.([0-9]+)(-([a-z]+\.[0-9]+))?$'
     echo "$1" | grep -qE "$RE" || exit 1
     #shellcheck disable=SC2155
-    local MAJOR=$(  echo "$1" | sed -r "s#$RE#\1#")
+    local MAJOR=$(echo "$1" | sed -r "s#$RE#\1#")
     #shellcheck disable=SC2155
-    local MINOR=$(  echo "$1" | sed -r "s#$RE#\2#")
+    local MINOR=$(echo "$1" | sed -r "s#$RE#\2#")
     #shellcheck disable=SC2155
-    local PATCH=$(  echo "$1" | sed -r "s#$RE#\3#")
+    local PATCH=$(echo "$1" | sed -r "s#$RE#\3#")
     #shellcheck disable=SC2155
-    local SPECIAL=$(echo "$1" | sed -r "s#$RE#\5#")
+    local BUILD=$(echo "$1" | sed -r "s#$RE#\5#")
     case "${2}" in
-        --major)   echo "${MAJOR}"   ;;
-        --minor)   echo "${MINOR}"   ;;
-        --patch)   echo "${PATCH}"   ;;
-        --special) echo "${SPECIAL}" ;;
+        --major) echo "${MAJOR}" ;;
+        --minor) echo "${MINOR}" ;;
+        --patch) echo "${PATCH}" ;;
+        --build) echo "${BUILD}" ;;
         *)
             cat <<EOF
-{"major": ${MAJOR}, "minor": ${MINOR}, "patch": ${PATCH}, "special": "${SPECIAL}"}
+{"major": ${MAJOR}, "minor": ${MINOR}, "patch": ${PATCH}, "build": "${BUILD}"}
 EOF
             ;;
     esac
