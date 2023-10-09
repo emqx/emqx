emqx/emqx

commit bf16417513dbf63cdc6c4e2603848529020a9438

feat(session): introduce session implementation concept

apps/emqx/src/emqx_cm.erl
@@ -19,9 +19,7 @@
 
 -behaviour(gen_server).
 
--include("emqx.hrl").
 -include("emqx_cm.hrl").
--include("emqx_session.hrl").
 -include("logger.hrl").
 -include("types.hrl").
 -include_lib("snabbkaffe/include/snabbkaffe.hrl").
@@ -48,14 +46,13 @@
     set_chan_stats/2
 ]).
 
--export([get_chann_conn_mod/2]).
+% -export([get_chann_conn_mod/2]).
 
 -export([
     open_session/3,
     discard_session/1,
     discard_session/2,
-    takeover_session/1,
-    takeover_session/2,
+    takeover_channel_session/2,
     kick_session/1,
     kick_session/2
 ]).
@@ -63,15 +60,14 @@
 -export([
     lookup_channels/1,
     lookup_channels/2,
-
-    lookup_client/1
+    lookup_client/1,
+    pick_channel/1
 ]).
 
 %% Test/debug interface
 -export([
     all_channels/0,
-    all_client_ids/0,
-    get_session_confs/2
+    all_client_ids/0
 ]).
 
 %% Client management
@@ -96,12 +92,16 @@
     clean_down/1,
     mark_channel_connected/1,
     mark_channel_disconnected/1,
-    get_connected_client_count/0,
-    takeover_finish/2,
+    get_connected_client_count/0
+]).
 
+%% RPC targets
+-export([
+    takeover_session/2,
+    takeover_finish/2,
     do_kick_session/3,
-    do_get_chan_stats/2,
     do_get_chan_info/2,
+    do_get_chan_stats/2,
     do_get_chann_conn_mod/2
 ]).
 
@@ -261,96 +261,64 @@ set_chan_stats(ClientId, ChanPid, Stats) ->
     {ok, #{
         session := emqx_session:session(),
         present := boolean(),
-        pendings => list()
+        replay => _ReplayContext
     }}
     | {error, Reason :: term()}.
 open_session(true, ClientInfo = #{clientid := ClientId}, ConnInfo) ->
     Self = self(),
-    CleanStart = fun(_) ->
+    emqx_cm_locker:trans(ClientId, fun(_) ->
         ok = discard_session(ClientId),
-        ok = emqx_session:destroy(ClientId),
+        ok = emqx_session:destroy(ClientInfo, ConnInfo),
         create_register_session(ClientInfo, ConnInfo, Self)
-    end,
-    emqx_cm_locker:trans(ClientId, CleanStart);
+    end);
 open_session(false, ClientInfo = #{clientid := ClientId}, ConnInfo) ->
     Self = self(),
-    ResumeStart = fun(_) ->
-        case takeover_session(ClientId) of
-            {living, ConnMod, ChanPid, Session} ->
-                ok = emqx_session:resume(ClientInfo, Session),
-                case wrap_rpc(emqx_cm_proto_v2:takeover_finish(ConnMod, ChanPid)) of
-                    {ok, Pendings} ->
-                        clean_register_session(Session, Pendings, ClientInfo, ConnInfo, Self);
-                    {error, _} ->
-                        create_register_session(ClientInfo, ConnInfo, Self)
-                end;
-            none ->
+    emqx_cm_locker:trans(ClientId, fun(_) ->
+        case emqx_session:open(ClientInfo, ConnInfo) of
+            {true, Session, ReplayContext} ->
+                ok = register_channel(ClientId, Self, ConnInfo),
+                {ok, #{session => Session, present => true, replay => ReplayContext}};
+            false ->
                 create_register_session(ClientInfo, ConnInfo, Self)
         end
-    end,
-    emqx_cm_locker:trans(ClientId, ResumeStart).
-
-create_session(ClientInfo, ConnInfo) ->
-    Options = get_session_confs(ClientInfo, ConnInfo),
-    Session = emqx_session:init_and_open(Options),
-    ok = emqx_metrics:inc('session.created'),
-    ok = emqx_hooks:run('session.created', [ClientInfo, emqx_session:info(Session)]),
-    Session.
+    end).
 
 create_register_session(ClientInfo = #{clientid := ClientId}, ConnInfo, ChanPid) ->
-    Session = create_session(ClientInfo, ConnInfo),
+    Session = emqx_session:create(ClientInfo, ConnInfo),
     ok = register_channel(ClientId, ChanPid, ConnInfo),
     {ok, #{session => Session, present => false}}.
 
-clean_register_session(Session, Pendings, #{clientid := ClientId}, ConnInfo, ChanPid) ->
-    ok = register_channel(ClientId, ChanPid, ConnInfo),
-    {ok, #{
-        session => clean_session(Session),
-        present => true,
-        pendings => clean_pendings(Pendings)
-    }}.
+%% @doc Try to takeover a session from existing channel.
+%% Naming is wierd, because `takeover_session/2` is an RPC target and cannot be renamed.
+-spec takeover_channel_session(emqx_types:clientid(), _TODO) ->
+    {ok, emqx_session:session(), _ReplayContext} | none | {error, _Reason}.
+takeover_channel_session(ClientId, OnTakeover) ->
+    takeover_channel_session(ClientId, pick_channel(ClientId), OnTakeover).
 
-get_session_confs(#{zone := Zone, clientid := ClientId}, #{
-    receive_maximum := MaxInflight, expiry_interval := EI
-}) ->
-    #{
-        clientid => ClientId,
-        max_subscriptions => get_mqtt_conf(Zone, max_subscriptions),
-        upgrade_qos => get_mqtt_conf(Zone, upgrade_qos),
-        max_inflight => MaxInflight,
-        retry_interval => get_mqtt_conf(Zone, retry_interval),
-        await_rel_timeout => get_mqtt_conf(Zone, await_rel_timeout),
-        max_awaiting_rel => get_mqtt_conf(Zone, max_awaiting_rel),
-        mqueue => mqueue_confs(Zone),
-        %% TODO: Add conf for allowing/disallowing persistent sessions.
-        %% Note that the connection info is already enriched to have
-        %% default config values for session expiry.
-        is_persistent => EI > 0
-    }.
+takeover_channel_session(ClientId, ChanPid, OnTakeover) when is_pid(ChanPid) ->
+    case takeover_session(ClientId, ChanPid) of
+        {living, ConnMod, Session} ->
+            Session1 = OnTakeover(Session),
+            case wrap_rpc(emqx_cm_proto_v2:takeover_finish(ConnMod, ChanPid)) of
+                {ok, Pendings} ->
+                    {ok, Session1, Pendings};
+                {error, _} = Error ->
+                    Error
+            end;
+        none ->
+            none
+    end;
+takeover_channel_session(_ClientId, undefined, _OnTakeover) ->
+    none.
 
-mqueue_confs(Zone) ->
-    #{
-        max_len => get_mqtt_conf(Zone, max_mqueue_len),
-        store_qos0 => get_mqtt_conf(Zone, mqueue_store_qos0),
-        priorities => get_mqtt_conf(Zone, mqueue_priorities),
-        default_priority => get_mqtt_conf(Zone, mqueue_default_priority)
-    }.
-
-get_mqtt_conf(Zone, Key) ->
-    emqx_config:get_zone_conf(Zone, [mqtt, Key]).
-
-%% @doc Try to takeover a session.
--spec takeover_session(emqx_types:clientid()) ->
-    none
-    | {living, atom(), pid(), emqx_session:session()}
-    | {persistent, emqx_session:session()}
-    | {expired, emqx_session:session()}.
-takeover_session(ClientId) ->
+-spec pick_channel(emqx_types:clientid()) ->
+    maybe(pid()).
+pick_channel(ClientId) ->
     case lookup_channels(ClientId) of
         [] ->
-            emqx_session:lookup(ClientId);
+            undefined;
         [ChanPid] ->
-            takeover_session(ClientId, ChanPid);
+            ChanPid;
         ChanPids ->
             [ChanPid | StalePids] = lists:reverse(ChanPids),
             ?SLOG(warning, #{msg => "more_than_one_channel_found", chan_pids => ChanPids}),
@@ -360,7 +328,7 @@ takeover_session(ClientId) ->
                 end,
                 StalePids
             ),
-            takeover_session(ClientId, ChanPid)
+            ChanPid
     end.
 
 takeover_finish(ConnMod, ChanPid) ->
@@ -370,9 +338,10 @@ takeover_finish(ConnMod, ChanPid) ->
         ChanPid
     ).
 
+%% @doc RPC Target @ emqx_cm_proto_v2:takeover_session/2
 takeover_session(ClientId, Pid) ->
     try
-        do_takeover_session(ClientId, Pid)
+        do_takeover_begin(ClientId, Pid)
     catch
         _:R when
             R == noproc;
@@ -380,25 +349,25 @@ takeover_session(ClientId, Pid) ->
             %% request_stepdown/3
             R == unexpected_exception
         ->
-            emqx_session:lookup(ClientId);
+            none;
         % rpc_call/3
         _:{'EXIT', {noproc, _}} ->
-            emqx_session:lookup(ClientId)
+            none
     end.
 
-do_takeover_session(ClientId, ChanPid) when node(ChanPid) == node() ->
-    case get_chann_conn_mod(ClientId, ChanPid) of
+do_takeover_begin(ClientId, ChanPid) when node(ChanPid) == node() ->
+    case do_get_chann_conn_mod(ClientId, ChanPid) of
         undefined ->
-            emqx_session:lookup(ClientId);
+            none;
         ConnMod when is_atom(ConnMod) ->
             case request_stepdown({takeover, 'begin'}, ConnMod, ChanPid) of
                 {ok, Session} ->
-                    {living, ConnMod, ChanPid, Session};
+                    {living, ConnMod, Session};
                 {error, Reason} ->
                     error(Reason)
             end
     end;
-do_takeover_session(ClientId, ChanPid) ->
+do_takeover_begin(ClientId, ChanPid) ->
     wrap_rpc(emqx_cm_proto_v2:takeover_session(ClientId, ChanPid)).
 
 %% @doc Discard all the sessions identified by the ClientId.
@@ -488,9 +457,10 @@ discard_session(ClientId, ChanPid) ->
 kick_session(ClientId, ChanPid) ->
     kick_session(kick, ClientId, ChanPid).
 
+%% @doc RPC Target @ emqx_cm_proto_v2:kick_session/3
 -spec do_kick_session(kick | discard, emqx_types:clientid(), chan_pid()) -> ok.
-do_kick_session(Action, ClientId, ChanPid) ->
-    case get_chann_conn_mod(ClientId, ChanPid) of
+do_kick_session(Action, ClientId, ChanPid) when node(ChanPid) =:= node() ->
+    case do_get_chann_conn_mod(ClientId, ChanPid) of
         undefined ->
             %% already deregistered
             ok;
@@ -725,9 +695,6 @@ do_get_chann_conn_mod(ClientId, ChanPid) ->
         error:badarg -> undefined
     end.
 
-get_chann_conn_mod(ClientId, ChanPid) ->
-    wrap_rpc(emqx_cm_proto_v2:get_chann_conn_mod(ClientId, ChanPid)).
-
 mark_channel_connected(ChanPid) ->
     ?tp(emqx_cm_connected_client_count_inc, #{chan_pid => ChanPid}),
     ets:insert_new(?CHAN_LIVE_TAB, {ChanPid, true}),
@@ -744,14 +711,3 @@ get_connected_client_count() ->
         undefined -> 0;
         Size -> Size
     end.
-
-clean_session(Session) ->
-    emqx_session:filter_queue(fun is_banned_msg/1, Session).
-
-clean_pendings(Pendings) ->
-    lists:filter(fun is_banned_msg/1, Pendings).
-
-is_banned_msg(#message{from = ClientId}) ->
-    [] =:= emqx_banned:look_up({clientid, ClientId});
-is_banned_msg({deliver, _Topic, Msg}) ->
-    is_banned_msg(Msg).

apps/emqx/include/emqx_session.hrl
@@ -1,5 +1,5 @@
 %%--------------------------------------------------------------------
-%% Copyright (c) 2017-2023 EMQ Technologies Co., Ltd. All Rights Reserved.
+%% Copyright (c) 2022-2023 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
@@ -17,39 +17,7 @@
 -ifndef(EMQX_SESSION_HRL).
 -define(EMQX_SESSION_HRL, true).
 
--record(session, {
-    %% Client's id
-    clientid :: emqx_types:clientid(),
-    id :: emqx_session:session_id(),
-    %% Is this session a persistent session i.e. was it started with Session-Expiry > 0
-    is_persistent :: boolean(),
-    %% Client’s Subscriptions.
-    subscriptions :: map(),
-    %% Max subscriptions allowed
-    max_subscriptions :: non_neg_integer() | infinity,
-    %% Upgrade QoS?
-    upgrade_qos :: boolean(),
-    %% Client <- Broker: QoS1/2 messages sent to the client but
-    %% have not been unacked.
-    inflight :: emqx_inflight:inflight(),
-    %% All QoS1/2 messages published to when client is disconnected,
-    %% or QoS1/2 messages pending transmission to the Client.
-    %%
-    %% Optionally, QoS0 messages pending transmission to the Client.
-    mqueue :: emqx_mqueue:mqueue(),
-    %% Next packet id of the session
-    next_pkt_id = 1 :: emqx_types:packet_id(),
-    %% Retry interval for redelivering QoS1/2 messages (Unit: millisecond)
-    retry_interval :: timeout(),
-    %% Client -> Broker: QoS2 messages received from the client, but
-    %% have not been completely acknowledged
-    awaiting_rel :: map(),
-    %% Maximum number of awaiting QoS2 messages allowed
-    max_awaiting_rel :: non_neg_integer() | infinity,
-    %% Awaiting PUBREL Timeout (Unit: millisecond)
-    await_rel_timeout :: timeout(),
-    %% Created at
-    created_at :: pos_integer()
-}).
+-define(IS_SESSION_IMPL_MEM(S), (is_tuple(S) andalso element(1, S) =:= session)).
+-define(IS_SESSION_IMPL_DS(S), (is_tuple(S) andalso element(1, S) =:= sessionds)).
 
 -endif.

apps/emqx/include/emqx_session_mem.hrl
@@ -1,0 +1,58 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2022-2023 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+-ifndef(EMQX_SESSION_MEM_HRL).
+-define(EMQX_SESSION_MEM_HRL, true).
+
+-record(session, {
+    %% Client's id
+    clientid :: emqx_types:clientid(),
+    id :: emqx_session:session_id(),
+    %% Is this session a persistent session i.e. was it started with Session-Expiry > 0
+    is_persistent :: boolean(),
+    %% Client’s Subscriptions.
+    subscriptions :: map(),
+    %% Max subscriptions allowed
+    max_subscriptions :: non_neg_integer() | infinity,
+    %% Upgrade QoS?
+    upgrade_qos :: boolean(),
+    %% Client <- Broker: QoS1/2 messages sent to the client but
+    %% have not been unacked.
+    inflight :: emqx_inflight:inflight(),
+    %% All QoS1/2 messages published to when client is disconnected,
+    %% or QoS1/2 messages pending transmission to the Client.
+    %%
+    %% Optionally, QoS0 messages pending transmission to the Client.
+    mqueue :: emqx_mqueue:mqueue(),
+    %% Next packet id of the session
+    next_pkt_id = 1 :: emqx_types:packet_id(),
+    %% Retry interval for redelivering QoS1/2 messages (Unit: millisecond)
+    retry_interval :: timeout(),
+    %% Client -> Broker: QoS2 messages received from the client, but
+    %% have not been completely acknowledged
+    awaiting_rel :: map(),
+    %% Maximum number of awaiting QoS2 messages allowed
+    max_awaiting_rel :: non_neg_integer() | infinity,
+    %% Awaiting PUBREL Timeout (Unit: millisecond)
+    await_rel_timeout :: timeout(),
+    %% Created at
+    created_at :: pos_integer(),
+
+    %% Timers
+    timers :: #{_Name => reference()}
+}).
+
+-endif.

apps/emqx/src/emqx_app.erl
@@ -38,7 +38,7 @@
 
 start(_Type, _Args) ->
     ok = maybe_load_config(),
-    _ = emqx_persistent_session_ds:init(),
+    _ = emqx_persistent_message:init(),
     ok = maybe_start_quicer(),
     ok = emqx_bpapi:start(),
     ok = emqx_alarm_handler:load(),

apps/emqx/src/emqx_broker.erl
@@ -224,7 +224,7 @@ publish(Msg) when is_record(Msg, message) ->
             }),
             [];
         Msg1 = #message{topic = Topic} ->
-            _ = emqx_persistent_session_ds:persist_message(Msg1),
+            _ = emqx_persistent_message:persist(Msg1),
             route(aggre(emqx_router:match_routes(Topic)), delivery(Msg1))
     end.
 

apps/emqx/src/emqx_channel.erl
@@ -104,7 +104,7 @@
     %% Takeover
     takeover :: boolean(),
     %% Resume
-    resuming :: boolean(),
+    resuming :: false | _ReplayContext,
     %% Pending delivers when takeovering
     pendings :: list()
 }).
@@ -403,7 +403,7 @@ handle_in(
         #channel{clientinfo = ClientInfo, session = Session}
 ) ->
     case emqx_session:puback(ClientInfo, PacketId, Session) of
-        {ok, Msg, NSession} ->
+        {ok, Msg, [], NSession} ->
             ok = after_message_acked(ClientInfo, Msg, Properties),
             {ok, Channel#channel{session = NSession}};
         {ok, Msg, Publishes, NSession} ->
@@ -460,7 +460,7 @@ handle_in(
     }
 ) ->
     case emqx_session:pubcomp(ClientInfo, PacketId, Session) of
-        {ok, NSession} ->
+        {ok, [], NSession} ->
             {ok, Channel#channel{session = NSession}};
         {ok, Publishes, NSession} ->
             handle_out(publish, Publishes, Channel#channel{session = NSession});
@@ -593,12 +593,10 @@ process_connect(
         {ok, #{session := Session, present := false}} ->
             NChannel = Channel#channel{session = Session},
             handle_out(connack, {?RC_SUCCESS, sp(false), AckProps}, ensure_connected(NChannel));
-        {ok, #{session := Session, present := true, pendings := Pendings}} ->
-            Pendings1 = lists:usort(lists:append(Pendings, emqx_utils:drain_deliver())),
+        {ok, #{session := Session, present := true, replay := ReplayContext}} ->
             NChannel = Channel#channel{
                 session = Session,
-                resuming = true,
-                pendings = Pendings1
+                resuming = ReplayContext
             },
             handle_out(connack, {?RC_SUCCESS, sp(true), AckProps}, ensure_connected(NChannel));
         {error, client_id_unavailable} ->
@@ -725,9 +723,8 @@ do_publish(
         {ok, PubRes, NSession} ->
             RC = pubrec_reason_code(PubRes),
             NChannel0 = Channel#channel{session = NSession},
-            NChannel1 = ensure_timer(expire_awaiting_rel, NChannel0),
-            NChannel2 = ensure_quota(PubRes, NChannel1),
-            handle_out(pubrec, {PacketId, RC}, NChannel2);
+            NChannel1 = ensure_quota(PubRes, NChannel0),
+            handle_out(pubrec, {PacketId, RC}, NChannel1);
         {error, RC = ?RC_PACKET_IDENTIFIER_IN_USE} ->
             ok = emqx_metrics:inc('packets.publish.inuse'),
             handle_out(pubrec, {PacketId, RC}, Channel);
@@ -900,8 +897,8 @@ maybe_update_expiry_interval(
             %% Check if the client turns off persistence (turning it on is disallowed)
             case EI =:= 0 andalso OldEI > 0 of
                 true ->
-                    NSession = emqx_session:unpersist(NChannel#channel.session),
-                    NChannel#channel{session = NSession};
+                    ok = emqx_session:destroy(NChannel#channel.session),
+                    NChannel#channel{session = undefined};
                 false ->
                     NChannel
             end
@@ -937,10 +934,12 @@ handle_deliver(
         clientinfo = ClientInfo
     }
 ) ->
+    % NOTE
+    % This is essentially part of `emqx_session_mem` logic, thus call it directly.
     Delivers1 = maybe_nack(Delivers),
-    NSession = emqx_session:enqueue(ClientInfo, Delivers1, Session),
-    NChannel = Channel#channel{session = NSession},
-    {ok, NChannel};
+    Messages = emqx_session:enrich_delivers(ClientInfo, Delivers1, Session),
+    NSession = emqx_session_mem:enqueue(ClientInfo, Messages, Session),
+    {ok, Channel#channel{session = NSession}};
 handle_deliver(
     Delivers,
     Channel = #channel{
@@ -950,11 +949,11 @@ handle_deliver(
     }
 ) ->
     case emqx_session:deliver(ClientInfo, Delivers, Session) of
+        {ok, [], NSession} ->
+            {ok, Channel#channel{session = NSession}};
         {ok, Publishes, NSession} ->
             NChannel = Channel#channel{session = NSession},
-            handle_out(publish, Publishes, ensure_timer(retry_delivery, NChannel));
-        {ok, NSession} ->
-            {ok, Channel#channel{session = NSession}}
+            handle_out(publish, Publishes, NChannel)
     end.
 
 %% Nack delivers from shared subscription
@@ -1164,7 +1163,9 @@ handle_call(
         conninfo = #{clientid := ClientId}
     }
 ) ->
-    ok = emqx_session:takeover(Session),
+    % NOTE
+    % This is essentially part of `emqx_session_mem` logic, thus call it directly.
+    ok = emqx_session_mem:takeover(Session),
     %% TODO: Should not drain deliver here (side effect)
     Delivers = emqx_utils:drain_deliver(),
     AllPendings = lists:append(Delivers, Pendings),
@@ -1222,14 +1223,18 @@ handle_info(
     {sock_closed, Reason},
     Channel =
         #channel{
-            conn_state = ConnState
+            conn_state = ConnState,
+            clientinfo = ClientInfo,
+            session = Session
         }
 ) when
     ConnState =:= connected orelse ConnState =:= reauthenticating
 ->
+    {Intent, Session1} = emqx_session:disconnect(ClientInfo, Session),
     Channel1 = ensure_disconnected(Reason, maybe_publish_will_msg(Channel)),
-    case maybe_shutdown(Reason, Channel1) of
-        {ok, Channel2} -> {ok, ?REPLY_EVENT(disconnected), Channel2};
+    Channel2 = Channel1#channel{session = Session1},
+    case maybe_shutdown(Reason, Intent, Channel2) of
+        {ok, Channel3} -> {ok, ?REPLY_EVENT(disconnected), Channel3};
         Shutdown -> Shutdown
     end;
 handle_info({sock_closed, Reason}, Channel = #channel{conn_state = disconnected}) ->
@@ -1302,41 +1307,14 @@ handle_timeout(
     end;
 handle_timeout(
     _TRef,
-    _Name = retry_delivery,
-    Channel = #channel{conn_state = disconnected}
-) ->
-    {ok, Channel};
-handle_timeout(
-    _TRef,
-    Name = retry_delivery,
+    {emqx_session, Name},
     Channel = #channel{session = Session, clientinfo = ClientInfo}
 ) ->
-    case emqx_session:retry(ClientInfo, Session) of
-        {ok, NSession} ->
-            NChannel = Channel#channel{session = NSession},
-            {ok, clean_timer(Name, NChannel)};
-        {ok, Publishes, Timeout, NSession} ->
-            NChannel = Channel#channel{session = NSession},
-            handle_out(publish, Publishes, reset_timer(Name, Timeout, NChannel))
-    end;
-handle_timeout(
-    _TRef,
-    _Name = expire_awaiting_rel,
-    Channel = #channel{conn_state = disconnected}
-) ->
-    {ok, Channel};
-handle_timeout(
-    _TRef,
-    Name = expire_awaiting_rel,
-    Channel = #channel{session = Session, clientinfo = ClientInfo}
-) ->
-    case emqx_session:expire(ClientInfo, awaiting_rel, Session) of
-        {ok, NSession} ->
-            NChannel = Channel#channel{session = NSession},
-            {ok, clean_timer(Name, NChannel)};
-        {ok, Timeout, NSession} ->
-            NChannel = Channel#channel{session = NSession},
-            {ok, reset_timer(Name, Timeout, NChannel)}
+    case emqx_session:handle_timeout(ClientInfo, Name, Session) of
+        {ok, [], NSession} ->
+            {ok, Channel#channel{session = NSession}};
+        {ok, Replies, NSession} ->
+            handle_out(publish, Replies, Channel#channel{session = NSession})
     end;
 handle_timeout(_TRef, expire_session, Channel) ->
     shutdown(expired, Channel);
@@ -1391,18 +1369,11 @@ ensure_timer(Name, Time, Channel = #channel{timers = Timers}) ->
 reset_timer(Name, Channel) ->
     ensure_timer(Name, clean_timer(Name, Channel)).
 
-reset_timer(Name, Time, Channel) ->
-    ensure_timer(Name, Time, clean_timer(Name, Channel)).
-
 clean_timer(Name, Channel = #channel{timers = Timers}) ->
     Channel#channel{timers = maps:remove(Name, Timers)}.
 
 interval(keepalive, #channel{keepalive = KeepAlive}) ->
     emqx_keepalive:info(interval, KeepAlive);
-interval(retry_delivery, #channel{session = Session}) ->
-    emqx_session:info(retry_interval, Session);
-interval(expire_awaiting_rel, #channel{session = Session}) ->
-    emqx_session:info(await_rel_timeout, Session);
 interval(expire_session, #channel{conninfo = ConnInfo}) ->
     maps:get(expiry_interval, ConnInfo);
 interval(will_message, #channel{will_msg = WillMsg}) ->
@@ -2053,22 +2024,15 @@ maybe_resume_session(#channel{resuming = false}) ->
     ignore;
 maybe_resume_session(#channel{
     session = Session,
-    resuming = true,
-    pendings = Pendings,
+    resuming = ReplayContext,
     clientinfo = ClientInfo
 }) ->
-    {ok, Publishes, Session1} = emqx_session:replay(ClientInfo, Session),
-    case emqx_session:deliver(ClientInfo, Pendings, Session1) of
-        {ok, Session2} ->
-            {ok, Publishes, Session2};
-        {ok, More, Session2} ->
-            {ok, lists:append(Publishes, More), Session2}
-    end.
+    emqx_session:replay(ClientInfo, ReplayContext, Session).
 
 %%--------------------------------------------------------------------
 %% Maybe Shutdown the Channel
 
-maybe_shutdown(Reason, Channel = #channel{conninfo = ConnInfo}) ->
+maybe_shutdown(Reason, _Intent = idle, Channel = #channel{conninfo = ConnInfo}) ->
     case maps:get(expiry_interval, ConnInfo) of
         ?EXPIRE_INTERVAL_INFINITE ->
             {ok, Channel};
@@ -2076,7 +2040,9 @@ maybe_shutdown(Reason, Channel = #channel{conninfo = ConnInfo}) ->
             {ok, ensure_timer(expire_session, I, Channel)};
         _ ->
             shutdown(Reason, Channel)
-    end.
+    end;
+maybe_shutdown(Reason, _Intent = shutdown, Channel) ->
+    shutdown(Reason, Channel).
 
 %%--------------------------------------------------------------------
 %% Parse Topic Filters

commit 522302fee1c7817049715a200e102a44ded1aa94

fix(cm): bring back pre-v5.3.0 compat in `takeover_session_begin/1`
Which was accidentally broken in bf164175.

apps/emqx/src/emqx_cm.erl
@@ -298,9 +298,9 @@ takeover_session_begin(ClientId) ->
 
 takeover_session_begin(ClientId, ChanPid) when is_pid(ChanPid) ->
     case takeover_session(ClientId, ChanPid) of
-        {living, ConnMod, Session} ->
+        {living, ConnMod, ChanPid, Session} ->
             {ok, Session, {ConnMod, ChanPid}};
-        none ->
+        _ ->
             none
     end;
 takeover_session_begin(_ClientId, undefined) ->
@@ -368,13 +368,20 @@ do_takeover_begin(ClientId, ChanPid) when node(ChanPid) == node() ->
         ConnMod when is_atom(ConnMod) ->
             case request_stepdown({takeover, 'begin'}, ConnMod, ChanPid) of
                 {ok, Session} ->
-                    {living, ConnMod, Session};
+                    {living, ConnMod, ChanPid, Session};
                 {error, Reason} ->
                     error(Reason)
             end
     end;
 do_takeover_begin(ClientId, ChanPid) ->
-    wrap_rpc(emqx_cm_proto_v2:takeover_session(ClientId, ChanPid)).
+    case wrap_rpc(emqx_cm_proto_v2:takeover_session(ClientId, ChanPid)) of
+        %% NOTE: v5.3.0
+        {living, ConnMod, Session} ->
+            {living, ConnMod, ChanPid, Session};
+        %% NOTE: other versions
+        Res ->
+            Res
+    end.
 
 %% @doc Discard all the sessions identified by the ClientId.
 -spec discard_session(emqx_types:clientid()) -> ok.

apps/emqx/src/proto/emqx_cm_proto_v2.erl
@@ -65,8 +65,10 @@ get_chann_conn_mod(ClientId, ChanPid) ->
 
 -spec takeover_session(emqx_types:clientid(), emqx_cm:chan_pid()) ->
     none
-    | {expired | persistent, emqx_session:session()}
     | {living, _ConnMod :: atom(), emqx_cm:chan_pid(), emqx_session:session()}
+    %% NOTE: v5.3.0
+    | {living, _ConnMod :: atom(), emqx_session:session()}
+    | {expired | persistent, emqx_session:session()}
     | {badrpc, _}.
 takeover_session(ClientId, ChanPid) ->
     rpc:call(node(ChanPid), emqx_cm, takeover_session, [ClientId, ChanPid], ?T_TAKEOVER * 2).