6e5a3f8bb89ef4f761589db13d4097cadb8f9f43
Stefan Strigler <stefan.strigler@emqx.io>
2023-12-15 16:20:27 +0100

    test: fix mocked calls

--- apps/emqx_bridge/test/emqx_bridge_v1_compatibility_layer_SUITE.erl
+++ apps/emqx_bridge/test/emqx_bridge_v1_compatibility_layer_SUITE.erl
@@ -89,7 +89,7 @@ setup_mocks() ->
 
     catch meck:new(emqx_bridge_v2, MeckOpts),
     meck:expect(emqx_bridge_v2, bridge_v2_type_to_connector_type, 1, con_type()),
-    meck:expect(emqx_bridge_v2, bridge_v1_type_to_bridge_v2_type, 1, bridge_type()),
+    meck:expect(emqx_bridge_v2, bridge_v1_type_to_action_type, 1, bridge_type()),
     IsBridgeV2TypeFun = fun(Type) ->
         BridgeV2Type = bridge_type(),
         BridgeV2TypeBin = bridge_type_bin(),
