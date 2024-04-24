4f12d45436f6c4b524ff36010e66cac3eb36fd3b
zhongwencool <zhongwencool@gmail.com>
2024-04-24 15:34:11 +0800

    chore: provided more specific error for wrong import method

--- apps/emqx_auth_mnesia/src/emqx_authn_mnesia.erl
+++ apps/emqx_auth_mnesia/src/emqx_authn_mnesia.erl
@@ -527,6 +527,10 @@ find_password_hash(hash, User = #{<<"password_hash">> := PasswordHash}, _) ->
     {PasswordHash, maps:get(<<"salt">>, User, <<>>)};
 find_password_hash(plain, #{<<"password">> := Password}, Algorithm) ->
     emqx_authn_password_hashing:hash(Algorithm, Password);
+find_password_hash(hash, _User, _) ->
+    error("hash_import_required_password_hash_field");
+find_password_hash(plain, _User, _Algorithm) ->
+    error("plain_import_required_password_field");
 find_password_hash(_, _, _) ->
     error(bad_format).
 
