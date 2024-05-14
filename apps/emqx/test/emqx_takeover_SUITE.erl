bcbeadd4af04b1c44fd13778ab32ac5cff413512
Thales Macedo Garitezi <thalesmg@gmail.com>
2024-05-14 17:16:00 -0300

    test: stabilize flaky test

--- apps/emqx/test/emqx_takeover_SUITE.erl
+++ apps/emqx/test/emqx_takeover_SUITE.erl
@@ -1042,7 +1042,13 @@ assert_client_exit(Pid, v5, takenover) ->
     %% @ref: MQTT 5.0 spec [MQTT-3.1.4-3]
     ?assertReceive({'EXIT', Pid, {disconnected, ?RC_SESSION_TAKEN_OVER, _}});
 assert_client_exit(Pid, v3, takenover) ->
-    ?assertReceive({'EXIT', Pid, {shutdown, tcp_closed}}, 1_000, #{pid => Pid});
+    ?assertReceive(
+        {'EXIT', Pid, {shutdown, Reason}} when
+            Reason =:= tcp_closed orelse
+                Reason =:= closed,
+        1_000,
+        #{pid => Pid}
+    );
 assert_client_exit(Pid, v3, kicked) ->
     ?assertReceive({'EXIT', Pid, _}, 1_000, #{pid => Pid});
 assert_client_exit(Pid, v5, kicked) ->
