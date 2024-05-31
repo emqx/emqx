57741a41e94f0270b28c74b47dcee3b7d21b48be
Ilya Averyanov <av@rubybox.dev>
2024-05-31 22:27:31 +0300

    chore(tests): improve test stability

--- apps/emqx_management/test/emqx_mgmt_api_status_SUITE.erl
+++ apps/emqx_management/test/emqx_mgmt_api_status_SUITE.erl
@@ -53,6 +53,10 @@ groups() ->
 init_per_suite(Config) ->
     Apps = emqx_cth_suite:start(
         [
+            %% Dashboard implicitly loads full schema so we must make preparations for it
+            %% to be valid (see `emqx_cth_suite:default_appspec(emqx_conf, SuiteOpts)`).
+            %% Otherwise, restarting `emqx` will fail in `t_status_not_ok`
+            emqx_conf,
             emqx,
             emqx_management,
             emqx_mgmt_api_test_util:emqx_dashboard()
