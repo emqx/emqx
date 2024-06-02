8d7417d51c48ae2ba8348f45d37853b3964f2fe4
zmstone <zmstone@gmail.com>
2024-06-02 10:10:17 +0200

    chore: stop pinning supervisor in mix.exs

--- mix.exs
+++ mix.exs
@@ -102,8 +102,7 @@ defmodule EMQXUmbrella.MixProject do
       {:uuid, github: "okeuday/uuid", tag: "v2.0.6", override: true},
       {:quickrand, github: "okeuday/quickrand", tag: "v2.0.6", override: true},
       {:ra, "2.7.3", override: true},
-      {:mimerl, "1.2.0", override: true},
-      {:supervisor3, "1.1.12", override: true}
+      {:mimerl, "1.2.0", override: true}
     ] ++
       emqx_apps(profile_info, version) ++
       enterprise_deps(profile_info) ++ jq_dep() ++ quicer_dep()
