63f1856a2cf49116a80a07b4396e459c4263567e
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2024-06-13 15:19:57 +0200

    feat(ds): Dynamic backend registration

--- apps/emqx_durable_storage/src/emqx_ds_storage_layer.erl
+++ apps/emqx_durable_storage/src/emqx_ds_storage_layer.erl
@@ -55,7 +55,7 @@
 -export([init/1, format_status/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 
 %% internal exports:
--export([db_dir/1]).
+-export([db_dir/1, base_dir/0]).
 
 -export_type([
     gen_id/0,
@@ -889,13 +889,17 @@ rocksdb_open(Shard, Options) ->
             Error
     end.
 
+-spec base_dir() -> file:filename().
+base_dir() ->
+    application:get_env(?APP, db_data_dir, emqx:data_dir()).
+
 -spec db_dir(shard_id()) -> file:filename().
 db_dir({DB, ShardId}) ->
-    filename:join([emqx_ds:base_dir(), DB, binary_to_list(ShardId)]).
+    filename:join([base_dir(), DB, binary_to_list(ShardId)]).
 
 -spec checkpoints_dir(shard_id()) -> file:filename().
 checkpoints_dir({DB, ShardId}) ->
-    filename:join([emqx_ds:base_dir(), DB, checkpoints, binary_to_list(ShardId)]).
+    filename:join([base_dir(), DB, checkpoints, binary_to_list(ShardId)]).
 
 -spec checkpoint_dir(shard_id(), _Name :: file:name()) -> file:filename().
 checkpoint_dir(ShardId, Name) ->
