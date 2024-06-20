5e2693c9b48665b6c0b50fe21e1ca65291e55a2d
firest <enlanubo@gmail.com>
2024-06-20 19:39:02 +0800

    feat(oidc): implement session management

--- apps/emqx_dashboard_sso/src/emqx_dashboard_sso_sup.erl
+++ apps/emqx_dashboard_sso/src/emqx_dashboard_sso_sup.erl
@@ -6,17 +6,24 @@
 
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/0, start_child/2, stop_child/1]).
 
 -export([init/1]).
 
--define(CHILD(I, ShutDown), {I, {I, start_link, []}, permanent, ShutDown, worker, [I]}).
+-define(CHILD(I, Args), {I, {I, start_link, Args}, permanent, 5000, worker, [I]}).
+-define(CHILD(I), ?CHILD(I, [])).
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+start_child(Mod, Args) ->
+    supervisor:start_child(?MODULE, ?CHILD(Mod, Args)).
+
+stop_child(Mod) ->
+    supervisor:terminate_child(?MODULE, Mod).
+
 init([]) ->
     {ok,
         {{one_for_one, 5, 100}, [
-            ?CHILD(emqx_dashboard_sso_manager, 5000)
+            ?CHILD(emqx_dashboard_sso_manager)
         ]}}.
