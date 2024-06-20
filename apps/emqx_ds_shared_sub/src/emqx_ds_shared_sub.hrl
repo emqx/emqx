d0cff63ed6359c87c64861ca8c452bb33f332517
Ilya Averyanov <av@rubybox.dev>
2024-06-20 15:18:47 +0300

    feat(queue): remove unused hrl-guard

--- apps/emqx_ds_shared_sub/src/emqx_ds_shared_sub.hrl
+++ apps/emqx_ds_shared_sub/src/emqx_ds_shared_sub.hrl
@@ -2,9 +2,4 @@
 %% Copyright (c) 2024 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%--------------------------------------------------------------------
 
--ifndef(EMQX_DS_SHARED_SUB_HRL).
--define(EMQX_DS_SHARED_SUB_HRL, true).
-
 -define(gproc_id(ID), {n, l, ID}).
-
--endif.
