795d280861747767fd1025751525a3cad99462fe
Thales Macedo Garitezi <thalesmg@gmail.com>
2024-06-27 11:57:44 -0300

    fix(connector api): handle bad tls config file conversion errors
    Fixes https://emqx.atlassian.net/browse/EMQX-12581

--- apps/emqx_connector/test/emqx_connector_api_SUITE.erl
+++ apps/emqx_connector/test/emqx_connector_api_SUITE.erl
@@ -718,6 +718,37 @@ t_create_with_bad_name(Config) ->
     ?assertMatch(#{<<"kind">> := <<"validation_error">>}, Msg),
     ok.
 
+%% Checks that we correctly handle `throw({bad_ssl_config, _})' from
+%% `emqx_connector:convert_certs' and massage the error message accordingly.
+t_create_with_bad_tls_files(Config) ->
+    ConnectorName = atom_to_binary(?FUNCTION_NAME),
+    Conf0 = ?KAFKA_CONNECTOR(ConnectorName),
+    Conf = Conf0#{<<"ssl">> => #{<<"cacertfile">> => <<"bad_pem_file">>}},
+    ?check_trace(
+        begin
+            {ok, 400, #{
+                <<"message">> := Msg0
+            }} = request_json(
+                post,
+                uri(["connectors"]),
+                Conf,
+                Config
+            ),
+            ?assertMatch(
+                #{
+                    <<"kind">> := <<"validation_error">>,
+                    <<"reason">> := <<"bad_ssl_config">>,
+                    <<"file_read_error">> := <<"enoent">>,
+                    <<"bad_fields">> := [[<<"cacertfile">>]]
+                },
+                json(Msg0)
+            ),
+            ok
+        end,
+        []
+    ),
+    ok.
+
 t_actions_field(Config) ->
     Name = ?CONNECTOR_NAME,
     ?assertMatch(
