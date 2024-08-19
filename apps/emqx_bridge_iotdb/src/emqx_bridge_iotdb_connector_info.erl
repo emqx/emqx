75b5d400730f1e7c70b783aa2b0195b38325ff87
firest <enlanubo@gmail.com>
2024-08-19 14:16:49 +0800

    feat(iotdb): introduced a thrift driver for IotDB connector

--- apps/emqx_bridge_iotdb/src/emqx_bridge_iotdb_connector_info.erl
+++ apps/emqx_bridge_iotdb/src/emqx_bridge_iotdb_connector_info.erl
@@ -16,6 +16,10 @@
     api_schema/1
 ]).
 
+-define(CONNECTOR, emqx_bridge_iotdb_connector).
+-define(DRIVER_REST, "rest").
+-define(DRIVER_THRIFT, "thrift").
+
 type_name() ->
     iotdb.
 
@@ -31,7 +35,7 @@ config_transform_module() ->
 config_schema() ->
     {iotdb,
         hoconsc:mk(
-            hoconsc:map(name, hoconsc:ref(emqx_bridge_iotdb_connector, config)),
+            hoconsc:map(name, hoconsc:union(fun driver_union_selector/1)),
             #{
                 desc => <<"IoTDB Connector Config">>,
                 required => false
@@ -42,6 +46,37 @@ schema_module() ->
     emqx_bridge_iotdb_connector.
 
 api_schema(Method) ->
-    emqx_connector_schema:api_ref(
-        emqx_bridge_iotdb_connector, <<"iotdb">>, Method
-    ).
+    {<<"iotdb">>, hoconsc:union(mk_api_union_selector(Method))}.
+
+driver_union_selector(all_union_members) ->
+    [
+        ref(?DRIVER_REST, "config"),
+        ref(?DRIVER_THRIFT, "config")
+    ];
+driver_union_selector({value, Value}) ->
+    case Value of
+        #{<<"driver">> := <<"thrift">>} ->
+            [ref(?DRIVER_THRIFT, "config")];
+        _ ->
+            [ref(?DRIVER_REST, "config")]
+    end.
+
+mk_api_union_selector(Method) ->
+    fun
+        (all_union_members) ->
+            [
+                ref(?DRIVER_REST, Method),
+                ref(?DRIVER_THRIFT, Method)
+            ];
+        ({value, Value}) ->
+            case Value of
+                #{<<"driver">> := <<"thrift">>} ->
+                    [ref(?DRIVER_THRIFT, Method)];
+                _ ->
+                    [ref(?DRIVER_REST, Method)]
+            end
+    end.
+
+ref(Driver, Field) ->
+    Name = Field ++ "_" ++ Driver,
+    hoconsc:ref(?CONNECTOR, Name).
