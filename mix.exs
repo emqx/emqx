f5bc4e6600d43759f7656d57da236753103a56e5
Thales Macedo Garitezi <thalesmg@gmail.com>
2024-08-29 17:52:03 -0300

    refactor(mix): move kafka dependencies to `UMP.common_dep`

--- mix.exs
+++ mix.exs
@@ -272,6 +272,18 @@ defmodule EMQXUmbrella.MixProject do
   def common_dep(:influxdb),
     do: {:influxdb, github: "emqx/influxdb-client-erl", tag: "1.1.13", override: true}
 
+  def common_dep(:wolff), do: {:wolff, "3.0.4"}
+  def common_dep(:brod_gssapi), do: {:brod_gssapi, "0.1.3"}
+
+  def common_dep(:kafka_protocol),
+    do: {:kafka_protocol, "4.1.8", override: true}
+
+  def common_dep(:brod), do: {:brod, github: "kafka4beam/brod", tag: "3.18.0"}
+  ## TODO: remove `mix.exs` from `wolff` and remove this override
+  ## TODO: remove `mix.exs` from `pulsar` and remove this override
+  def common_dep(:snappyer), do: {:snappyer, "1.2.9", override: true}
+  def common_dep(:crc32cer), do: {:crc32cer, "0.1.8", override: true}
+
   ###############################################################################################
   # BEGIN DEPRECATED FOR MIX BLOCK
   # These should be removed once we fully migrate to mix
@@ -387,13 +399,13 @@ defmodule EMQXUmbrella.MixProject do
     [
       {:hstreamdb_erl,
        github: "hstreamdb/hstreamdb_erl", tag: "0.5.18+v0.18.1+ezstd-v1.0.5-emqx1"},
-      {:influxdb, github: "emqx/influxdb-client-erl", tag: "1.1.13", override: true},
-      {:wolff, "3.0.4"},
-      {:kafka_protocol, tag: "4.1.8", override: true},
-      {:brod_gssapi, "0.1.3", override: true},
-      {:brod, github: "kafka4beam/brod", tag: "3.18.0"},
-      {:snappyer, "1.2.9", override: true},
-      {:crc32cer, "0.1.8", override: true},
+      common_dep(:influxdb),
+      common_dep(:wolff),
+      common_dep(:kafka_protocol),
+      common_dep(:brod_gssapi),
+      common_dep(:brod),
+      common_dep(:snappyer),
+      common_dep(:crc32cer),
       {:opentsdb, github: "emqx/opentsdb-client-erl", tag: "v0.5.1", override: true},
       {:greptimedb,
        github: "GreptimeTeam/greptimedb-ingester-erl", tag: "v0.1.8", override: true},
