018d72892d4e3278797cfb271e8adf1f3e734bbb
zmstone <zmstone@gmail.com>
2024-09-23 17:28:25 +0200

    build: always check mix style

--- scripts/git-hook-pre-commit.sh
+++ scripts/git-hook-pre-commit.sh
@@ -12,12 +12,14 @@ if [[ "${files_dirty}" == '' ]] && [[ "${files_cached}" == '' ]]; then
     exit 0
 fi
 files="$(echo -e "${files_dirty} \n ${files_cached}" | xargs)"
+
+# mix format check is quite fast
+which mix && mix format --check-formatted
+
 if [ "${ERLFMT_WRITE:-false}" = 'true' ]; then
     # shellcheck disable=SC2086
     ./scripts/erlfmt -w $files
 else
-    # mix format check is quite fast
-    which mix && mix format --check-formatted
     # shellcheck disable=SC2086
     if ! (./scripts/erlfmt -c $files); then
         echo "EXECUTE 'make fmt-diff' to fix" >&2
