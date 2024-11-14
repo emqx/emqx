f0024b912e7341b62fca2ea2baac7b76d7686480
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2024-11-14 10:53:52 -0300

    fix(resource metrics): throttle resource metrics handling error message
    If for whatever reason handling a "hot" metric such as `matched` breaks, it's likely that
    logs will be flooded with identical messages.

--- apps/emqx_conf/src/emqx_conf_schema.erl
+++ apps/emqx_conf/src/emqx_conf_schema.erl
@@ -84,6 +84,7 @@
     dropped_msg_due_to_mqueue_is_full,
     external_broker_crashed,
     failed_to_retain_message,
+    handle_resource_metrics_failed,
     socket_receive_paused_by_rate_limit,
     unrecoverable_resource_error
 ]).
