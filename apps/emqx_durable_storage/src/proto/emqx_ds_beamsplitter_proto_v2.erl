fee94ca3ab85d3ce7cb69620ac692b251d9970fc
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2024-11-24 20:26:35 +0100

    feat(sessds): Add serialization token to the beamsplitter BPAPI

--- apps/emqx_durable_storage/src/proto/emqx_ds_beamsplitter_proto_v2.erl
+++ apps/emqx_durable_storage/src/proto/emqx_ds_beamsplitter_proto_v2.erl
@@ -13,31 +13,28 @@
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
 %%--------------------------------------------------------------------
--module(emqx_ds_beamsplitter_proto_v1).
+-module(emqx_ds_beamsplitter_proto_v2).
 
 -behavior(emqx_bpapi).
 -include_lib("emqx_utils/include/bpapi.hrl").
 
 %% API:
--export([dispatch/2]).
+-export([dispatch/3]).
 
 %% behavior callbacks:
--export([introduced_in/0, deprecated_since/0]).
+-export([introduced_in/0]).
 
 %%================================================================================
 %% API functions
 %%================================================================================
 
--spec dispatch(node(), emqx_ds_beamformer:beam()) -> true.
-dispatch(Node, Beam) ->
-    emqx_rpc:cast(Node, emqx_ds_beamformer, do_dispatch, [Beam]).
+-spec dispatch(_SerializationToken, node(), emqx_ds_beamformer:beam()) -> true.
+dispatch(SerializationToken, Node, Beam) ->
+    emqx_rpc:cast(SerializationToken, Node, emqx_ds_beamformer, do_dispatch, [Beam]).
 
 %%================================================================================
 %% behavior callbacks
 %%================================================================================
 
 introduced_in() ->
-    "5.8.0".
-
-deprecated_since() ->
     "5.8.3".
