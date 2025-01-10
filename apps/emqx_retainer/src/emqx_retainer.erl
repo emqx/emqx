4af324a78b00ca4b076d144b31be40277cbe89c2
firest <enlanubo@gmail.com>
2025-01-10 18:06:55 +0800

    fix(limiter): minor improvements and rebase bug fixes

--- apps/emqx_retainer/src/emqx_retainer.erl
+++ apps/emqx_retainer/src/emqx_retainer.erl
@@ -312,7 +312,7 @@ handle_call({update_config, _NewConf, _OldConf}, _From, #{is_started := false} =
     {reply, ok, State};
 handle_call({update_config, NewConf, OldConf}, _From, #{is_started := true} = State) ->
     State2 = update_config(State, NewConf, OldConf),
-    ok = emqx_retainer_dispatcher:refresh_limiter(NewConf),
+    ok = emqx_retainer_dispatcher:refresh_limiter(),
     ok = emqx_retainer_publisher:refresh_limits(NewConf),
     {reply, ok, State2};
 handle_call(start, _From, #{is_started := IsStarted} = State0) ->
