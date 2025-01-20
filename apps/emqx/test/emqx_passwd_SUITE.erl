8203f2bf856c680c40f1aa51075071c1974b0baf
Andrew Mayorov <encube.ul@gmail.com>
2025-01-20 18:49:30 +0100

    fix(emqx): ignore case in hexadecimal password hash comparisons

--- apps/emqx/test/emqx_passwd_SUITE.erl
+++ apps/emqx/test/emqx_passwd_SUITE.erl
@@ -96,6 +96,8 @@ t_hash(_) ->
     ),
     Sha512 = emqx_passwd:hash({sha512, Salt, suffix}, Password),
     true = emqx_passwd:check_pass({sha512, Salt, suffix}, Sha512, Password),
+    %% Case is ignored for comparisons.
+    true = emqx_passwd:check_pass({sha512, Salt, suffix}, string:uppercase(Sha512), Password),
     false = emqx_passwd:check_pass({sha512, Salt, suffix}, Sha512, WrongPassword),
     ?assertEqual(
         emqx_passwd:hash_data(sha512, Password),
@@ -120,6 +122,10 @@ t_hash(_) ->
     >>,
     _Pbkdf2 = emqx_passwd:hash({pbkdf2, sha, Pbkdf2Salt, 2, 32}, Password),
     true = emqx_passwd:check_pass({pbkdf2, sha, Pbkdf2Salt, 2, 32}, Pbkdf2, Password),
+    %% Case is ignored for comparisons.
+    true = emqx_passwd:check_pass(
+        {pbkdf2, sha, Pbkdf2Salt, 2, 32}, string:uppercase(Pbkdf2), Password
+    ),
     false = emqx_passwd:check_pass({pbkdf2, sha, Pbkdf2Salt, 2, 32}, Pbkdf2, WrongPassword),
     ?assertException(
         error, _, emqx_passwd:check_pass({pbkdf2, sha, Pbkdf2Salt, 2, BadDKlen}, Pbkdf2, Password)
