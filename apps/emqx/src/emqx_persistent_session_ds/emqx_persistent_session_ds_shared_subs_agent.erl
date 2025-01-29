6bf7a791967eb070be708358490d6df0f29f6d0f
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2025-01-29 23:34:40 +0100

    fix(dssharedsub): Fix dialyzer warnings

--- apps/emqx/src/emqx_persistent_session_ds/emqx_persistent_session_ds_shared_subs_agent.erl
+++ apps/emqx/src/emqx_persistent_session_ds/emqx_persistent_session_ds_shared_subs_agent.erl
@@ -17,7 +17,7 @@
     start_time := emqx_ds:time()
 }.
 
--type t() :: term().
+-type state() :: term().
 -type share_topic_filter() :: emqx_persistent_session_ds:share_topic_filter().
 
 -type opts() :: #{
@@ -83,16 +83,20 @@
 %% Behaviour
 %%--------------------------------------------------------------------
 
--callback new(opts()) -> t().
--callback open([{share_topic_filter(), subscription()}], opts()) -> t().
--callback pre_subscribe(t(), share_topic_filter(), emqx_types:subopts()) -> ok | {error, term()}.
--callback has_subscription(t(), subscription_id()) -> boolean().
--callback has_subscriptions(t()) -> boolean().
--callback on_subscribe(t(), subscription_id(), share_topic_filter(), emqx_types:subopts()) -> t().
--callback on_unsubscribe(t(), subscription_id()) -> t().
--callback on_info(t(), subscription_id(), term()) -> {[event()], t()}.
--callback on_disconnect(t(), #{subscription_id() => [stream_progress()]}) -> t().
--callback on_stream_progress(t(), #{subscription_id() => [stream_progress()]}) -> t().
+-callback new(opts()) -> state().
+-callback open([{share_topic_filter(), subscription()}], opts()) -> state().
+-callback pre_subscribe(state(), share_topic_filter(), emqx_types:subopts()) ->
+    ok | {error, term()}.
+-callback has_subscription(state(), subscription_id()) -> boolean().
+-callback has_subscriptions(state()) -> boolean().
+-callback on_subscribe(state(), subscription_id(), share_topic_filter(), emqx_types:subopts()) ->
+    state().
+-callback on_unsubscribe(state(), subscription_id()) -> state().
+-callback on_info(state(), subscription_id(), term()) -> {[event()], state()}.
+-callback on_disconnect(state(), #{subscription_id() => [stream_progress()]}) -> state().
+-callback on_stream_progress(state(), #{subscription_id() => [stream_progress()]}) -> state().
+
+-type t() :: {module(), state()}.
 
 %%--------------------------------------------------------------------
 %% API
@@ -100,43 +104,44 @@
 
 -spec new(opts()) -> t().
 new(Opts) ->
-    ?shared_subs_agent:new(Opts).
+    {?shared_subs_agent, ?shared_subs_agent:new(Opts)}.
 
 -spec open([{share_topic_filter(), subscription()}], opts()) -> t().
 open(Topics, Opts) ->
-    ?shared_subs_agent:open(Topics, Opts).
+    {?shared_subs_agent, ?shared_subs_agent:open(Topics, Opts)}.
 
 -spec pre_subscribe(t(), share_topic_filter(), emqx_types:subopts()) -> ok | {error, term()}.
-pre_subscribe(Agent, ShareTopicFilter, SubOpts) ->
-    ?shared_subs_agent:pre_subscribe(Agent, ShareTopicFilter, SubOpts).
+pre_subscribe({Mod, Agent}, ShareTopicFilter, SubOpts) ->
+    Mod:pre_subscribe(Agent, ShareTopicFilter, SubOpts).
 
 -spec has_subscription(t(), subscription_id()) -> boolean().
-has_subscription(Agent, SubscriptionId) ->
-    ?shared_subs_agent:has_subscription(Agent, SubscriptionId).
+has_subscription({Mod, Agent}, SubscriptionId) ->
+    Mod:has_subscription(Agent, SubscriptionId).
 
 -spec has_subscriptions(t()) -> boolean().
-has_subscriptions(Agent) ->
-    ?shared_subs_agent:has_subscriptions(Agent).
+has_subscriptions({Mod, Agent}) ->
+    Mod:has_subscriptions(Agent).
 
 -spec on_subscribe(t(), subscription_id(), share_topic_filter(), emqx_types:subopts()) -> t().
-on_subscribe(Agent, SubscriptionId, ShareTopicFilter, SubOpts) ->
-    ?shared_subs_agent:on_subscribe(Agent, SubscriptionId, ShareTopicFilter, SubOpts).
+on_subscribe({Mod, Agent}, SubscriptionId, ShareTopicFilter, SubOpts) ->
+    {Mod, Mod:on_subscribe(Agent, SubscriptionId, ShareTopicFilter, SubOpts)}.
 
 -spec on_unsubscribe(t(), subscription_id()) -> t().
-on_unsubscribe(Agent, SubscriptionId) ->
-    ?shared_subs_agent:on_unsubscribe(Agent, SubscriptionId).
+on_unsubscribe({Mod, Agent}, SubscriptionId) ->
+    {Mod, Mod:on_unsubscribe(Agent, SubscriptionId)}.
 
 -spec on_disconnect(t(), #{subscription_id() => [stream_progress()]}) -> t().
-on_disconnect(Agent, StreamProgresses) ->
-    ?shared_subs_agent:on_disconnect(Agent, StreamProgresses).
+on_disconnect({Mod, Agent}, StreamProgresses) ->
+    {Mod, Mod:on_disconnect(Agent, StreamProgresses)}.
 
 -spec on_stream_progress(t(), #{subscription_id() => [stream_progress()]}) -> t().
-on_stream_progress(Agent, StreamProgress) ->
-    ?shared_subs_agent:on_stream_progress(Agent, StreamProgress).
+on_stream_progress({Mod, Agent}, StreamProgress) ->
+    {Mod, Mod:on_stream_progress(Agent, StreamProgress)}.
 
 -spec on_info(t(), subscription_id(), term()) -> {[event()], t()}.
-on_info(Agent, SubscriptionId, Info) ->
-    ?shared_subs_agent:on_info(Agent, SubscriptionId, Info).
+on_info({Mod, Agent0}, SubscriptionId, Info) ->
+    {Events, Agent} = Mod:on_info(Agent0, SubscriptionId, Info),
+    {Events, {Mod, Agent}}.
 
 -spec send(pid() | reference(), subscription_id(), term()) -> term().
 send(Dest, SubscriptionId, Msg) ->
