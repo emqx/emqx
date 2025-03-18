01546335b19cc9963f02bf0d305fefbd10abac92
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2025-03-18 13:12:16 -0300

    chore: bump `emqtt` -> 1.14.4
    Basically fixes flaky tests:
    
    - https://github.com/emqx/emqx/pull/14861
    - https://github.com/emqx/emqx/actions/runs/13928128336/job/38980458444?pr=14894#step:5:343
    
    See also:
    - https://github.com/emqx/emqtt/pull/279
    - https://github.com/emqx/emqtt/pull/280

--- mix.exs
+++ mix.exs
@@ -246,7 +246,7 @@ defmodule EMQXUmbrella.MixProject do
   def common_dep(:emqtt),
     do:
       {:emqtt,
-       github: "emqx/emqtt", tag: "1.14.2", override: true, system_env: maybe_no_quic_env()}
+       github: "emqx/emqtt", tag: "1.14.4", override: true, system_env: maybe_no_quic_env()}
 
   def common_dep(:typerefl),
     do: {:typerefl, github: "ieQu1/typerefl", tag: "0.9.6", override: true}
