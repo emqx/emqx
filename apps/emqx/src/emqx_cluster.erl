5d8777ef13e16cc15f1731fa6513b37a8d6698d9
zmstone <zmstone@gmail.com>
2025-04-05 11:35:47 +0200

    refactor: check cluster join permission before join
    pervisouly (merged to 5.9 but not released yet), the allow-join check
    was done on the join requesting node, this may prevent node from
    rolling upgrade without a license deployed first.
    
    now the can_i_join rpc call will check if the requesting node is allowed
    to join an existing node (with a valid license). The requesting node
    will then copy the license after joined, so there is no need to add
    license before jonning a licensed cluster.

--- apps/emqx/src/emqx_cluster.erl
+++ apps/emqx/src/emqx_cluster.erl
@@ -24,6 +24,11 @@
     ensure_singleton_mode/0
 ]).
 
+%% RPC callback functions
+-export([
+    can_i_join/1
+]).
+
 -define(CLUSTER_MODE_NORMAL, normal).
 -define(CLUSTER_MODE_SINGLE, singleton).
 
@@ -34,12 +39,15 @@
 -define(DEFAULT_MODE, ?CLUSTER_MODE_SINGLE).
 -endif.
 
-join(Node) ->
-    case is_single_node_mode() of
-        true ->
-            {error, single_node_mode};
-        false ->
-            ekka:join(Node)
+join(PeerNode) ->
+    %% Local node starts with default license (singleton mode),
+    %% But the peer node(s) may have a license which allows local node to join.
+    %% So we do not check the license locally.
+    case check_permission(PeerNode) of
+        ok ->
+            ekka:join(PeerNode);
+        {error, Message} ->
+            {error, Message}
     end.
 
 leave() ->
@@ -47,6 +55,30 @@ leave() ->
 
 force_leave(Node) ->
     ekka:force_leave(Node).
+
+check_permission(PeerNode) ->
+    %% This call happens before clustered, so it's not possible to
+    %% check peer node's bpapi versions.
+    try
+        emqx_cluster_proto_v1:can_i_join(node(), PeerNode)
+    catch
+        error:{exception, undef, [{emqx_cluster, can_i_join, _, _}]} ->
+            %% The peer node is older than 5.9.0
+            %% This can happen during rolling upgrade.
+            ok
+    end.
+
+%% @doc Check if the requesting node is allowed to join the cluster.
+%% Called by license checker for community license.
+-spec can_i_join(node()) -> ok | {error, string()}.
+can_i_join(_RequestingNode) ->
+    case is_single_node_mode() of
+        true ->
+            Msg = lists:flatten(io_lib:format("Node ~s has a single node license", [node()])),
+            {error, Msg};
+        false ->
+            ok
+    end.
 
 is_single_node_mode() ->
     case application:get_env(emqx, cluster_mode, ?DEFAULT_MODE) of
