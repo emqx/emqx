bf5ff96b8fec847e46591669bb65b1ef5ee982dc
zmstone <zmstone@gmail.com>
2025-04-08 22:53:53 +0200

    ci: fix license key for old version EMQX

--- scripts/test/cluster-smoke-test.sh
+++ scripts/test/cluster-smoke-test.sh
@@ -14,14 +14,14 @@ cd -P -- "$(dirname -- "$0")"
 
 ## can join cluster by default
 echo '======== Allow clustering with "evaluation" license'
-env EMQX_LICENSE__KEY1='evaluation' \
-    EMQX_LICENSE__KEY2='default' \
+env ICENSE_KEY1='evaluation' \
+    ICENSE_KEY2='default' \
 ./start-two-nodes-in-docker.sh "${DOCKER_IMAGE_TAG}"
 
 ## cannot join cluster if node1 does not have a license
 echo '======== Do not allow clustering with "default" license'
-! env EMQX_LICENSE__KEY1='default' \
-      EMQX_LICENSE__KEY2='default' \
+! env LICENSE_KEY1='default' \
+      LICENSE_KEY2='default' \
   ./start-two-nodes-in-docker.sh "${DOCKER_IMAGE_TAG}" || {
     echo "ERROR: 'default' license allowed cluster join, but expected to fail"
     exit 1
@@ -30,8 +30,8 @@ echo '======== Do not allow clustering with "default" license'
 echo '======== Allow clustering with "default" license if peer node is before 5.9'
 ## new (>= 5.9.0) can join cluster with old (< 5.9.0)
 IMAGE1='emqx/emqx-enterprise:5.8.6'
-env EMQX_LICENSE__KEY1='default' \
-    EMQX_LICENSE__KEY2='default' \
+env LICENSE_KEY1='default' \
+    LICENSE_KEY2='default' \
 ./start-two-nodes-in-docker.sh "${IMAGE1}" "${DOCKER_IMAGE_TAG}"
 
 ## Clenaup
