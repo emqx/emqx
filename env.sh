a0637699fc8de5d26352fcc6d7e3a13948421650
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2025-04-15 15:35:08 -0300

    chore: bump builder image and OTP -> 27.2-3

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 # https://github.com/emqx/emqx-builder
-export EMQX_BUILDER_VSN=5.5-0
-export OTP_VSN=27.2-2
-export ELIXIR_VSN=1.17.3
+export EMQX_BUILDER_VSN=5.5-2
+export OTP_VSN=27.2-3
+export ELIXIR_VSN=1.18.3
 export EMQX_BUILDER=ghcr.io/emqx/emqx-builder/${EMQX_BUILDER_VSN}:${ELIXIR_VSN}-${OTP_VSN}-ubuntu24.04
 export EMQX_DOCKER_BUILD_FROM=ghcr.io/emqx/emqx-builder/${EMQX_BUILDER_VSN}:${ELIXIR_VSN}-${OTP_VSN}-debian12
 export EMQX_DOCKER_RUN_FROM=debian:12-slim
--- scripts/buildx.sh
+++ scripts/buildx.sh
@@ -36,7 +36,7 @@ help() {
     echo ""
     echo "--builder <BUILDER>:"
     echo "    Docker image to use for building"
-    echo "    E.g. ghcr.io/emqx/emqx-builder/5.5-0:1.17.3-27.2-2-debian12"
+    echo "    E.g. ghcr.io/emqx/emqx-builder/5.5-2:1.18.3-27.2-3-debian12"
     echo "    For hot upgrading tar.gz, specify a builder image with the same OS distribution as the running one."
     echo "    Specifically, for EMQX's docker containers hot upgrading, please use the debian12-based builder. "
 }
