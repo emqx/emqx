64b3f19a7a9ed8bfc2977f540513b256f09ba62e
Andrew Mayorov <encube.ul@gmail.com>
2025-04-18 23:06:59 +0200

    test(clink): verify pre-existing route repl in smoke test

--- apps/emqx_cluster_link/test/emqx_cluster_link_SUITE.erl
+++ apps/emqx_cluster_link/test/emqx_cluster_link_SUITE.erl
@@ -164,7 +164,6 @@ nodes_target(Config) ->
 t_message_forwarding('init', Config) ->
     SourceNodes = emqx_cth_cluster:start(mk_source_cluster(?FUNCTION_NAME, Config)),
     TargetNodes = emqx_cth_cluster:start(mk_target_cluster(?FUNCTION_NAME, Config)),
-    _Apps = start_cluster_link(SourceNodes ++ TargetNodes, Config),
     ok = snabbkaffe:start_trace(),
     [
         {source_nodes, SourceNodes},
@@ -177,28 +176,67 @@ t_message_forwarding('end', Config) ->
     ok = emqx_cth_cluster:stop(?config(target_nodes, Config)).
 
 t_message_forwarding(Config) ->
-    [SourceNode1 | _] = nodes_source(Config),
-    [TargetNode1, TargetNode2 | _] = nodes_target(Config),
-
-    SourceC1 = emqx_cluster_link_cth:connect_client("t_message_forwarding", SourceNode1),
+    IsShared = ?config(is_shared_sub, Config),
+    SourceNodes = [SourceNode1 | _] = nodes_source(Config),
+    TargetNodes = [TargetNode1, TargetNode2 | _] = nodes_target(Config),
+    %% Connect client to the target cluster.
     TargetC1 = emqx_cluster_link_cth:connect_client("t_message_forwarding1", TargetNode1),
     TargetC2 = emqx_cluster_link_cth:connect_client("t_message_forwarding2", TargetNode2),
-    IsShared = ?config(is_shared_sub, Config),
-
-    {ok, _, _} = emqtt:subscribe(TargetC1, maybe_shared_topic(IsShared, <<"t/+">>), qos1),
-    {ok, _, _} = emqtt:subscribe(TargetC2, maybe_shared_topic(IsShared, <<"t/#">>), qos1),
-    {ok, _} = ?block_until(#{?snk_kind := "cluster_link_route_sync_complete"}),
-    {ok, _} = emqtt:publish(SourceC1, <<"t/42">>, <<"hello">>, qos1),
-    ?assertReceive(
-        {publish, #{topic := <<"t/42">>, payload := <<"hello">>, client_pid := TargetC1}}
-    ),
-    ?assertReceive(
-        {publish, #{topic := <<"t/42">>, payload := <<"hello">>, client_pid := TargetC2}}
-    ),
-    ?assertNotReceive({publish, _Message = #{}}),
-    ok = emqtt:stop(SourceC1),
-    ok = emqtt:stop(TargetC1),
-    ok = emqtt:stop(TargetC2).
+    %% Connect a client to the source cluster.
+    SourceC1 = emqx_cluster_link_cth:connect_client("t_message_forwarding", SourceNode1),
+    ?check_trace(
+        try
+            %% Subscribe both to different topics.
+            T11 = maybe_shared_topic(IsShared, <<"t1/+">>),
+            T12 = maybe_shared_topic(IsShared, <<"t1/#">>),
+            {ok, _, _} = emqtt:subscribe(TargetC1, T11, qos1),
+            {ok, _, _} = emqtt:subscribe(TargetC2, T12, qos1),
+            %% Start cluster link, existing routes should be replicated.
+            _Apps = start_cluster_link(SourceNodes ++ TargetNodes, Config),
+            {ok, _} = ?block_until(#{
+                ?snk_kind := "cluster_link_bootstrap_complete",
+                ?snk_meta := #{node := TargetNode1}
+            }),
+            {ok, _} = ?block_until(#{
+                ?snk_kind := "cluster_link_bootstrap_complete",
+                ?snk_meta := #{node := TargetNode2}
+            }),
+            %% Publish a message to the source cluster.
+            {ok, _} = emqtt:publish(SourceC1, <<"t1/42">>, <<"hello">>, qos1),
+            ?assertReceive(
+                {publish, #{topic := <<"t1/42">>, payload := <<"hello">>, client_pid := TargetC1}}
+            ),
+            ?assertReceive(
+                {publish, #{topic := <<"t1/42">>, payload := <<"hello">>, client_pid := TargetC2}}
+            ),
+            ?assertNotReceive(
+                {publish, #{}}
+            ),
+            %% Subscribe both clients to another pair of topics while cluster link is active.
+            T21 = maybe_shared_topic(IsShared, <<"t2/#">>),
+            T22 = maybe_shared_topic(IsShared, <<"t2/+/+">>),
+            {ok, _, _} = emqtt:subscribe(TargetC1, T21, qos1),
+            {ok, _, _} = emqtt:subscribe(TargetC2, T22, qos1),
+            {ok, _} = ?block_until(#{?snk_kind := "cluster_link_route_sync_complete"}),
+            %% Publish another message.
+            {ok, _} = emqtt:publish(SourceC1, <<"t2/3/4">>, <<"heh">>, qos1),
+            ?assertReceive(
+                {publish, #{topic := <<"t2/3/4">>, payload := <<"heh">>, client_pid := TargetC1}}
+            ),
+            ?assertReceive(
+                {publish, #{topic := <<"t2/3/4">>, payload := <<"heh">>, client_pid := TargetC2}}
+            ),
+            ?assertNotReceive(
+                {publish, #{}}
+            )
+        after
+            %% Stop clients.
+            ok = emqtt:stop(SourceC1),
+            ok = emqtt:stop(TargetC1),
+            ok = emqtt:stop(TargetC2)
+        end,
+        fun(_) -> ok end
+    ).
 
 t_target_extrouting_gc('init', Config) ->
     SourceCluster = mk_source_cluster(?FUNCTION_NAME, Config),
@@ -216,7 +254,8 @@ t_target_extrouting_gc('init', Config) ->
     ];
 t_target_extrouting_gc('end', Config) ->
     ok = snabbkaffe:stop(),
-    ok = emqx_cth_cluster:stop(?config(source_nodes, Config)).
+    ok = emqx_cth_cluster:stop(?config(source_nodes, Config)),
+    ok = emqx_cth_cluster:stop(?config(target_nodes, Config)).
 
 t_target_extrouting_gc(Config) ->
     [SourceNode1 | _] = nodes_source(Config),
@@ -224,72 +263,75 @@ t_target_extrouting_gc(Config) ->
     SourceC1 = emqx_cluster_link_cth:connect_client("t_target_extrouting_gc", SourceNode1),
     TargetC1 = emqx_cluster_link_cth:connect_client_unlink("t_target_extrouting_gc1", TargetNode1),
     TargetC2 = emqx_cluster_link_cth:connect_client_unlink("t_target_extrouting_gc2", TargetNode2),
-    IsShared = ?config(is_shared_sub, Config),
-
-    TopicFilter1 = <<"t/+">>,
-    TopicFilter2 = <<"t/#">>,
-    {ok, _, _} = emqtt:subscribe(TargetC1, maybe_shared_topic(IsShared, TopicFilter1), qos1),
-    {ok, _, _} = emqtt:subscribe(TargetC2, maybe_shared_topic(IsShared, TopicFilter2), qos1),
-    {ok, _} = ?block_until(#{
-        ?snk_kind := "cluster_link_route_sync_complete", ?snk_meta := #{node := TargetNode1}
-    }),
-    {ok, _} = ?block_until(#{
-        ?snk_kind := "cluster_link_route_sync_complete", ?snk_meta := #{node := TargetNode2}
-    }),
-    {ok, _} = emqtt:publish(SourceC1, <<"t/1">>, <<"HELLO1">>, qos1),
-    {ok, _} = emqtt:publish(SourceC1, <<"t/2/ext">>, <<"HELLO2">>, qos1),
-    {ok, _} = emqtt:publish(SourceC1, <<"t/3/ext">>, <<"HELLO3">>, qos1),
-    Pubs1 = [M || {publish, M} <- ?drainMailbox(1_000)],
-    %% We switch off `TargetNode2' first.  Since `TargetNode1' is the sole endpoint
-    %% configured in Target Cluster, the link will keep working (i.e., CL MQTT ecpool
-    %% workers will stay connected).  If we turned `TargetNode1' first, then the link
-    %% would stay down and stop replicating messages.
-    {ok, _} = ?wait_async_action(
-        emqx_cth_cluster:stop_node(TargetNode2),
-        #{?snk_kind := clink_extrouter_actor_cleaned, cluster := <<"cl.target">>}
-    ),
-    {ok, _} = emqtt:publish(SourceC1, <<"t/4/ext">>, <<"HELLO4">>, qos1),
-    {ok, _} = emqtt:publish(SourceC1, <<"t/5">>, <<"HELLO5">>, qos1),
-    Pubs2 = [M || {publish, M} <- ?drainMailbox(1_000)],
-    {ok, _} = ?wait_async_action(
-        emqx_cth_cluster:stop_node(TargetNode1),
-        #{?snk_kind := clink_extrouter_actor_cleaned, cluster := <<"cl.target">>}
-    ),
-    ok = emqtt:stop(SourceC1),
-    %% Verify that extrouter table eventually becomes empty.
-    ?assertEqual(
-        [],
-        erpc:call(SourceNode1, emqx_cluster_link_extrouter, topics, []),
-        {
-            erpc:call(SourceNode1, ets, tab2list, [emqx_external_router_actor]),
-            erpc:call(SourceNode1, ets, tab2list, [emqx_external_router_route])
-        }
-    ),
-    %% Verify all relevant messages were forwarded.
-    ?assertMatch(
-        [
-            #{topic := <<"t/1">>, payload := <<"HELLO1">>, client_pid := _C1},
-            #{topic := <<"t/1">>, payload := <<"HELLO1">>, client_pid := _C2},
-            #{topic := <<"t/2/ext">>, payload := <<"HELLO2">>},
-            #{topic := <<"t/3/ext">>, payload := <<"HELLO3">>},
-            %% We expect only `HELLO5' and not `HELLO4' to be here because the former was
-            %% published while only `TargetNode1' was alive, and this node held only the
-            %% `t/+' subscription at that time.
-            #{topic := <<"t/5">>, payload := <<"HELLO5">>}
-        ],
-        lists:sort(emqx_utils_maps:key_comparer(topic), Pubs1 ++ Pubs2)
-    ),
-    %% Verify there was no unnecessary forwarding.
-    Trace = snabbkaffe:collect_trace(),
-    ?assertMatch(
-        [
-            #{message := #message{topic = <<"t/1">>, payload = <<"HELLO1">>}},
-            #{message := #message{topic = <<"t/2/ext">>, payload = <<"HELLO2">>}},
-            #{message := #message{topic = <<"t/3/ext">>, payload = <<"HELLO3">>}},
-            #{message := #message{topic = <<"t/5">>, payload = <<"HELLO5">>}}
-        ],
-        ?of_kind(clink_message_forwarded, Trace),
-        Trace
+    ?check_trace(
+        begin
+            IsShared = ?config(is_shared_sub, Config),
+            TopicFilter1 = maybe_shared_topic(IsShared, <<"t/+">>),
+            TopicFilter2 = maybe_shared_topic(IsShared, <<"t/#">>),
+            {ok, _, _} = emqtt:subscribe(TargetC1, TopicFilter1, qos1),
+            {ok, _, _} = emqtt:subscribe(TargetC2, TopicFilter2, qos1),
+            {ok, _} = ?block_until(#{
+                ?snk_kind := "cluster_link_route_sync_complete", ?snk_meta := #{node := TargetNode1}
+            }),
+            {ok, _} = ?block_until(#{
+                ?snk_kind := "cluster_link_route_sync_complete", ?snk_meta := #{node := TargetNode2}
+            }),
+            {ok, _} = emqtt:publish(SourceC1, <<"t/1">>, <<"HELLO1">>, qos1),
+            {ok, _} = emqtt:publish(SourceC1, <<"t/2/ext">>, <<"HELLO2">>, qos1),
+            {ok, _} = emqtt:publish(SourceC1, <<"t/3/ext">>, <<"HELLO3">>, qos1),
+            Pubs1 = [M || {publish, M} <- ?drainMailbox(1_000)],
+            %% We switch off `TargetNode2' first.  Since `TargetNode1' is the sole endpoint
+            %% configured in Target Cluster, the link will keep working (i.e., CL MQTT ecpool
+            %% workers will stay connected).  If we turned `TargetNode1' first, then the link
+            %% would stay down and stop replicating messages.
+            {ok, _} = ?wait_async_action(
+                emqx_cth_cluster:stop_node(TargetNode2),
+                #{?snk_kind := clink_extrouter_actor_cleaned, cluster := <<"cl.target">>}
+            ),
+            {ok, _} = emqtt:publish(SourceC1, <<"t/4/ext">>, <<"HELLO4">>, qos1),
+            {ok, _} = emqtt:publish(SourceC1, <<"t/5">>, <<"HELLO5">>, qos1),
+            Pubs2 = [M || {publish, M} <- ?drainMailbox(1_000)],
+            {ok, _} = ?wait_async_action(
+                emqx_cth_cluster:stop_node(TargetNode1),
+                #{?snk_kind := clink_extrouter_actor_cleaned, cluster := <<"cl.target">>}
+            ),
+            ok = emqtt:stop(SourceC1),
+            %% Verify that extrouter table eventually becomes empty.
+            ?assertEqual(
+                [],
+                erpc:call(SourceNode1, emqx_cluster_link_extrouter, topics, []),
+                {
+                    erpc:call(SourceNode1, ets, tab2list, [emqx_external_router_actor]),
+                    erpc:call(SourceNode1, ets, tab2list, [emqx_external_router_route])
+                }
+            ),
+            %% Verify all relevant messages were forwarded.
+            ?assertMatch(
+                [
+                    #{topic := <<"t/1">>, payload := <<"HELLO1">>, client_pid := _C1},
+                    #{topic := <<"t/1">>, payload := <<"HELLO1">>, client_pid := _C2},
+                    #{topic := <<"t/2/ext">>, payload := <<"HELLO2">>},
+                    #{topic := <<"t/3/ext">>, payload := <<"HELLO3">>},
+                    %% We expect only `HELLO5' and not `HELLO4' to be here because the former was
+                    %% published while only `TargetNode1' was alive, and this node held only the
+                    %% `t/+' subscription at that time.
+                    #{topic := <<"t/5">>, payload := <<"HELLO5">>}
+                ],
+                lists:sort(emqx_utils_maps:key_comparer(topic), Pubs1 ++ Pubs2)
+            )
+        end,
+        fun(Trace) ->
+            %% Verify there was no unnecessary forwarding.
+            ?assertMatch(
+                [
+                    #{message := #message{topic = <<"t/1">>, payload = <<"HELLO1">>}},
+                    #{message := #message{topic = <<"t/2/ext">>, payload = <<"HELLO2">>}},
+                    #{message := #message{topic = <<"t/3/ext">>, payload = <<"HELLO3">>}},
+                    #{message := #message{topic = <<"t/5">>, payload = <<"HELLO5">>}}
+                ],
+                ?of_kind(clink_message_forwarded, Trace)
+            )
+        end
     ).
 
 %% Checks that, if an exception occurs while handling a route op message, we disconnect
@@ -298,9 +340,6 @@ t_disconnect_on_errors('init', Config) ->
     SourceNodes = emqx_cth_cluster:start(mk_source_cluster(?FUNCTION_NAME, Config)),
     [TargetNodeSpec | _] = mk_target_cluster(?FUNCTION_NAME, Config),
     TargetNodes = emqx_cth_cluster:start([TargetNodeSpec]),
-    % ?ON(hd(SourceNodes), dbg:tracer()),
-    % ?ON(hd(SourceNodes), dbg:p(all, c)),
-    % ?ON(hd(SourceNodes), dbg:tpl({emqx_cluster_link_routerepl, '_', '_'}, x)),
     _Apps = start_cluster_link(SourceNodes ++ TargetNodes, Config),
     ok = snabbkaffe:start_trace(),
     [
@@ -431,8 +470,7 @@ t_restart_connection_on_actor_init_timeout(Config) ->
             ),
             ok
         end
-    ),
-    ok.
+    ).
 
 %% Checks that connect / subscribe errors during routerepl actor initialization are
 %% handled gracefully.
@@ -534,11 +572,9 @@ t_graceful_retry_on_actor_error(Config) ->
             ?assertMatch(
                 [_SN1, _SN2, _TN1, _TN2],
                 ?of_kind("cluster_link_actor_init", Trace)
-            ),
-            ok
+            )
         end
-    ),
-    ok.
+    ).
 
 %%
 
