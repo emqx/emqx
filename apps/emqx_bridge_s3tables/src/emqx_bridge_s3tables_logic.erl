b77ff3529817e10df2e844749472d4f68a3f6d0d
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2025-05-09 16:53:03 -0300

    fix(iceberg/s3tables): fix transform result type

--- apps/emqx_bridge_s3tables/src/emqx_bridge_s3tables_logic.erl
+++ apps/emqx_bridge_s3tables/src/emqx_bridge_s3tables_logic.erl
@@ -530,6 +530,12 @@ iceberg_type_to_avro(<<"binary">>) ->
 iceberg_type_to_avro(Type) ->
     throw({unsupported_type, Type}).
 
+iceberg_type_to_primitive_avro(#{<<"id">> := _, <<"type">> := T}) ->
+    iceberg_type_to_avro(T);
+iceberg_type_to_primitive_avro(T) ->
+    %% Assuming it's a binary primitive
+    T.
+
 memoized_required_bytes() ->
     maybe
         undefined ?= persistent_term:get(?REQUIRED_BYTES_PT_KEY, undefined),
@@ -734,14 +740,14 @@ Returns the resulting Iceberg type after the partition transform function is app
 value.
 """.
 transform_result_type(<<"identity">>, SourceIceType) ->
-    {ok, SourceIceType};
+    {ok, iceberg_type_to_primitive_avro(SourceIceType)};
 transform_result_type(<<"void">>, SourceIceType) ->
-    {ok, SourceIceType};
+    {ok, iceberg_type_to_primitive_avro(SourceIceType)};
 transform_result_type(<<"bucket[", _/binary>>, _SourceIceType) ->
     {ok, <<"int">>};
 %% TODO
-transform_result_type(TransformName, _SourceIceType) ->
-    {error, {unsupported_transform, TransformName}}.
+transform_result_type(TransformName, SourceIceType) ->
+    {error, {unsupported_transform, TransformName, SourceIceType}}.
 
 mk_transform_fn(<<"identity">>) ->
     {ok, fmap(fun(X) -> X end)};
@@ -839,7 +845,7 @@ human_readable_partition_value(X, IceType) ->
 
 do_manifest_entry_avro_schema(PartitionFields, AvroScJSON, IcebergSchema) ->
     AvroPartitionFields = lists:map(
-        fun(#{id := Id, name := N, source_type := #{<<"type">> := T}}) ->
+        fun(#{?id := Id, ?name := N, ?result_type := T}) ->
             #{
                 <<"field-id">> => Id,
                 <<"name">> => N,
