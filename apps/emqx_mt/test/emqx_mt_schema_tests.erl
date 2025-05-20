90c0374bb3e4a7475bfac1b675f3aa1a6d6b2ace
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2025-05-20 11:21:20 -0300

    fix(mt): `default_max_sessions` must be positive
    Fixes https://emqx.atlassian.net/browse/EMQX-14291

---
+++ apps/emqx_mt/test/emqx_mt_schema_tests.erl
@@ -0,0 +1,42 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2024-2025 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%--------------------------------------------------------------------
+-module(emqx_mt_schema_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+%%------------------------------------------------------------------------------
+%% Helper fns
+%%------------------------------------------------------------------------------
+
+bin(X) -> emqx_utils_conv:bin(X).
+
+parse_and_check(InnerConfigs) ->
+    RootBin = <<"multi_tenancy">>,
+    RawConf = #{RootBin => InnerConfigs},
+    #{RootBin := Checked} = hocon_tconf:check_plain(
+        emqx_mt_schema,
+        RawConf,
+        #{
+            required => false,
+            atom_key => false,
+            make_serializable => false
+        }
+    ),
+    Checked.
+
+%%------------------------------------------------------------------------------
+%% Test cases
+%%------------------------------------------------------------------------------
+
+default_max_sessions_positive_test() ->
+    ?assertThrow(
+        {_, [
+            #{
+                kind := validation_error,
+                %% Wrong type
+                reason := matched_no_union_member
+            }
+        ]},
+        parse_and_check(#{<<"default_max_sessions">> => 0})
+    ).
