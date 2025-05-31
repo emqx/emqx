f886dece2ca509af033e9d7c813d4b12fd14019c
zmstone <zmstone@gmail.com>
2025-05-31 17:29:07 +0200

    refactor: completely random jwk to sign login token

--- apps/emqx_dashboard_sso/src/emqx_dashboard_sso_saml.erl
+++ apps/emqx_dashboard_sso/src/emqx_dashboard_sso_saml.erl
@@ -255,7 +255,7 @@ gen_redirect_response(DashboardAddr, Username) ->
 ensure_user_exists(Username) ->
     case emqx_dashboard_admin:lookup_user(saml, Username) of
         [User] ->
-            emqx_dashboard_token:sign(User, <<>>);
+            emqx_dashboard_token:sign(User);
         [] ->
             case emqx_dashboard_admin:add_sso_user(saml, Username, ?ROLE_VIEWER, <<>>) of
                 {ok, _} ->
