7d6196944de55f85678a3d8d769d6bbc38638478
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2025-06-27 14:23:29 -0300

    chore: bump app vsns

--- apps/emqx_bridge_snowflake/mix.exs
+++ apps/emqx_bridge_snowflake/mix.exs
@@ -5,7 +5,7 @@ defmodule EMQXBridgeSnowflake.MixProject do
   def project do
     [
       app: :emqx_bridge_snowflake,
-      version: "0.1.2",
+      version: "0.1.3",
       build_path: "../../_build",
       erlc_options: UMP.strict_erlc_options(),
       erlc_paths: UMP.erlc_paths(),
