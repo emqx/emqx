1f4fc410cba64eb3aba27481669a1fed6706954c
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2025-06-30 16:34:54 -0300

    test: fix warning about doubly-exported function

--- apps/emqx_resource/src/emqx_resource_manager.erl
+++ apps/emqx_resource/src/emqx_resource_manager.erl
@@ -68,7 +68,7 @@
 -export([health_check/1, channel_health_check/2]).
 
 -ifdef(TEST).
--export([stop/2, summarize_query_mode/2]).
+-export([summarize_query_mode/2]).
 -endif.
 
 %%------------------------------------------------------------------------------
