167c42564aaeae99c4d35ce10dba77f01596a66d
Ivan Dyachkov <dev@dyachkov.org>
2025-07-23 11:24:18 +0200

    chore: bump version to 5.8.8-alpha.1

--- apps/emqx/include/emqx_release.hrl
+++ apps/emqx/include/emqx_release.hrl
@@ -32,7 +32,7 @@
 %% `apps/emqx/src/bpapi/README.md'
 
 %% Opensource edition
--define(EMQX_RELEASE_CE, "5.8.7").
+-define(EMQX_RELEASE_CE, "5.8.8-alpha.1").
 
 %% Enterprise edition
--define(EMQX_RELEASE_EE, "5.8.7").
+-define(EMQX_RELEASE_EE, "5.8.8-alpha.1").
