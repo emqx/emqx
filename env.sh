3f01a72a2226ce9d7ccbbb895b63e6500a23d583
zmstone <zmstone@gmail.com>
2025-07-25 10:33:38 +0200

    ci: bump OTP version to 26.2.5.14-1

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 # https://github.com/emqx/emqx-builder
-export EMQX_BUILDER_VSN=5.4-4
-export OTP_VSN=26.2.5.2-3
+export EMQX_BUILDER_VSN=5.5-4
+export OTP_VSN=26.2.5.14-1
 export ELIXIR_VSN=1.15.7
 export EMQX_BUILDER=ghcr.io/emqx/emqx-builder/${EMQX_BUILDER_VSN}:${ELIXIR_VSN}-${OTP_VSN}-ubuntu22.04
 export EMQX_DOCKER_BUILD_FROM=ghcr.io/emqx/emqx-builder/${EMQX_BUILDER_VSN}:${ELIXIR_VSN}-${OTP_VSN}-debian12
--- scripts/buildx.sh
+++ scripts/buildx.sh
@@ -12,6 +12,11 @@
 
 set -euo pipefail
 
+# ensure dir
+cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
+# shellcheck disable=SC1091
+source ./env.sh
+
 help() {
     echo
     echo "-h|--help:"
@@ -36,7 +41,7 @@ help() {
     echo ""
     echo "--builder <BUILDER>:"
     echo "    Docker image to use for building"
-    echo "    E.g. ghcr.io/emqx/emqx-builder/5.4-4:1.15.7-26.2.5.2-3-debian12"
+    echo "    E.g. ${EMQX_BUILDER}"
     echo "    For hot upgrading tar.gz, specify a builder image with the same OS distribution as the running one."
     echo "    Specifically, for EMQX's docker containers hot upgrading, please use the debian12-based builder. "
 }
@@ -47,11 +52,6 @@ die() {
     help
     exit 1
 }
-
-# ensure dir
-cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
-# shellcheck disable=SC1091
-source ./env.sh
 
 while [ "$#" -gt 0 ]; do
     case $1 in
