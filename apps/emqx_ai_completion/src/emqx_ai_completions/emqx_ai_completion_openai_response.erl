12c358abfc52be16fcc4fc9ffd39e24dbbcd3bfc
Ilia Averianov <av@rubybox.dev>
2025-08-18 18:16:21 +0300

    fix(ai_completion): correctly extract text from /response result

--- apps/emqx_ai_completion/src/emqx_ai_completions/emqx_ai_completion_openai_response.erl
+++ apps/emqx_ai_completion/src/emqx_ai_completions/emqx_ai_completion_openai_response.erl
@@ -5,13 +5,11 @@
 -module(emqx_ai_completion_openai_response).
 
 -behaviour(emqx_ai_completion).
+
 -include_lib("emqx/include/logger.hrl").
 -include_lib("snabbkaffe/include/trace.hrl").
 
--export([
-    call_completion/3,
-    list_models/1
-]).
+-export([call_completion/3, list_models/1]).
 
 %%------------------------------------------------------------------------------
 %% API
@@ -29,38 +27,36 @@ call_completion(
 ) ->
     Prompt = maps:get(prompt, Options, SystemPrompt),
     Client = create_client(Provider),
-    Request = #{
-        <<"model">> => Model,
-        <<"input">> => Data,
-        <<"instructions">> => Prompt
-    },
-    ?tp(debug, emqx_ai_completion_request, #{
-        request => Request
-    }),
+    Request =
+        #{
+            <<"model">> => Model,
+            <<"input">> => Data,
+            <<"instructions">> => Prompt
+        },
+    ?tp(debug, emqx_ai_completion_request, #{request => Request}),
     case emqx_ai_completion_client:api_post(Client, <<"responses">>, Request) of
-        {ok, #{
-            <<"status">> := <<"completed">>,
-            <<"output">> := [
+        {ok, #{<<"status">> := <<"completed">>, <<"output">> := Output}} ->
+            Content = take_content(Output),
+            ?tp(
+                debug,
+                emqx_ai_completion_result,
                 #{
-                    <<"type">> := <<"message">>,
-                    <<"status">> := <<"completed">>,
-                    <<"content">> := [#{<<"type">> := <<"output_text">>, <<"text">> := Output} | _]
+                    result => Content,
+                    provider => ProviderName,
+                    completion_profile => Name
                 }
-                | _
-            ]
-        }} ->
-            ?tp(debug, emqx_ai_completion_result, #{
-                result => Output,
-                provider => ProviderName,
-                completion_profile => Name
-            }),
-            Output;
+            ),
+            Content;
         {error, Reason} ->
-            ?tp(error, emqx_ai_completion_error, #{
-                reason => Reason,
-                provider => ProviderName,
-                completion_profile => Name
-            }),
+            ?tp(
+                error,
+                emqx_ai_completion_error,
+                #{
+                    reason => Reason,
+                    provider => ProviderName,
+                    completion_profile => Name
+                }
+            ),
             <<"">>
     end.
 
@@ -73,3 +69,33 @@ list_models(Provider) ->
 
 create_client(Provider) ->
     emqx_ai_completion_openai:create_client(Provider).
+
+%% Content is like:
+%%
+%%     [#{<<"id">> => <<"rs_689c31cac8348195b1cff9d8f1d3a7b50d623e416147c0e2">>,
+%%     <<"summary">> => [],
+%%     <<"type">> => <<"reasoning">>},
+%%   #{<<"content">> =>
+%%         [#{<<"annotations">> => [],
+%%            <<"logprobs">> => [],
+%%            <<"text">> => <<"3">>,
+%%            <<"type">> => <<"output_text">>}],
+%%     <<"id">> => <<"msg_689c31cca6648195a2bef2be2bfbbe5e0d623e416147c0e2">>,
+%%     <<"role">> => <<"assistant">>,
+%%     <<"status">> => <<"completed">>,
+%%     <<"type">> => <<"message">>}],
+%%
+%% See https://platform.openai.com/docs/api-reference/responses/object#responses/object-output
+
+take_content(Output) ->
+    Texts = [
+        Text
+     || #{
+            <<"content">> := Content,
+            <<"role">> := <<"assistant">>,
+            <<"type">> := <<"message">>
+        } <-
+            Output,
+        #{<<"text">> := Text, <<"type">> := <<"output_text">>} <- Content
+    ],
+    iolist_to_binary(Texts).
