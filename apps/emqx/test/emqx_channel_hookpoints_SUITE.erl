efd429eb2d81b4faea4e967aff026419c6640c7c
Ilia Averianov <av@rubybox.dev>
2025-09-02 18:18:04 +0300

    feat(mq): fix emqx_channel_hookpoints_SUITE copyright dates

--- apps/emqx/test/emqx_channel_hookpoints_SUITE.erl
+++ apps/emqx/test/emqx_channel_hookpoints_SUITE.erl
@@ -1,5 +1,5 @@
 %%--------------------------------------------------------------------
-%% Copyright (c) 2025 EMQ Technologies Co., Ltd. All Rights Reserved.
+%% Copyright (c) 2023-2025 EMQ Technologies Co., Ltd. All Rights Reserved.
 %%--------------------------------------------------------------------
 
 -module(emqx_channel_hookpoints_SUITE).
