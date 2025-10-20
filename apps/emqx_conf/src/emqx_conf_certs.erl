a60352b567548d1061de35113e9fe513f978108f
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2025-10-20 10:50:34 -0300

    refactor(certs): use reversible escaping of namespaces

--- apps/emqx_conf/src/emqx_conf_certs.erl
+++ apps/emqx_conf/src/emqx_conf_certs.erl
@@ -223,44 +223,4 @@ filename_to_kind(_) ->
     error.
 
 escape_name(Name) ->
-    escape_uri(str(Name)).
-
-%% copied from `edoc_lib' because dialyzer cannot see this private
-%% function there.
-escape_uri([C | Cs]) when C >= $a, C =< $z ->
-    [C | escape_uri(Cs)];
-escape_uri([C | Cs]) when C >= $A, C =< $Z ->
-    [C | escape_uri(Cs)];
-escape_uri([C | Cs]) when C >= $0, C =< $9 ->
-    [C | escape_uri(Cs)];
-escape_uri([C = $. | Cs]) ->
-    [C | escape_uri(Cs)];
-escape_uri([C = $- | Cs]) ->
-    [C | escape_uri(Cs)];
-escape_uri([C = $_ | Cs]) ->
-    [C | escape_uri(Cs)];
-escape_uri([C | Cs]) when C > 16#7f ->
-    %% This assumes that characters are at most 16 bits wide.
-    escape_byte(((C band 16#c0) bsr 6) + 16#c0) ++
-        escape_byte(C band 16#3f + 16#80) ++
-        escape_uri(Cs);
-escape_uri([C | Cs]) ->
-    escape_byte(C) ++ escape_uri(Cs);
-escape_uri([]) ->
-    [].
-
-%% copied from `edoc_lib' because dialyzer cannot see this private
-%% function there.
-%% has a small modification: it uses `=' in place of `%' so that it
-%% won't generate invalid paths in windows.
-escape_byte(C) when C >= 0, C =< 255 ->
-    [$=, hex_digit(C bsr 4), hex_digit(C band 15)].
-
-%% copied from `edoc_lib' because dialyzer cannot see this private
-%% function there.
-hex_digit(N) when N >= 0, N =< 9 ->
-    N + $0;
-hex_digit(N) when N > 9, N =< 15 ->
-    N + $a - 10.
-
-str(X) -> emqx_utils_conv:str(X).
+    uri_string:quote(Name).
