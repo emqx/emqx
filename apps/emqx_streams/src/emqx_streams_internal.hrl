f22695aa504b90eb8838e901b556275ddf6a4f74
Andrew Mayorov <encube.ul@gmail.com>
2025-10-31 14:54:05 +0100

    feat(streams): describe stream dispatch protocol draft
    Also implement basic data structure for fair resource allocation.

--- apps/emqx_streams/src/emqx_streams_internal.hrl
+++ apps/emqx_streams/src/emqx_streams_internal.hrl
@@ -5,6 +5,10 @@
 -ifndef(EMQX_STREAMS_INTERNAL_HRL).
 -define(EMQX_STREAMS_INTERNAL_HRL, true).
 
+-include_lib("snabbkaffe/include/trace.hrl").
+
+-define(tp_debug(KIND, EVENT), ?tp_ignore_side_effects_in_prod(KIND, EVENT)).
+
 -define(SCHEMA_ROOT, streams).
 
 -endif.
