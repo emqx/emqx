12b7c59e0e5f467ccb54136e760d6850ac54fcbc
Ilia Averianov <av@rubybox.dev>
2025-11-05 20:32:49 +0200

    feat(streams): add stream write path proto

--- apps/emqx_streams/test/emqx_streams_test_utils.erl
+++ apps/emqx_streams/test/emqx_streams_test_utils.erl
@@ -4,7 +4,20 @@
 
 -module(emqx_streams_test_utils).
 
+-export([
+    emqtt_connect/1,
+    emqtt_pub_stream/3,
+    emqtt_pub_stream/4,
+    emqtt_sub_stream/2,
+    emqtt_drain/0,
+    emqtt_drain/1,
+    emqtt_drain/2,
+    emqtt_ack/1
+]).
+
 -export([create_stream/1, fill_stream_defaults/1]).
+
+-export([populate/2, populate_lastvalue/2]).
 
 -export([cleanup_streams/0]).
 
@@ -13,6 +26,62 @@
 -include_lib("../src/emqx_streams_internal.hrl").
 -include_lib("snabbkaffe/include/snabbkaffe.hrl").
 -include_lib("eunit/include/eunit.hrl").
+
+-define(STREAM_KEY_USER_PROPERTY, <<"stream-key">>).
+
+emqtt_connect(Opts) ->
+    BaseOpts = [{proto_ver, v5}],
+    {ok, C} = emqtt:start_link(BaseOpts ++ Opts),
+    {ok, _} = emqtt:connect(C),
+    C.
+
+emqtt_pub_stream(Client, Topic, Payload) ->
+    emqtt_pub_stream(Client, Topic, Payload, #{}).
+
+emqtt_pub_stream(Client, Topic, Payload, Opts) ->
+    Properties =
+        case Opts of
+            #{key := Key} -> #{'User-Property' => [{?STREAM_KEY_USER_PROPERTY, Key}]};
+            _ -> #{}
+        end,
+    Qos = maps:get(qos, Opts, 1),
+    PubOpts = [{qos, Qos}],
+    emqtt:publish(Client, Topic, Properties, Payload, PubOpts).
+
+emqtt_sub_stream(Client, Topic) ->
+    FullTopic = <<"$s/", Topic/binary>>,
+    {ok, _, _} = emqtt:subscribe(Client, {FullTopic, 1}),
+    ok.
+
+emqtt_drain() ->
+    emqtt_drain(0, 0).
+
+emqtt_drain(MinMsg) when is_integer(MinMsg) ->
+    emqtt_drain(MinMsg, 0).
+
+emqtt_drain(MinMsg, Timeout) when is_integer(MinMsg) andalso is_integer(Timeout) ->
+    emqtt_drain(MinMsg, Timeout, [], 0).
+
+emqtt_drain(MinMsg, Timeout, AccMsgs, AccNReceived) ->
+    receive
+        {publish, Msg} ->
+            emqtt_drain(MinMsg, Timeout, [Msg | AccMsgs], AccNReceived + 1)
+    after Timeout ->
+        case AccNReceived >= MinMsg of
+            true ->
+                {ok, lists:reverse(AccMsgs)};
+            false ->
+                {error, {not_enough_messages, {received, AccNReceived}, {min, MinMsg}}}
+        end
+    end.
+
+emqtt_ack(Msgs) ->
+    ok = lists:foreach(
+        fun(#{client_pid := Pid, packet_id := PacketId}) ->
+            emqtt:puback(Pid, PacketId)
+        end,
+        Msgs
+    ).
 
 create_stream(#{topic_filter := TopicFilter} = Stream0) ->
     Stream1 = fill_stream_defaults(Stream0),
@@ -56,6 +125,57 @@ fill_stream_defaults(#{topic_filter := _TopicFilter} = Stream0) ->
             Stream1
     end.
 
+populate(N, #{topic_prefix := TopicPrefix} = Opts) ->
+    PayloadPrefix = maps:get(payload_prefix, Opts, <<"payload-">>),
+    NeedDifferentClients = maps:get(different_clients, Opts, false),
+    C0 = emqtt_connect([]),
+    C = lists:foldl(
+        fun(I, Conn0) ->
+            IBin = integer_to_binary(I),
+            Topic = <<TopicPrefix/binary, IBin/binary>>,
+            Payload = <<PayloadPrefix/binary, IBin/binary>>,
+            emqtt_pub_stream(Conn0, Topic, Payload, pub_opts(Opts, #{})),
+            case NeedDifferentClients of
+                true ->
+                    emqtt:disconnect(Conn0),
+                    emqtt_connect([]);
+                false ->
+                    Conn0
+            end
+        end,
+        C0,
+        lists:seq(0, N - 1)
+    ),
+    ok = emqtt:disconnect(C).
+
+populate_lastvalue(N, #{topic_prefix := TopicPrefix} = Opts) ->
+    PayloadPrefix = maps:get(payload_prefix, Opts, <<"payload-">>),
+    NeedDifferentClients = maps:get(different_clients, Opts, false),
+    NKeys = maps:get(n_keys, Opts, N),
+    C0 = emqtt_connect([]),
+    C = lists:foldl(
+        fun(I, Conn0) ->
+            IBin = integer_to_binary(I),
+            Topic = <<TopicPrefix/binary, IBin/binary>>,
+            Payload = <<PayloadPrefix/binary, IBin/binary>>,
+            Key = <<"k-", (integer_to_binary(I rem NKeys))/binary>>,
+            emqtt_pub_stream(Conn0, Topic, Payload, pub_opts(Opts, #{key => Key})),
+            case NeedDifferentClients of
+                true ->
+                    emqtt:disconnect(Conn0),
+                    emqtt_connect([]);
+                false ->
+                    Conn0
+            end
+        end,
+        C0,
+        lists:seq(0, N - 1)
+    ),
+    ok = emqtt:disconnect(C).
+
+pub_opts(PopulateOpts, PubOpts) ->
+    maps:merge(maps:with([qos], PopulateOpts), PubOpts).
+
 cleanup_streams() ->
     ok = emqx_streams_registry:delete_all().
 
@@ -71,8 +191,16 @@ cth_config(emqx_streams, ConfigOverrides) ->
     };
 cth_config(emqx, ConfigOverrides) ->
     DefaultConfig = #{
-        %% TODO
-        %% Configure Streams DS config
+        <<"streams_messages">> => #{
+            <<"transaction">> => #{
+                <<"flush_interval">> => 100,
+                <<"idle_flush_interval">> => 20,
+                <<"conflict_window">> => 5000
+            },
+            <<"subscriptions">> => #{
+                <<"batch_size">> => 1
+            }
+        }
     },
     Config = emqx_utils_maps:deep_merge(DefaultConfig, ConfigOverrides),
     #{
