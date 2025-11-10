b171537e66d32a6ef5ee122d7e6aae47f4eb79ac
zmstone <zmstone@gmail.com>
2025-11-10 17:03:57 +0100

    chore: auto-fix app versions

--- apps/emqx_gateway_ocpp/mix.exs
+++ apps/emqx_gateway_ocpp/mix.exs
@@ -5,7 +5,7 @@ defmodule EMQXGatewayOcpp.MixProject do
   def project do
     [
       app: :emqx_gateway_ocpp,
-      version: "6.0.1",
+      version: "6.0.2",
       build_path: "../../_build",
       erlc_options: UMP.erlc_options(),
       erlc_paths: UMP.erlc_paths(),
