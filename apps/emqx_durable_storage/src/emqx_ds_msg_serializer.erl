6c138d48c48a8285d59acee4e96cfb9bc605e08f
Andrew Mayorov <encube.ul@gmail.com>
2025-12-12 16:35:58 +0100

    chore(ds): drop unused include

--- apps/emqx_durable_storage/src/emqx_ds_msg_serializer.erl
+++ apps/emqx_durable_storage/src/emqx_ds_msg_serializer.erl
@@ -22,7 +22,6 @@
 ]).
 
 -include_lib("emqx_utils/include/emqx_message.hrl").
--include_lib("typerefl/include/types.hrl").
 
 -elvis([{elvis_style, atom_naming_convention, disable}]).
 -include("../gen_src/DurableMessage.hrl").
