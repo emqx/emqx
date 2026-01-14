f0a5cf1c2ee47d8a65f9469f296e4da9dc2ed232
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2026-01-14 09:21:00 -0300

    perf(schema registry spb): hibernate gc process by default

--- apps/emqx_schema_registry/src/emqx_schema_registry_spb_gc.erl
+++ apps/emqx_schema_registry/src/emqx_schema_registry_spb_gc.erl
@@ -53,13 +53,13 @@ init(_Opts) ->
     State = #{
         ?clients => #{}
     },
-    {ok, State}.
+    {ok, State, hibernate}.
 
 terminate(_Reason, _State) ->
     ok.
 
 handle_call(Call, _From, State) ->
-    {reply, {error, {unknown_call, Call}}, State}.
+    {reply, {error, {unknown_call, Call}}, State, hibernate}.
 
 handle_cast(#register_client{pid = Pid}, State0) ->
     State =
@@ -70,14 +70,14 @@ handle_cast(#register_client{pid = Pid}, State0) ->
                 _ = monitor(process, Pid),
                 State0#{?clients := Clients0#{Pid => true}}
         end,
-    {noreply, State};
+    {noreply, State, hibernate};
 handle_cast(#unregister_client{pid = Pid}, State0) ->
     #{?clients := Clients0} = State0,
     Clients = maps:remove(Pid, Clients0),
     State = State0#{?clients := Clients},
-    {noreply, State};
+    {noreply, State, hibernate};
 handle_cast(_Cast, State) ->
-    {noreply, State}.
+    {noreply, State, hibernate}.
 
 handle_info({'DOWN', _, _, Pid, _}, #{?clients := Clients0} = State0) when
     is_map_key(Pid, Clients0)
@@ -85,9 +85,9 @@ handle_info({'DOWN', _, _, Pid, _}, #{?clients := Clients0} = State0) when
     Clients = maps:remove(Pid, Clients0),
     State = State0#{?clients := Clients},
     emqx_schema_registry_spb_state:cleanup_entries_for(Pid),
-    {noreply, State};
+    {noreply, State, hibernate};
 handle_info(_Info, State) ->
-    {noreply, State}.
+    {noreply, State, hibernate}.
 
 %%------------------------------------------------------------------------------
 %% Internal fns
