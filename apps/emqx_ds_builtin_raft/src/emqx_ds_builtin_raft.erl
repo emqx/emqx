9810c925ab91ee3c9941b430d994a13ac19521aa
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2026-01-27 17:48:11 +0100

    fix(dsraft): Improve error handling in the update_schema command

--- apps/emqx_ds_builtin_raft/src/emqx_ds_builtin_raft.erl
+++ apps/emqx_ds_builtin_raft/src/emqx_ds_builtin_raft.erl
@@ -727,7 +727,7 @@ otx_get_latest_generation(DB, Shard) ->
     emqx_ds_storage_layer:generation_current({DB, Shard}).
 
 otx_become_leader(DB, Shard) ->
-    maybe_propagate_initial_schema(DB, Shard),
+    ok = maybe_propagate_initial_schema(DB, Shard),
     Command = emqx_ds_builtin_raft_machine:otx_new_leader(self()),
     case local_raft_leader(DB, Shard) of
         unknown ->
@@ -910,11 +910,19 @@ Called in otx_become_leader.
 """.
 -spec maybe_propagate_initial_schema(emqx_ds:db(), emqx_ds:shard()) -> ok.
 maybe_propagate_initial_schema(DB, Shard) ->
-    %% Propagate leader's schema to the replicas:
+    %% Propagate leader's schema to the replicas. It happens only
+    %% during shard initialization due to small pending id:
+    PendingId = -1,
     SiteSchema = emqx_dsch:get_db_schema(DB),
     Site = emqx_dsch:this_site(),
-    Command = emqx_ds_builtin_raft_machine:update_schema(0, Site, SiteSchema),
-    ra_command(DB, Shard, Command, -1).
+    Command = emqx_ds_builtin_raft_machine:update_schema(PendingId, Site, SiteSchema),
+    Result = ra_command(DB, Shard, Command, -1),
+    ?tp(debug, ra_propagate_leader_schema, #{db => DB, shard => Shard, result => Result}),
+    case Result of
+        ok -> ok;
+        {error, overlaps_existing_generations} -> ok;
+        {error, _} = Err -> Err
+    end.
 
 -spec add_generation_to_shard(emqx_ds:db(), emqx_ds:shard(), non_neg_integer()) -> ok.
 add_generation_to_shard(DB, Shard, Retries) ->
