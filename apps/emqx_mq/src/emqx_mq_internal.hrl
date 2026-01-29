28005472918581f34761c7920ae22a96e999fc15
Ilia Averianov <av@rubybox.dev>
2026-01-29 15:58:58 +0200

    feat(mq): update core emqx_mq modules and make emqx_mq_SUITE pass

--- apps/emqx_mq/src/emqx_mq_internal.hrl
+++ apps/emqx_mq/src/emqx_mq_internal.hrl
@@ -8,8 +8,8 @@
 -include_lib("snabbkaffe/include/snabbkaffe.hrl").
 -include_lib("emqx_utils/include/emqx_ds_dbs.hrl").
 
--define(tp_debug(KIND, EVENT), ?tp_ignore_side_effects_in_prod(KIND, EVENT)).
-%% -define(tp_debug(KIND, EVENT), ?tp(warning, KIND, EVENT)).
+% -define(tp_debug(KIND, EVENT), ?tp_ignore_side_effects_in_prod(KIND, EVENT)).
+-define(tp_debug(KIND, EVENT), ?tp(warning, KIND, EVENT)).
 
 %% Queue name for pre-6.1.1 queues.
 -define(DEFAULT_MQ_NAME, <<"default">>).
@@ -26,7 +26,11 @@
     info :: term()
 }).
 
--record(info_mq_inspect, {receiver :: reference(), topic_filter :: emqx_mq_types:mq_topic()}).
+-record(info_mq_inspect, {
+    receiver :: reference(),
+    topic_filter :: emqx_mq_types:mq_topic() | undefined,
+    name :: emqx_mq_types:mq_name()
+}).
 
 -record(mq_sub_connected, {
     consumer_ref :: emqx_mq_types:consumer_ref()
@@ -74,5 +78,7 @@
 
 -define(MQ_QUOTA_BUFFER, mq_message_quota_buffer).
 -define(DEFAULT_QUOTA_BUFFER_POOL_SIZE, 10).
+
+-define(LEGACY_QUEUE_NAME(TF), <<"/", (TF)/binary>>).
 
 -endif.
--- apps/emqx_mq/src/emqx_mq_sub_registry.erl
+++ apps/emqx_mq/src/emqx_mq_sub_registry.erl
@@ -19,8 +19,10 @@ of channels to the Message Queue consumers.
     all/0
 ]).
 
+-type name_topic() :: {emqx_mq_types:mq_name(), emqx_mq_types:mq_topic() | undefined}.
+
 -define(SUB_PD_KEY(SUBSCRIBER_REF), {mq, SUBSCRIBER_REF}).
--define(TOPIC_PD_KEY(TOPIC), {mqt, TOPIC}).
+-define(NAME_TOPIC_PD_KEY(TOPIC), {mqt, TOPIC}).
 
 %%--------------------------------------------------------------------
 %% API
@@ -29,41 +31,41 @@ of channels to the Message Queue consumers.
 -spec register(emqx_mq_sub:t()) -> ok.
 register(Sub) ->
     SubscriberRef = emqx_mq_sub:subscriber_ref(Sub),
-    MQTopicFilter = emqx_mq_sub:mq_topic_filter(Sub),
-    case erlang:get(?TOPIC_PD_KEY(MQTopicFilter)) of
+    NameTopic = emqx_mq_sub:name_topic(Sub),
+    case erlang:get(?NAME_TOPIC_PD_KEY(NameTopic)) of
         undefined ->
             ok;
         _OldSubscriberRef ->
             %% This should never happen.
-            error({mq_sub_registry_topic_conflict, MQTopicFilter})
+            error({mq_sub_registry_topic_conflict, NameTopic})
     end,
     ?tp_debug(mq_sub_registry_register, #{
-        subscriber_ref => SubscriberRef, mq_topic_filter => MQTopicFilter, sub => Sub
+        subscriber_ref => SubscriberRef, name_topic => NameTopic, sub => Sub
     }),
     _ = erlang:put(?SUB_PD_KEY(SubscriberRef), maps:without([subscriber_ref], Sub)),
-    _ = erlang:put(?TOPIC_PD_KEY(MQTopicFilter), SubscriberRef),
+    _ = erlang:put(?NAME_TOPIC_PD_KEY(NameTopic), SubscriberRef),
     ok.
 
--spec delete(emqx_mq_types:subscriber_ref() | emqx_types:topic()) ->
+-spec delete(emqx_mq_types:subscriber_ref() | name_topic()) ->
     emqx_mq_sub:t() | undefined.
 delete(SubscriberRef) when is_reference(SubscriberRef) ->
     case erlang:erase(?SUB_PD_KEY(SubscriberRef)) of
         undefined ->
             undefined;
         Sub ->
-            MQTopicFilter = emqx_mq_sub:mq_topic_filter(Sub),
-            _ = erlang:erase(?TOPIC_PD_KEY(MQTopicFilter)),
+            NameTopic = emqx_mq_sub:name_topic(Sub),
+            _ = erlang:erase(?NAME_TOPIC_PD_KEY(NameTopic)),
             Sub#{subscriber_ref => SubscriberRef}
     end;
-delete(MQTopicFilter) when is_binary(MQTopicFilter) ->
-    case erlang:get(?TOPIC_PD_KEY(MQTopicFilter)) of
+delete({Name, Topic} = NameTopic) when is_binary(Name) andalso (is_binary(Topic) orelse Topic =:= undefined) ->
+    case erlang:get(?NAME_TOPIC_PD_KEY(NameTopic)) of
         undefined ->
             undefined;
         SubscriberRef ->
             delete(SubscriberRef)
     end.
 
--spec find(emqx_mq_types:subscriber_ref() | emqx_mq_types:mq_topic()) ->
+-spec find(emqx_mq_types:subscriber_ref() | name_topic()) ->
     emqx_mq_sub:t() | undefined.
 find(SubscriberRef) when is_reference(SubscriberRef) ->
     case erlang:get(?SUB_PD_KEY(SubscriberRef)) of
@@ -72,8 +74,8 @@ find(SubscriberRef) when is_reference(SubscriberRef) ->
         Sub ->
             Sub#{subscriber_ref => SubscriberRef}
     end;
-find(MQTopicFilter) when is_binary(MQTopicFilter) ->
-    case erlang:get(?TOPIC_PD_KEY(MQTopicFilter)) of
+find({Name, Topic} = NameTopic) when is_binary(Name) andalso (is_binary(Topic) orelse Topic =:= undefined) ->
+    case erlang:get(?NAME_TOPIC_PD_KEY(NameTopic)) of
         undefined ->
             undefined;
         SubscriberRef ->
