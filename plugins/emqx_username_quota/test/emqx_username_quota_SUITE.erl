bfd4cadadc2f337b86eb0f0cc2912c91136be02b
zmstone <zmstone@gmail.com>
2026-02-17 09:12:09 +0100

    test(username-quota): ensure test coverage of cluster node down

--- plugins/emqx_username_quota/test/emqx_username_quota_SUITE.erl
+++ plugins/emqx_username_quota/test/emqx_username_quota_SUITE.erl
@@ -26,11 +26,29 @@ end_per_suite(Config) ->
     ok = emqx_cth_suite:stop(?config(apps, Config)).
 
 init_per_testcase(_Case, Config) ->
-    ok = ensure_snapshot_started(),
-    ok = emqx_username_quota:reset(),
-    ok = emqx_username_quota_config:update(#{
-        <<"snapshot_request_timeout_ms">> => 60000
-    }),
+    case is_cluster_watch_case(_Case) of
+        true ->
+            Config;
+        false ->
+            ok = ensure_snapshot_started(),
+            ok = emqx_username_quota:reset(),
+            ok = emqx_username_quota_config:update(#{
+                <<"snapshot_request_timeout_ms">> => 60000
+            }),
+            Config
+    end.
+
+is_cluster_watch_case({testcase, Case}) ->
+    is_cluster_watch_case(Case);
+is_cluster_watch_case({testcase, Case, _Opts}) ->
+    is_cluster_watch_case(Case);
+is_cluster_watch_case(Case) when is_atom(Case) ->
+    lists:prefix("t_cluster_watch_", atom_to_list(Case));
+is_cluster_watch_case(_Case) ->
+    false.
+
+end_per_testcase(_Case, Config) ->
+    meck:unload(),
     Config.
 
 ensure_snapshot_started() ->
@@ -172,3 +190,103 @@ t_api_list_rebuilding_with_retry_cursor(_Config) ->
         Body
     ),
     ok = meck:unload(emqx_username_quota_state).
+
+t_cluster_watch_init_terminate(_Config) ->
+    ok = meck:new(ekka, [non_strict, passthrough]),
+    ok = meck:new(emqx_username_quota_state, [non_strict, passthrough]),
+    ok = meck:expect(ekka, monitor, fun(membership) -> ok end),
+    ok = meck:expect(ekka, unmonitor, fun(membership) -> ok end),
+    ok = meck:expect(emqx_username_quota_state, clear_self_node, fun() -> ok end),
+    {ok, State} = emqx_username_quota_cluster_watch:init([]),
+    ?assertEqual(#{}, State),
+    ok = emqx_username_quota_cluster_watch:terminate(normal, State).
+
+t_cluster_watch_clear_for_node(_Config) ->
+    ok = meck:new(emqx, [non_strict, passthrough]),
+    ok = meck:new(emqx_username_quota_state, [non_strict, passthrough]),
+    Node = 'node1@127.0.0.1',
+    ok = meck:expect(emqx, running_nodes, fun() -> [Node] end),
+    ok = meck:expect(emqx_username_quota_state, clear_for_node, fun(_N) -> ok end),
+    ?assertEqual(
+        {noreply, #{}}, emqx_username_quota_cluster_watch:handle_info({clear_for_node, Node}, #{})
+    ),
+    ?assertNot(meck:called(emqx_username_quota_state, clear_for_node, [Node])),
+    ok = meck:expect(emqx, running_nodes, fun() -> [] end),
+    ?assertEqual(
+        {noreply, #{}}, emqx_username_quota_cluster_watch:handle_info({clear_for_node, Node}, #{})
+    ),
+    ?assert(meck:called(emqx_username_quota_state, clear_for_node, [Node])).
+
+t_cluster_watch_nodedown_core_and_replicant(_Config) ->
+    ok = meck:new(mria_rlog, [non_strict, passthrough]),
+    Node = 'node2@127.0.0.1',
+    ok = meck:expect(mria_rlog, role, fun() -> core end),
+    ?assertEqual(
+        {noreply, #{}}, emqx_username_quota_cluster_watch:handle_info({nodedown, Node}, #{})
+    ),
+    ok = meck:expect(mria_rlog, role, fun() -> replicant end),
+    ?assertEqual(
+        {noreply, #{}}, emqx_username_quota_cluster_watch:handle_info({nodedown, Node}, #{})
+    ).
+
+t_cluster_watch_membership_events(_Config) ->
+    ok = meck:new(mria_rlog, [non_strict, passthrough]),
+    ok = meck:expect(mria_rlog, role, fun() -> replicant end),
+    Node = 'node3@127.0.0.1',
+    ?assertEqual(
+        {noreply, #{}},
+        emqx_username_quota_cluster_watch:handle_info({membership, {mnesia, down, Node}}, #{})
+    ),
+    ?assertEqual(
+        {noreply, #{}},
+        emqx_username_quota_cluster_watch:handle_info({membership, {node, down, Node}}, #{})
+    ),
+    ?assertEqual(
+        {noreply, #{}},
+        emqx_username_quota_cluster_watch:handle_info({membership, up}, #{})
+    ),
+    ?assertEqual({noreply, #{}}, emqx_username_quota_cluster_watch:handle_info(unknown, #{})).
+
+t_cluster_watch_call_cast_code_change(_Config) ->
+    State = #{key => value},
+    ?assertEqual(
+        {reply, ignored, State},
+        emqx_username_quota_cluster_watch:handle_call(req, {self(), make_ref()}, State)
+    ),
+    ?assertEqual({noreply, State}, emqx_username_quota_cluster_watch:handle_cast(msg, State)),
+    ?assertEqual({ok, State}, emqx_username_quota_cluster_watch:code_change(old, State, extra)).
+
+t_cluster_watch_immediate_node_clear(_Config) ->
+    ok = meck:new(ekka, [non_strict, passthrough]),
+    ok = meck:new(emqx, [non_strict, passthrough]),
+    ok = meck:new(emqx_username_quota_state, [non_strict, passthrough]),
+    ok = meck:expect(ekka, monitor, fun(membership) -> ok end),
+    ok = meck:expect(ekka, unmonitor, fun(membership) -> ok end),
+    ok = meck:expect(emqx_username_quota_state, clear_self_node, fun() -> ok end),
+    ok = meck:expect(emqx, running_nodes, fun() -> [] end),
+    ok = meck:expect(emqx_username_quota_state, clear_for_node, fun(_N) -> ok end),
+    {ok, Pid} = emqx_username_quota_cluster_watch:start_link(),
+    true = unlink(Pid),
+    Node = 'node4@127.0.0.1',
+    ?assertEqual(async, emqx_username_quota_cluster_watch:immediate_node_clear(Node)),
+    ok = wait_until(fun() -> meck:called(emqx_username_quota_state, clear_for_node, [Node]) end),
+    exit(Pid, shutdown),
+    receive
+        {'EXIT', Pid, shutdown} -> ok
+    after 1000 ->
+        ok
+    end.
+
+wait_until(Pred) ->
+    wait_until(Pred, 40).
+
+wait_until(Pred, 0) ->
+    ?assert(Pred());
+wait_until(Pred, N) ->
+    case Pred() of
+        true ->
+            ok;
+        false ->
+            timer:sleep(25),
+            wait_until(Pred, N - 1)
+    end.
