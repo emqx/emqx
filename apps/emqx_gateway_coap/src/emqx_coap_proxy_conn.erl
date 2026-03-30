d3c8d3e5310d3b0f99f6c6c64e5e2631206b84ca
Jianbo He <heeejianbo@gmail.com>
2026-03-30 12:09:18 +0800

    fix(coap): harden token takeover checks and cover UDP/DTLS takeover

--- apps/emqx_gateway_coap/src/emqx_coap_proxy_conn.erl
+++ apps/emqx_gateway_coap/src/emqx_coap_proxy_conn.erl
@@ -1,17 +1,5 @@
 %%--------------------------------------------------------------------
 %% Copyright (c) 2021-2026 EMQ Technologies Co., Ltd. All Rights Reserved.
-%%
-%% Licensed under the Apache License, Version 2.0 (the "License");
-%% you may not use this file except in compliance with the License.
-%% You may obtain a copy of the License at
-%%
-%%     http://www.apache.org/licenses/LICENSE-2.0
-%%
-%% Unless required by applicable law or agreed to in writing, software
-%% distributed under the License is distributed on an "AS IS" BASIS,
-%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-%% See the License for the specific language governing permissions and
-%% limitations under the License.
 %%--------------------------------------------------------------------
 
 -module(emqx_coap_proxy_conn).
@@ -36,23 +24,80 @@ find_or_create(CId, Transport, Peer, Opts) ->
             emqx_gateway_conn:start_link(Transport, Peer, Opts)
     end.
 
-get_connection_id(_Transport, _Peer, State, Data) ->
-    case parse_incoming(Data, [], State) of
+get_connection_id(_Transport, Peer, State, Data) ->
+    {ParseState, BoundCId} = split_state(State),
+    case parse_incoming(Data, [], ParseState) of
         {[Msg | _] = Packets, NState} ->
-            case emqx_coap_message:extract_uri_query(Msg) of
-                #{
-                    <<"clientid">> := ClientId
-                } ->
-                    {ok, ClientId, Packets, NState};
-                _ ->
-                    ErrMsg = <<"Missing token or clientid in connection mode">>,
-                    Reply = emqx_coap_message:piggyback({error, bad_request}, ErrMsg, Msg),
-                    Bin = emqx_coap_frame:serialize_pkt(Reply, emqx_coap_frame:serialize_opts()),
-                    {error, Bin}
+            case Msg of
+                #coap_message{} ->
+                    {CId, NBoundCId} = choose_cid(Msg, BoundCId, Peer),
+                    {ok, CId, Packets, merge_state(NState, NBoundCId)};
+                {coap_ignore, _} ->
+                    %% RFC 7252 Section 3: unknown versions must be silently ignored.
+                    {ok, route_cid(BoundCId, Peer), Packets, merge_state(NState, BoundCId)};
+                {coap_format_error, Type, MsgId, _} ->
+                    %% RFC 7252 Section 4.2: reject CON format errors with Reset.
+                    _ = {Type, MsgId},
+                    %% RFC 7252 Section 4.2: per-message errors must not stop the listener.
+                    {ok, route_cid(BoundCId, Peer), Packets, merge_state(NState, BoundCId)};
+                {coap_request_error, Req, Error} ->
+                    %% RFC 7252 Section 5.4.1/5.8: reply with a 4.xx error for bad requests.
+                    _ = {Req, Error},
+                    %% RFC 7252 Section 4.2: per-message errors must not stop the listener.
+                    {ok, route_cid(BoundCId, Peer), Packets, merge_state(NState, BoundCId)}
             end;
         _Error ->
             invalid
     end.
+
+peer_id(Peer) ->
+    {peer, Peer}.
+
+split_state(#{parse_state := ParseState, cid := BoundCId}) ->
+    {ParseState, BoundCId};
+split_state(#{parse_state := ParseState}) ->
+    {ParseState, undefined};
+split_state(ParseState) ->
+    {ParseState, undefined}.
+
+merge_state(ParseState, BoundCId) ->
+    #{parse_state => ParseState, cid => BoundCId}.
+
+route_cid(undefined, Peer) ->
+    peer_id(Peer);
+route_cid(BoundCId, _Peer) ->
+    BoundCId.
+
+choose_cid(Msg, BoundCId, Peer) ->
+    URIQuery = emqx_coap_message:extract_uri_query(Msg),
+    ReqClientId = normalize_clientid(extract_clientid(URIQuery)),
+    select_cid(ReqClientId, BoundCId, Peer).
+
+extract_clientid(URIQuery) ->
+    case maps:find(<<"clientid">>, URIQuery) of
+        {ok, ClientId} ->
+            ClientId;
+        error ->
+            maps:get("clientid", URIQuery, undefined)
+    end.
+
+normalize_clientid(undefined) ->
+    undefined;
+normalize_clientid(ClientId) when is_binary(ClientId) ->
+    ClientId;
+normalize_clientid(ClientId) when is_list(ClientId) ->
+    list_to_binary(ClientId);
+normalize_clientid(_Other) ->
+    undefined.
+
+select_cid(undefined, undefined, Peer) ->
+    {peer_id(Peer), undefined};
+select_cid(undefined, BoundCId, _Peer) ->
+    {BoundCId, BoundCId};
+select_cid(ReqClientId, undefined, _Peer) ->
+    {ReqClientId, ReqClientId};
+select_cid(_ReqClientId, BoundCId, _Peer) ->
+    {BoundCId, BoundCId}.
 
 dispatch(Pid, _State, Packet) ->
     erlang:send(Pid, Packet).
