6761c34bbce9eb47b16c05762414682e50faa38c
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2026-05-05 09:37:23 -0300

    fix(rule events): add `connected_at` to `client.connack` event
    Fixes https://emqx.atlassian.net/browse/EMQX-14945

--- apps/emqx_rule_engine/src/emqx_rule_events.erl
+++ apps/emqx_rule_engine/src/emqx_rule_events.erl
@@ -489,6 +489,7 @@ eventmsg_connack(
     Keepalive = maps:get(keepalive, ConnInfo, 0),
     ConnProps = maps:get(conn_props, ConnInfo, #{}),
     ExpiryInterval = maps:get(expiry_interval, ConnInfo, 0),
+    ConnectedAt = maps:get(connected_at, ConnInfo, undefined),
     with_basic_columns(
         'client.connack',
         #{
@@ -502,6 +503,7 @@ eventmsg_connack(
             proto_ver => ProtoVer,
             keepalive => Keepalive,
             expiry_interval => ExpiryInterval,
+            connected_at => ConnectedAt,
             conn_props => printable_maps(ConnProps)
         },
         #{}
--- apps/emqx_rule_engine/test/emqx_rule_engine_SUITE.erl
+++ apps/emqx_rule_engine/test/emqx_rule_engine_SUITE.erl
@@ -4397,6 +4397,7 @@ verify_event_fields('client.connack', Fields) ->
     #{
         clientid := ClientId,
         clean_start := CleanStart,
+        connected_at := ConnectedAt,
         username := Username,
         peername := PeerName,
         sockname := SockName,
@@ -4421,6 +4422,7 @@ verify_event_fields('client.connack', Fields) ->
     ?assert(is_boolean(CleanStart)),
     ?assertEqual(60000, ExpiryInterval),
     ?assertMatch(#{'Session-Expiry-Interval' := 60}, Properties),
+    ?assert(is_integer(ConnectedAt) orelse ConnectedAt == undefined),
     ?assert(0 =< TimestampElapse andalso TimestampElapse =< 60 * 1000);
 verify_event_fields('client.check_authz_complete', Fields) ->
     #{
