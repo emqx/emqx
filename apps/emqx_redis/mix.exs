19f2403ec10c4cf7bde2b00a40f66592497e16e8
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2026-05-15 17:45:46 -0300

    chore: bump app vsns

--- apps/emqx_redis/mix.exs
+++ apps/emqx_redis/mix.exs
@@ -5,7 +5,7 @@ defmodule EMQXRedis.MixProject do
   def project do
     [
       app: :emqx_redis,
-      version: "6.2.0",
+      version: "6.2.1",
       build_path: "../../_build",
       erlc_options: UMP.erlc_options(),
       erlc_paths: UMP.erlc_paths(),
