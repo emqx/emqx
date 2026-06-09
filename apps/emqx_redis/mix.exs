3107e90bf8664ab8ac68036471be220106d31749
Jianbo He <heeejianbo@gmail.com>
2026-06-09 14:51:44 +0800

    chore(redis): update sentinel registry dependency

--- apps/emqx_redis/mix.exs
+++ apps/emqx_redis/mix.exs
@@ -26,7 +26,7 @@ defmodule EMQXRedis.MixProject do
       {:emqx_connector, in_umbrella: true, runtime: false},
       {:emqx_resource, in_umbrella: true},
       {:eredis_cluster,
-       github: "emqx/eredis_cluster", ref: "34b55acb50f95fd6887c755d2b27b3170502d4e4"}
+       github: "emqx/eredis_cluster", ref: "32333e1cfae9fc16a2ba943048e1cfdc379c95d5"}
     ]
   end
 end
