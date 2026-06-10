2b5725f821ee2b23b15fc6de091344c7890de70e
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2026-06-10 17:37:04 -0300

    feat(kafka): allow usage of iam roles anywhere credential helper
    Fixes https://github.com/emqx/emqx/issues/17417

--- apps/emqx_bridge_kafka/src/emqx_bridge_kafka_msk_iam_authn.erl
+++ apps/emqx_bridge_kafka/src/emqx_bridge_kafka_msk_iam_authn.erl
@@ -4,9 +4,10 @@
 -module(emqx_bridge_kafka_msk_iam_authn).
 
 %% API
--export([token_callback/1, generate_token/0]).
+-export([token_callback/1, token_callback/2, mk_token_callback/1, generate_token/1]).
 
 -include_lib("erlcloud/include/erlcloud_aws.hrl").
+-include_lib("emqx/include/logger.hrl").
 
 -elvis([{elvis_style, atom_naming_convention, disable}]).
 
@@ -22,12 +23,41 @@
 %% API
 %%------------------------------------------------------------------------------
 
-token_callback(_Context) ->
-    {ok, #{token => generate_token()}}.
+token_callback(Context) ->
+    token_callback(Context, _Opts = #{}).
 
-generate_token() ->
-    AWSConfig = load_config(),
-    {ok, Region0} = erlcloud_ec2_meta:get_instance_metadata_v2("placement/region", AWSConfig, #{}),
+token_callback(_Context, Opts) ->
+    try
+        Token = generate_token(Opts),
+        {ok, #{token => Token}}
+    catch
+        throw:#{msg := _} = LogCtx ->
+            ?SLOG(warning, LogCtx),
+            {error, LogCtx};
+        Kind:Reason:Stacktrace ->
+            ?SLOG(warning, #{
+                msg => "kafka_msk_token_exception",
+                kind => Kind,
+                reason => Reason,
+                stacktrace => Stacktrace
+            }),
+            {error, failed_to_fetch_token}
+    end.
+
+mk_token_callback(#{} = Opts) ->
+    fun(Context) -> token_callback(Context, Opts) end.
+
+generate_token(Opts0) ->
+    Opts1 = maps:with([endpoint], Opts0),
+    Opts2 = emqx_utils_maps:update_if_present(endpoint, fun str/1, Opts1),
+    AWSConfig = load_config(Opts2),
+    Region0 =
+        case Opts0 of
+            #{region := Region1} ->
+                Region1;
+            _ ->
+                get_metadata("placement/region", AWSConfig, Opts2, "fetch_region")
+        end,
     Region = str(Region0),
     Date = erlcloud_aws:iso_8601_basic_time(),
     Service = ?SERVICE,
@@ -74,25 +104,29 @@ generate_token() ->
 %% Internal fns
 %%------------------------------------------------------------------------------
 
-load_config() ->
+load_config(Opts0) ->
     AWSConfig0 = erlcloud_aws:default_config(),
-    {ok, Token} = erlcloud_ec2_meta:get_metadata_v2_session_token(AWSConfig0),
-    Opts = #{session_token => Token},
-    {ok, Body0} =
-        erlcloud_ec2_meta:get_instance_metadata_v2("iam/security-credentials/", AWSConfig0, Opts),
+    Token = get_session_token(AWSConfig0, Opts0),
+    Opts = Opts0#{session_token => Token},
+    Body0 = get_metadata("iam/security-credentials/", AWSConfig0, Opts, "list_roles"),
     [Role0 | _] = binary:split(Body0, <<$\n>>),
     Role = binary_to_list(Role0),
-    {ok, Body1} =
-        erlcloud_ec2_meta:get_instance_metadata_v2(
-            "iam/security-credentials/" ++ Role,
-            AWSConfig0,
-            Opts
-        ),
+    Body1 = get_metadata(
+        "iam/security-credentials/" ++ Role, AWSConfig0, Opts, "get_security_token"
+    ),
     #{
         <<"AccessKeyId">> := AccessKeyIdBin,
         <<"SecretAccessKey">> := SecretAccessKeyBin,
         <<"Token">> := SecurityTokenBin
     } = emqx_utils_json:decode(Body1),
+    maybe
+        <<"">> ?= SecurityTokenBin,
+        %% likely an ill-configured credential helper process
+        throw(#{
+            msg => "kafka_msk_credential_endpoint_returned_empty_token",
+            opts => emqx_utils:redact(Opts0)
+        })
+    end,
     AWSConfig0#aws_config{
         access_key_id = binary_to_list(AccessKeyIdBin),
         secret_access_key = binary_to_list(SecretAccessKeyBin),
@@ -120,3 +154,27 @@ signature(Context) ->
     Res.
 
 str(X) -> emqx_utils_conv:str(X).
+
+get_session_token(AWSConfig, Opts) ->
+    case erlcloud_ec2_meta:get_metadata_v2_session_token(AWSConfig, Opts) of
+        {ok, Token} ->
+            Token;
+        {error, Reason} ->
+            throw(#{
+                msg => "kafka_msk_failed_to_fetch_session_token",
+                reason => Reason,
+                opts => emqx_utils:redact(Opts)
+            })
+    end.
+
+get_metadata(Path, AWSConfig, Opts, Step) ->
+    case erlcloud_ec2_meta:get_instance_metadata_v2(Path, AWSConfig, Opts) of
+        {ok, Token} ->
+            Token;
+        {error, Reason} ->
+            throw(#{
+                msg => "kafka_msk_failed_to_" ++ Step,
+                reason => Reason,
+                opts => emqx_utils:redact(maps:without([security_token], Opts))
+            })
+    end.
