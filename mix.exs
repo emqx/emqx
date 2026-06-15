8d4f924ba86e648d7a8c8b1ecffa3c7d5f0a1643
zmstone <zmstone@gmail.com>
2026-06-15 14:29:48 +0200

    build(deps): upgrade jiffy 1.1.2 -> 2.0.1

--- mix.exs
+++ mix.exs
@@ -190,7 +190,7 @@ defmodule EMQXUmbrella.MixProject do
   def common_dep(:ehttpc),
     do: {:ehttpc, github: "emqx/ehttpc", tag: "0.7.3", override: true}
 
-  def common_dep(:jiffy), do: {:jiffy, "1.1.2", override: true}
+  def common_dep(:jiffy), do: {:jiffy, "2.0.1", override: true}
 
   def common_dep(:grpc),
     do:
