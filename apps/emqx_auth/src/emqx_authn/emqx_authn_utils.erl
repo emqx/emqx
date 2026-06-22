1f9de9e52d144abe9eb3b1e3e04d40ac028f4d63
Ilia Averianov <av@rubybox.dev>
2026-06-22 20:35:09 +0300

    feat(authn): add specs

--- apps/emqx_auth/src/emqx_authn/emqx_authn_utils.erl
+++ apps/emqx_auth/src/emqx_authn/emqx_authn_utils.erl
@@ -36,10 +36,19 @@
     owner_id => OWNER_ID
 }).
 
+-type authn_provider_state() :: #{resource_id => binary(), enable => boolean(), _ => _}.
+-type resource_config() :: map().
+-type mechanism() :: atom() | binary().
+-type backend() :: atom() | binary().
+-type template_var() :: emqx_template:varname() | {var_namespace, emqx_template:varname()}.
+-type used_template_vars() :: [template_var()].
+
 %%--------------------------------------------------------------------
 %% APIs
 %%--------------------------------------------------------------------
 
+-spec create_resource(module(), resource_config(), authn_provider_state(), mechanism(), backend()) ->
+    ok | {error, term()}.
 create_resource(Module, ResourceConfig, #{resource_id := ResourceId} = State, Mechanism, Backend) ->
     maybe
         OwnerId = owner_id(Mechanism, Backend),
@@ -54,6 +63,8 @@ create_resource(Module, ResourceConfig, #{resource_id := ResourceId} = State, Me
         ok = start_resource_if_enabled(State, Mechanism, Backend)
     end.
 
+-spec update_resource(module(), resource_config(), authn_provider_state(), mechanism(), backend()) ->
+    ok | {error, term()}.
 update_resource(Module, ResourceConfig, #{resource_id := ResourceId} = State, Mechanism, Backend) ->
     maybe
         OwnerId = owner_id(Mechanism, Backend),
@@ -64,6 +75,7 @@ update_resource(Module, ResourceConfig, #{resource_id := ResourceId} = State, Me
         start_resource_if_enabled(State, Mechanism, Backend)
     end.
 
+-spec start_resource_if_enabled(authn_provider_state(), mechanism(), backend()) -> ok.
 start_resource_if_enabled(#{resource_id := ResourceId, enable := true}, Mechanism, Backend) ->
     case emqx_resource:start(ResourceId) of
         ok ->
@@ -83,6 +95,7 @@ start_resource_if_enabled(#{resource_id := ResourceId, enable := true}, Mechanis
 start_resource_if_enabled(#{resource_id := _ResourceId, enable := false}, _Mechanism, _Backend) ->
     ok.
 
+-spec init_state(map(), map()) -> authn_provider_state().
 init_state(#{enable := Enable} = _Source, Values) ->
     maps:merge(
         #{
@@ -91,16 +104,23 @@ init_state(#{enable := Enable} = _Source, Values) ->
         Values
     ).
 
+-spec cleanup_resource_config(list(atom()), resource_config()) -> resource_config().
 cleanup_resource_config(WithoutFields, Config) ->
     maps:without([enable] ++ WithoutFields, Config).
 
+-spec parse_deep(term()) -> {used_template_vars(), emqx_template:t()}.
 parse_deep(Template) -> emqx_auth_template:parse_deep(Template, ?AUTHN_DEFAULT_ALLOWED_VARS).
 
+-spec parse_str(unicode:chardata()) -> {used_template_vars(), emqx_template:t()}.
 parse_str(Template) -> emqx_auth_template:parse_str(Template, ?AUTHN_DEFAULT_ALLOWED_VARS).
 
+-spec parse_sql(emqx_template_sql:raw_statement_template(), emqx_template_sql:sql_parameters()) ->
+    {used_template_vars(), emqx_template_sql:statement(), emqx_template_sql:row_template()}.
 parse_sql(Template, ReplaceWith) ->
     emqx_auth_template:parse_sql(Template, ReplaceWith, ?AUTHN_DEFAULT_ALLOWED_VARS).
 
+-spec check_password_from_selected_map(atom(), #{binary() => term()}, binary() | undefined) ->
+    {error, bad_username_or_password} | ok.
 check_password_from_selected_map(_Algorithm, _Selected, undefined) ->
     {error, bad_username_or_password};
 check_password_from_selected_map(Algorithm, Selected, Password) ->
@@ -124,16 +144,19 @@ check_password_from_selected_map(Algorithm, Selected, Password) ->
             end
     end.
 
+-spec is_superuser(#{binary() => term()}) -> #{is_superuser => boolean()}.
 is_superuser(#{<<"is_superuser">> := Value}) ->
     #{is_superuser => to_bool(Value)};
 is_superuser(#{}) ->
     #{is_superuser => false}.
 
+-spec client_attrs(#{binary() => term()}) -> #{client_attrs => map()}.
 client_attrs(#{<<"client_attrs">> := Attrs}) ->
     #{client_attrs => drop_invalid_attr(Attrs)};
 client_attrs(_) ->
     #{client_attrs => #{}}.
 
+-spec clientid_override(#{binary() => term()}) -> #{clientid_override => term()}.
 clientid_override(#{<<"clientid_override">> := Value}) when
     is_binary(Value) andalso Value /= <<"">>
 ->
@@ -141,45 +164,35 @@ clientid_override(#{<<"clientid_override">> := Value}) when
 clientid_override(_) ->
     #{}.
 
-drop_invalid_attr(Map) when is_map(Map) ->
-    maps:from_list(do_drop_invalid_attr(maps:to_list(Map))).
-
-do_drop_invalid_attr([]) ->
-    [];
-do_drop_invalid_attr([{K, V} | More]) ->
-    case emqx_utils:is_restricted_str(K) of
-        true ->
-            [{iolist_to_binary(K), iolist_to_binary(V)} | do_drop_invalid_attr(More)];
-        false ->
-            ?SLOG(debug, #{msg => "invalid_client_attr_dropped", attr_name => K}, #{
-                tag => "AUTHN"
-            }),
-            do_drop_invalid_attr(More)
-    end.
-
+-spec ensure_apps_started(term()) -> ok.
 ensure_apps_started(bcrypt) ->
     {ok, _} = application:ensure_all_started(bcrypt),
     ok;
 ensure_apps_started(_) ->
     ok.
 
+-spec bin(atom() | binary() | list()) -> binary().
 bin(A) when is_atom(A) -> atom_to_binary(A, utf8);
 bin(L) when is_list(L) -> iolist_to_binary(L);
 bin(X) when is_binary(X) -> X.
 
+-spec cleanup_resources() -> ok.
 cleanup_resources() ->
     lists:foreach(
         fun emqx_resource:remove_local/1,
         emqx_resource:list_group_instances(?AUTHN_RESOURCE_GROUP)
     ).
 
+-spec make_resource_id(term()) -> emqx_resource:resource_id().
 make_resource_id(Name) ->
     NameBin = bin([<<"authn:">>, bin(Name)]),
     emqx_resource:generate_id(NameBin).
 
+-spec without_password(map()) -> map().
 without_password(Credential) ->
     without_password(Credential, [password, <<"password">>]).
 
+-spec to_bool(term()) -> boolean().
 to_bool(<<"true">>) ->
     true;
 to_bool(true) ->
@@ -214,6 +227,11 @@ to_bool(MaybeBinInt) when is_binary(MaybeBinInt) ->
 to_bool(_) ->
     false.
 
+-spec cached_simple_sync_query(
+    emqx_auth_cache:cache_key(),
+    emqx_resource:resource_id(),
+    _Request :: term()
+) -> term().
 cached_simple_sync_query(CacheKey, ResourceID, Query) ->
     emqx_auth_utils:cached_simple_sync_query(?AUTHN_CACHE, CacheKey, ResourceID, Query).
 
@@ -252,3 +270,19 @@ without_password(Credential, [Name | Rest]) ->
 
 owner_id(Mechanism, Backend) ->
     bin([bin(Mechanism), ":", bin(Backend)]).
+
+drop_invalid_attr(Map) when is_map(Map) ->
+    maps:from_list(do_drop_invalid_attr(maps:to_list(Map))).
+
+do_drop_invalid_attr([]) ->
+    [];
+do_drop_invalid_attr([{K, V} | More]) ->
+    case emqx_utils:is_restricted_str(K) of
+        true ->
+            [{iolist_to_binary(K), iolist_to_binary(V)} | do_drop_invalid_attr(More)];
+        false ->
+            ?SLOG(debug, #{msg => "invalid_client_attr_dropped", attr_name => K}, #{
+                tag => "AUTHN"
+            }),
+            do_drop_invalid_attr(More)
+    end.
