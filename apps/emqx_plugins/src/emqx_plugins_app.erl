fdcdd9ebda820f1aebdd4957125e6fb959b0c3db
JianBo He <HeeeJianBo@gmail.com>
2026-07-03 13:38:50 +0800

    Revert "fix(plugins): purge stale plugins after cluster rejoin"

--- apps/emqx_plugins/src/emqx_plugins_app.erl
+++ apps/emqx_plugins/src/emqx_plugins_app.erl
@@ -18,7 +18,6 @@ start(_Type, _Args) ->
     %% load all pre-configured
     {ok, Sup} = emqx_plugins_sup:start_link(),
     ok = emqx_plugins:ensure_installed(),
-    ok = emqx_plugins:purge_unconfigured(),
     ok = emqx_plugins:ensure_started(),
     ok = emqx_config_handler:add_handler([?CONF_ROOT], emqx_plugins),
     ?tp("emqx_plugins_app_started", #{}),
