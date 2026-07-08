86dfb76ff693555351cf4180c1940722d9745728
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2026-07-08 09:30:46 -0300

    fix(conf cli): move load/unload from `emqx_modules` to own application
    Otherwise, using `ESSENTIAL` feature gate won't have `emqx ctl conf ...` working.

--- apps/emqx_modules/src/emqx_modules_app.erl
+++ apps/emqx_modules/src/emqx_modules_app.erl
@@ -24,7 +24,6 @@ stop(_State) ->
 maybe_enable_modules() ->
     emqx_conf:get([delayed, enable], true) andalso emqx_delayed:load(),
     emqx_observer_cli:enable(),
-    emqx_conf_cli:load(),
     ok = emqx_rewrite:enable(),
     emqx_topic_metrics:enable(),
     emqx_modules_conf:load().
@@ -33,6 +32,5 @@ maybe_disable_modules() ->
     emqx_conf:get([delayed, enable], true) andalso emqx_delayed:unload(),
     emqx_conf:get([observer_cli, enable], true) andalso emqx_observer_cli:disable(),
     emqx_rewrite:disable(),
-    emqx_conf_cli:unload(),
     emqx_topic_metrics:disable(),
     emqx_modules_conf:unload().
