50d2d357317c11e7331aa88f2756f91a495eb25e
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2026-07-21 12:11:25 +0200

    Revert "feat(ds): Integrate with classy"
    This reverts commit 8250ee68a1c77607914fd6e9d07b8431a7593c48.

--- apps/emqx_durable_storage/mix.exs
+++ apps/emqx_durable_storage/mix.exs
@@ -45,7 +45,6 @@ defmodule EMQXDurableStorage.MixProject do
       {:emqx_mix_utils, in_umbrella: true, runtime: false},
       {:emqx_utils, in_umbrella: true},
       {:emqx_bpapi, in_umbrella: true},
-      {:classy, runtime: false},
       :rocksdb,
       :gproc,
       :gen_rpc,
