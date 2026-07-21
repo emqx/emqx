f077ee5e0ad5d6e2124e3f03011657f9cb09e436
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2026-07-21 11:46:31 +0200

    feat(emqx_machine): Integrate with classy

--- apps/emqx_machine/src/emqx_machine.erl
+++ apps/emqx_machine/src/emqx_machine.erl
@@ -10,11 +10,18 @@
     brutal_shutdown/0,
     is_ready/0,
 
-    node_status/0
+    setup_classy_hooks/1,
+    migrate_site_id/0,
+    on_run_level/2,
+
+    node_status/0,
+
+    add_emqx_vsn/1,
+    on_node_classify/1
 ]).
 
 -export([open_ports_check/0]).
--export([mria_lb_custom_info/0, mria_lb_custom_info_check/1]).
+-export([mria_lb_discover/0, mria_lb_custom_info/0, mria_lb_custom_info_check/1]).
 
 -ifdef(TEST).
 -export([create_plan/0]).
@@ -32,27 +39,73 @@ start() ->
     ok = emqx_cluster:set_booting(true),
     ensure_valid_features(),
     emqx_mgmt_cli:load(),
-    case os:type() of
-        {win32, nt} ->
-            ok;
-        _Nix ->
-            os:set_signal(sighup, ignore),
-            %% default is handle
-            os:set_signal(sigterm, handle)
-    end,
-    ok = set_backtrace_depth(),
-    configure_shard_transports(),
-    set_mnesia_extra_diagnostic_checks(),
+    setup_vm(),
     ok = configure_otel_deps(),
+    %% Hand over control to classy:
+    ClassyDir = filename:join(emqx:data_dir(), "classy"),
+    ok = filelib:ensure_path(ClassyDir),
+    application:set_env(classy, table_dir, ClassyDir),
+    application:set_env(
+        classy, setup_hooks, {?MODULE, setup_classy_hooks, [fun ?MODULE:on_run_level/2]}
+    ),
+    {ok, _} = application:ensure_all_started(classy, permanent),
+    ok.
+
+setup_vm() ->
+    os:set_signal(sighup, ignore),
+    %% default is handle
+    os:set_signal(sigterm, handle),
+    ok = set_backtrace_depth().
+
+setup_classy_hooks(OnRunLevel) ->
+    application:set_env(classy, to_cluster_sets, [core]),
+    application:set_env(classy, quorum_sets, [core]),
+    classy:on_node_init(fun ?MODULE:migrate_site_id/0, 1),
+    %% Mria:
+    setup_mria(),
+    %% Cluster:
+    classy:pre_join(fun emqx_cluster:pre_join/4, 0),
+    classy:pre_kick(fun emqx_mgmt_api_ds:pre_kick/3, 0),
+    classy:enrich_site_info(fun ?MODULE:add_emqx_vsn/1, 0),
+    classy:on_node_classify(fun ?MODULE:on_node_classify/1, 0),
+    %% Application start:
+    classy:run_level(OnRunLevel, 99).
+
+on_run_level(From, To) ->
+    case {From, To} of
+        {single, cluster} ->
+            _ = emqx_machine_boot:post_boot(),
+            ok;
+        {cluster, single} ->
+            emqx_machine_boot:stop_apps();
+        _ ->
+            ok
+    end.
+
+migrate_site_id() ->
+    case emqx_dsch_migrate:read_old() of
+        {ok, #{schema := #{site := Site}}} ->
+            ?SLOG(notice, #{msg => "migrate_site_id", site => Site}),
+            classy_node:maybe_init_the_site(Site);
+        {ok, #{} = Bad} ->
+            ?SLOG(critical, #{msg => "bad_ds_schema", schema => Bad}),
+            ok;
+        _ ->
+            ok
+    end.
+
+setup_mria() ->
     %% Register mria callbacks that help to check compatibility of the
     %% replicant with the core node. Currently they rely on the exact
     %% match of the version of EMQX OTP application:
     _ = application:load(mria),
     _ = application:load(emqx),
+    mria_app:on_node_init(),
     mria_config:register_callback(lb_custom_info, fun ?MODULE:mria_lb_custom_info/0),
     mria_config:register_callback(lb_custom_info_check, fun ?MODULE:mria_lb_custom_info_check/1),
+    configure_shard_transports(),
+    set_mnesia_extra_diagnostic_checks(),
     mria_config:register_callback(heal_partition, fun emqx_broker_heal:on_autoheal/1),
-    ekka:start(),
     ok.
 
 graceful_shutdown() ->
@@ -225,6 +278,29 @@ mria_lb_custom_info_check(undefined) ->
     false;
 mria_lb_custom_info_check(OtherVsn) ->
     get_emqx_vsn() =:= OtherVsn.
+
+%% Forward classy autocluster results to mria
+%% TODO: integrate mria directly with classy?
+mria_lb_discover() ->
+    classy:nodes(connected).
+
+add_emqx_vsn(Acc) ->
+    Acc#{emqx => get_emqx_vsn()}.
+
+on_node_classify(#{emqx := Vsn}) ->
+    MyVsn = get_emqx_vsn(),
+    [
+        {emqx_vsn, Vsn},
+        case Vsn of
+            MyVsn -> emqx_same_vsn;
+            _ when Vsn > MyVsn -> emqx_next_vsn;
+            _ when Vsn < MyVsn -> emqx_prev_vsn
+        end
+    ];
+on_node_classify(#{}) ->
+    [].
+
+%% TODO: Forward mria start/stop callbacks for autoheal
 
 get_emqx_vsn() ->
     case application:get_key(emqx, vsn) of
