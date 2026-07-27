7541894827bcae25ee9c442546dca69015dadde1
Jianbo He <heeejianbo@gmail.com>
2026-07-27 12:40:04 +0800

    fix: handle plugin validation errors

---
+++ apps/emqx_management/src/proto/emqx_mgmt_api_plugins_proto_v5.erl
@@ -0,0 +1,37 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2026 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%-------------------------------------------------------------------
+-module(emqx_mgmt_api_plugins_proto_v5).
+
+-behaviour(emqx_bpapi).
+
+-export([
+    introduced_in/0,
+    ensure_start_package/2,
+    validate_start/2,
+    ensure_started/2,
+    ensure_stopped/2
+]).
+
+-include_lib("emqx/include/bpapi.hrl").
+
+-define(TIMEOUT, 10000).
+
+introduced_in() ->
+    "6.1.4".
+
+-spec ensure_start_package([node()], binary() | string()) -> emqx_rpc:multicall_result().
+ensure_start_package(Nodes, Name) ->
+    rpc:multicall(Nodes, emqx_plugins, ensure_start_package, [Name], ?TIMEOUT).
+
+-spec validate_start([node()], binary() | string()) -> emqx_rpc:multicall_result().
+validate_start(Nodes, Name) ->
+    rpc:multicall(Nodes, emqx_plugins, validate_start, [Name], ?TIMEOUT).
+
+-spec ensure_started([node()], binary() | string()) -> emqx_rpc:multicall_result().
+ensure_started(Nodes, Name) ->
+    rpc:multicall(Nodes, emqx_plugins, ensure_started, [Name], ?TIMEOUT).
+
+-spec ensure_stopped([node()], binary() | string()) -> emqx_rpc:multicall_result().
+ensure_stopped(Nodes, Name) ->
+    rpc:multicall(Nodes, emqx_plugins, ensure_stopped, [Name], ?TIMEOUT).
