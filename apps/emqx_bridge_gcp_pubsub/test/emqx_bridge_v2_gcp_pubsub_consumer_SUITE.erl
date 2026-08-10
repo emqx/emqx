01eac2edd09cfe558ac3877a321fe49659052c15
Thales Macedo Garitezi <16166434+thalesmg@users.noreply.github.com>
2026-08-10 09:52:03 -0300

    fix(gcp connectors): redact service account json (d58)
    Port of https://github.com/emqx/emqx/pull/18314
    
    Fixes https://github.com/emqx/emqx-dev-team-tasks/issues/323

--- apps/emqx_bridge_gcp_pubsub/test/emqx_bridge_v2_gcp_pubsub_consumer_SUITE.erl
+++ apps/emqx_bridge_gcp_pubsub/test/emqx_bridge_v2_gcp_pubsub_consumer_SUITE.erl
@@ -140,6 +140,13 @@ create_connector_api(Config, Overrides) ->
 create_source_api(TCConfig, Overrides) ->
     emqx_bridge_v2_testlib:create_source_api(TCConfig, Overrides).
 
+get_connector_api(TCConfig) ->
+    #{connector_type := Type, connector_name := Name} =
+        emqx_bridge_v2_testlib:get_common_values(TCConfig),
+    emqx_bridge_v2_testlib:simplify_result(
+        emqx_bridge_v2_testlib:get_connector_api(Type, Name)
+    ).
+
 get_source_api(TCConfig) ->
     #{type := Type, name := Name} =
         emqx_bridge_v2_testlib:get_common_values(TCConfig),
@@ -342,5 +349,19 @@ t_clear_stuck_unhealthy(TCConfig) ->
             {200, #{<<"status">> := <<"connected">>}},
             get_source_api(TCConfig)
         )
+    ),
+    ok.
+
+%% Verifies that reading the connector with a legacy service account field (in the root of
+%% the connector config) via the HTTP API returns a redacted service account.
+%% In 6.2.0, this was moved to under an `authentication` key.
+t_legacy_service_account_json_redact(TCConfig) ->
+    ?assertMatch(
+        {201, #{<<"service_account_json">> := <<"******">>}},
+        create_connector_api(TCConfig, #{})
+    ),
+    ?assertMatch(
+        {200, #{<<"service_account_json">> := <<"******">>}},
+        get_connector_api(TCConfig)
     ),
     ok.
