1bb9eccc281f335aa5086dd32bfd5f9532eed8ac
zmstone <zmstone@gmail.com>
2026-09-01 15:29:27 +0200

    test(audit): use include_lib for emqx_dashboard.hrl

--- apps/emqx_audit/test/emqx_audit_api_SUITE.erl
+++ apps/emqx_audit/test/emqx_audit_api_SUITE.erl
@@ -8,7 +8,7 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("common_test/include/ct.hrl").
 -include_lib("snabbkaffe/include/snabbkaffe.hrl").
--include("../../emqx_dashboard/include/emqx_dashboard.hrl").
+-include_lib("emqx_dashboard/include/emqx_dashboard.hrl").
 
 all() ->
     [
