2853e5b40fb653e6d426066b4ddbbaf8b2f27123
zmstone <zmstone@gmail.com>
2026-09-14 10:51:47 +0200

    chore(rocketmq): pin the client to its PR branch until v0.7.3 is tagged
    rebar3 failed to fetch the dependency by commit ref in CI.

--- apps/emqx_bridge_rocketmq/mix.exs
+++ apps/emqx_bridge_rocketmq/mix.exs
@@ -23,7 +23,8 @@ defmodule EMQXBridgeRocketmq.MixProject do
 
   def deps() do
     [
-      {:rocketmq, github: "emqx/rocketmq-client-erl", ref: "5dbd64e5053df1423c603476b18a38f295eda46c"},
+      {:rocketmq,
+       github: "emqx/rocketmq-client-erl", branch: "260914-topic-not-found-and-reconnect"},
       {:emqx_connector, in_umbrella: true, runtime: false},
       {:emqx_resource, in_umbrella: true},
       {:emqx_bridge, in_umbrella: true, runtime: false}
